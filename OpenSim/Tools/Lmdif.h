07eb3df551114fd4294b60b452f031e79e14b278
Peter Loan <james.peter.loan@gmail.com>
2007-01-26 18:23:06 +0000

    Forgot lmdif.* in earlier commit. Here they are.
    git-svn-id: file:///dev/shm/opensim/Trunk@2446 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ OpenSim/Tools/Lmdif.h
@@ -0,0 +1,43 @@
+
+/************************lmdif*************************
+ *
+ * Solves or minimizes the sum of squares of m nonlinear
+ * functions of n variables.
+ *
+ * From public domain Fortran version
+ * of Argonne National Laboratories MINPACK
+ *
+ * C translation by Steve Moshier
+ *
+ * Passing C++ and thread-safe by Ned Phipps
+ *
+ *****************************************************/
+
+#include "rdTools.h"
+
+void RDTOOLS_API lmdif_C(
+  void (*fcn)(int, int, double[], double[], int *, void *),
+  int    m,
+  int    n,
+  double x[],
+  double fvec[],
+  double ftol,
+  double xtol,
+  double gtol,
+  int    maxfev,
+  double epsfcn,
+  double diag[],
+  int    mode,
+  double factor,
+  int    nprint,
+  int   *info,
+  int   *nfev,
+  double fjac[],
+  int    ldfjac,
+  int    ipvt[],
+  double qtf[],
+  double wa1[],
+  double wa2[],
+  double wa3[],
+  double wa4[],
+  void *data);
