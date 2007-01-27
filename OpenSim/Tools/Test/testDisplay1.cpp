8a3d2de8bd8ee36b9e8086b6ee60111287914d0a
Ayman Habib <ahabib@stanford.edu>
2007-01-27 00:47:43 +0000

    First multiple sync. motions successful display (few missing pieces)
    git-svn-id: file:///dev/shm/opensim/Trunk@2448 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Tools/Test/testDisplay1.cpp
+++ OpenSim/Tools/Test/testDisplay1.cpp
@@ -5,12 +5,7 @@
 #include <string>
 #include <assert.h>
 #include <OpenSim/Tools/rdTools.h>
-#include <OpenSim/Tools/Geometry.h>
-#include <OpenSim/Simulation/SIMM/SimmModel.h>
-#include <OpenSim/Simulation/SIMM/SimmKinematicsEngine.h>
-#include <OpenSim/Simulation/SIMM/SimmModelVisibleIterator.h>
-#include <OpenSim/Simulation/SIMM/SimmModelIterator.h>
-#include <OpenSim/Simulation/SIMM/BodyIterator.h>
+#include <OpenSim/Simulation/SIMM/AbstractModel.h>
 
 using namespace OpenSim;
 using namespace std;
@@ -24,18 +19,9 @@ int TestVisibleObjectCollection();
  */
 int main(int argc, char* argv[])
 {
-	AbstractModel* model = new AbstractModel("dynamic.xml");
+	AbstractModel* model = new AbstractModel("C:/Projects/Models/FullBody/FullBody.xml");
 	model->setup();
 
-	BodyIterator iter = model->getDynamicsEngine().newBodyIterator();
-	while(!iter.finished()){
-		AbstractBody& nextBody = iter.getCurrent();
-		
-		cout << "Object:" << (owner?owner->getName():"no-owner") << "\t"  
-			<< "#geo files+analytical" << (nextVisibleObject->countGeometry()) << "\t" 
-			<< "Dependents" << nextVisibleObject->countDependents()
-			<< endl;
-		iter++;
-	}
+	delete model;
 	return(0);
 }
