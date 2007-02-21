469f37f7af34acf7cd6f18237a20219349abaefc
Ayman Habib <ahabib@stanford.edu>
2007-02-21 23:29:39 +0000

    Add constructor from file to IKTaskSet to be used from SimTrack GUI.
    git-svn-id: file:///dev/shm/opensim/Trunk@2658 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Simulation/SIMM/IKTaskSet.h
+++ OpenSim/Simulation/SIMM/IKTaskSet.h
@@ -12,6 +12,7 @@ namespace OpenSim {
 /**
  * @authors Eran Guendelman
  * @version 1.0
+ * - Added constructor from a file for use in GUI. -Ayman 02/20/07
  */
 
 class RDSIMULATION_API IKTaskSet : public Set<IKTask>
@@ -19,6 +20,7 @@ class RDSIMULATION_API IKTaskSet : public Set<IKTask>
 public:
 	IKTaskSet() { setType("IKTaskSet"); }
 	IKTaskSet(const IKTaskSet &aIKTaskSet) : Set<IKTask>(aIKTaskSet) { }
+	IKTaskSet(const std::string &aFileName) : Set<IKTask>(aFileName) { }
 #ifndef SWIG
 	IKTaskSet& operator=(const IKTaskSet &aIKTaskSet) { Set<IKTask>::operator=(aIKTaskSet); return *this; }
 #endif
