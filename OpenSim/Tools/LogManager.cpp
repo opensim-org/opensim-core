710ad61e44bc14922302179974a7e6cd64d20a21
Eran Guendelman <eran.guendelman@gmail.com>
2007-03-01 23:56:42 +0000

    Reactivate LogManager to see if it crashes the GUI still or not...
    git-svn-id: file:///dev/shm/opensim/Trunk@2712 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Tools/LogManager.cpp
+++ OpenSim/Tools/LogManager.cpp
@@ -52,7 +52,7 @@ sync()
 LogManager::LogManager()
 {
 	// Seems to be causing crashes in the GUI... maybe a multithreading issue.
-#if 0
+#if 1
 	// Change the underlying streambuf for the standard cout/cerr to our custom buffers
 	std::cout.rdbuf(&out);
 	std::cerr.rdbuf(&err);
