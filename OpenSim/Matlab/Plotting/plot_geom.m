262bf3982663807e03b7c44956bedc4bdc5d9b9d
Eran Guendelman <eran.guendelman@gmail.com>
2007-04-26 23:16:17 +0000

    function to plot geom read in from asc file
    git-svn-id: file:///dev/shm/opensim/Trunk@2927 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ OpenSim/Matlab/Plotting/plot_geom.m
@@ -0,0 +1,18 @@
+function plot_geom(geom)
+
+lightdir = [1 0 0];
+color = [0 0 1];
+
+close all;
+figure;
+hold on;
+for i=1:geom.np
+%for i=1:1
+    poly = geom.poly{i};
+%    poly(end+1) = poly(1);
+%    plot3(geom.vertices(poly,1), geom.vertices(poly,2), geom.vertices(poly,3));
+    N = sum(geom.normals(poly,:));
+    N = N / norm(N);
+    c = 0.5*(dot(N,lightdir)+1) * color;
+    patch(geom.vertices(poly,1), geom.vertices(poly,2), geom.vertices(poly,3), c)
+end
