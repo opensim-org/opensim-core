c5e4c33055d1949358aa2079d40d16c056cb5292
Eran Guendelman <eran.guendelman@gmail.com>
2007-05-10 18:59:49 +0000

    updated
    git-svn-id: file:///dev/shm/opensim/Trunk@2962 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Tests/testXMLParsing/testXMLParsing.cpp
+++ OpenSim/Tests/testXMLParsing/testXMLParsing.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <OpenSim/Common/Object.h>
 #include <OpenSim/Common/XMLDocument.h>
+#include <OpenSim/Common/XMLNode.h>
 #include <OpenSim/Common/IO.h>
 #include <OpenSim/Tools/CMCTool.h>
 #include <OpenSim/Simulation/Control/ControlSet.h>
@@ -11,18 +12,28 @@
 using namespace OpenSim;
 using namespace std;
 
-std::string transcode(const XMLCh *ch)
+void PrintSingleNode(const DOMNode *node)
 {
-	char *buffer = XMLString::transcode(ch);
-	string str = buffer;
-	delete[] buffer;
-	return str;
+	std::cout << XMLNode::Transcode(node->getNodeName()) << std::endl;
+}
+
+DOMElement *FindElement(const string &name, DOMNode *node)
+{
+	if(node->getNodeType() == DOMNode::ELEMENT_NODE && name == XMLNode::Transcode(node->getNodeName())) return (DOMElement*)node;
+	else {
+		const DOMNodeList *list = node->getChildNodes();
+		for(unsigned int i=0;i<list->getLength();i++) {
+			DOMElement *result = FindElement(name,list->item(i));
+			if(result) return result;
+		}
+	}
+	return 0;
 }
 
 void PrintNode(const DOMNode *node, const string &spaces = "")
 {
 	const DOMNodeList *list = node->getChildNodes();
-	std::cout << transcode(node->getNodeName()) << " (" << list->getLength() << " children)" << std::endl;
+	std::cout << XMLNode::Transcode(node->getNodeName()) << ": '" << XMLNode::Transcode(node->getNodeValue()) << "' (" << list->getLength() << " children)" << std::endl;
 	for(unsigned int i=0;i<list->getLength();i++) {
 		std::cout << spaces << "- " << i << ". ";
 		PrintNode(list->item(i),spaces+"- ");
@@ -43,17 +54,63 @@ int main(int argc,char **argv)
 #if 1
 	XMLDocument *xmldoc = new XMLDocument(filename);
 	DOMDocument *doc = xmldoc->getDOMDocument();
-	std::cout << "Document URI = " << IO::GetFileNameFromURI(transcode(doc->getDocumentURI())) << std::endl;
+	std::cout << "Document URI = " << IO::GetFileNameFromURI(XMLNode::Transcode(doc->getDocumentURI())) << std::endl;
 #if 0
 	DOMNode *node=doc->getDocumentElement()->getFirstChild();
-	std::cout << "child = " << transcode(node->getNodeName()) << std::endl;
+	std::cout << "child = " << XMLNode::Transcode(node->getNodeName()) << std::endl;
 	DOMNode *after=doc->getDocumentElement()->removeChild(node);
 	PrintDocument(doc);
 	std::cout << node << ", " << after << std::endl;
-	std::cout << "child = " << transcode(node->getNodeName()) << std::endl;
+	std::cout << "child = " << XMLNode::Transcode(node->getNodeName()) << std::endl;
 	delete after;
 #endif
 	PrintDocument(doc);
+
+#if 0
+	DOMElement *node = doc->createElement(XMLString::transcode("testing"));
+	doc->getDocumentElement()->appendChild(node);
+	doc->getDocumentElement()->removeChild(node);
+	node->release();
+	node->release();
+//	delete node;
+#endif
+
+	DOMElement *el;
+
+#if 0
+	std::cout << std::endl << std::endl;
+	el = FindElement("One",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+
+	std::cout << std::endl << std::endl;
+	el = FindElement("Two",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+
+	std::cout << std::endl << std::endl;
+	el = FindElement("Three",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+#else
+	std::cout << std::endl << std::endl;
+	el = FindElement("Three",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+
+	std::cout << std::endl << std::endl;
+	el = FindElement("Two",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+
+	std::cout << std::endl << std::endl;
+	el = FindElement("One",doc);
+	XMLNode::RemoveElementFromParent(el,true);
+	PrintDocument(doc);
+#endif
+
+	doc->release();
+//	delete doc;
 #endif
 
 #if 0
