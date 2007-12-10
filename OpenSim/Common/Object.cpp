5d2731b7ede51175489e4f9afcc3027487ed9dda
Ayman Habib <ahabib@stanford.edu>
2007-12-10 23:36:09 +0000

    Update OpenSim source copyright notice.
    git-svn-id: file:///dev/shm/opensim/Trunk@3915 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Common/Object.cpp
+++ OpenSim/Common/Object.cpp
@@ -1,7 +1,7 @@
 // Object.cpp
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /*
-* Copyright (c)  2005, Stanford University. All rights reserved. 
+* Copyright (c)   2005, Stanford University. All rights reserved. 
 * Use of the OpenSim software in source form is permitted provided that the following
 * conditions are met:
 * 	1. The software is used only for non-commercial research and education. It may not
