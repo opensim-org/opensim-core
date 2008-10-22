53b8be1c0af07582679c8ad1fdeadb3408c19b84
Ayman Habib <ahabib@stanford.edu>
2008-10-22 07:44:09 +0000

    remove dead folders from trunk.
    git-svn-id: file:///dev/shm/opensim/Trunk@4578 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Bin/cph.bat
+++
@@ -1,9 +0,0 @@
-echo Copying include files to %RDI_DEV%\IncludeDev
-cd %RDI_DEV%\Source
-for /r %%i in (*.h) do copy %%i %RDI_DEV%\IncludeDev
-exit(0)
-
-
-
-
-
--- OpenSim/Bin/cphq.bat
+++
@@ -1,8 +0,0 @@
-echo Copying include files to %RDI_DEV%\IncludeDev
-call %RDI_INSTALL%\Bin\cph > cph.log
-exit(0)
-
-
-
-
-
