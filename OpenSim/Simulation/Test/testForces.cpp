667389571ddfefbd631ff180fa450d337f037f9b
Ajay Seth <aseth@stanford.edu>
2011-05-03 23:16:08 +0000

    removed CoupledBushingForce from testForces since it was meant to be a plug-in example and not part of the simulation library.
    git-svn-id: file:///dev/shm/opensim/Trunk@6257 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Simulation/Test/testForces.cpp
+++ OpenSim/Simulation/Test/testForces.cpp
@@ -34,7 +34,7 @@
 //		3. ElasticFoundationForce
 //		4. HuntCrossleyForce
 //		5. CoordinateLimitForce
-//		6. CoupledBushingForce
+//		6. ExternalForce
 //		
 //     Add tests here as Forces are added to OpenSim
 //
@@ -42,7 +42,6 @@
 #include <OpenSim/OpenSim.h>
 #include <ctime>  // clock(), clock_t, CLOCKS_PER_SEC
 
-#include <OpenSim/Simulation/Model/CoupledBushingForce.h>
 #include <OpenSim/Simulation/Model/ExternalForce.h>
 
 using namespace OpenSim;
@@ -557,214 +556,6 @@ int testCoordinateLimitForce()
 	return 0;
 }
 
-int testCoupledBushingForce()
-{
-	double mass = 1;
-	double stiffness = 10;
-	double restlength = 0.0;
-	double h0 = 0;
-	double start_h = 0.5;
-	double ball_radius = 0.25;
-
-	double omega = sqrt(stiffness/mass);
-
-	double dh = mass*gravity_vec(1)/stiffness;
-
-	// Setup OpenSim model
-	Model *osimModel = new Model;
-	osimModel->setName("CoupledBushingTest");
-	//OpenSim bodies
-    OpenSim::Body& ground = osimModel->getGroundBody();
-	OpenSim::Body ball("ball", mass ,Vec3(0),  mass*SimTK::Inertia::sphere(0.1));
-	ball.addDisplayGeometry("sphere.vtp");
-	ball.scale(Vec3(ball_radius), false);
-
-	// Add joints
-	SliderJoint slider("", ground, Vec3(0), Vec3(0,0,Pi/2), ball, Vec3(0), Vec3(0,0,Pi/2));
-
-	double positionRange[2] = {-10, 10};
-	// Rename coordinates for a slider joint
-	CoordinateSet &slider_coords = slider.getCoordinateSet();
-	slider_coords[0].setName("ball_h");
-	slider_coords[0].setRange(positionRange);
-	slider_coords[0].setMotionType(Coordinate::Translational);
-
-	osimModel->addBody(&ball);
-
-	// BAD: have to set memoryOwner to false or program will crash when this test is complete.
-	osimModel->updBodySet().setMemoryOwner(false);
-
-	Mat66 stiffnessMat(0);
-	stiffnessMat(3,3) = stiffness; stiffnessMat(4,4) = stiffness; stiffnessMat(5,5) = stiffness;
-	Mat66 dampingMat(0);
-
-	osimModel->setGravity(gravity_vec);
-
-	CoupledBushingForce spring("ground", Vec3(0), Vec3(0), "ball", Vec3(0), Vec3(0), stiffnessMat, dampingMat);
-
-	osimModel->addForce(&spring);
-
-	osimModel->print("CoupledBushingForceModel.osim");
-
-	// Create the force reporter
-	ForceReporter* reporter = new ForceReporter(osimModel);
-	osimModel->addAnalysis(reporter);
-
-	SimTK::State &osim_state = osimModel->initSystem();
-
-	slider_coords[0].setValue(osim_state, start_h);
-    osimModel->getMultibodySystem().realize(osim_state, Stage::Position );
-
-	//==========================================================================================================
-	// Compute the force and torque at the specified times.
-
-    RungeKuttaMersonIntegrator integrator(osimModel->getMultibodySystem() );
-	integrator.setAccuracy(1e-6);
-    Manager manager(*osimModel,  integrator);
-    manager.setInitialTime(0.0);
-
-	double final_t = 2.0;
-	double nsteps = 10;
-	double dt = final_t/nsteps;
-
-	for(int i = 1; i <=nsteps; i++){
-		manager.setFinalTime(dt*i);
-		manager.integrate(osim_state);
-		osimModel->getMultibodySystem().realize(osim_state, Stage::Acceleration);
-        Vec3 pos;
-        osimModel->updSimbodyEngine().getPosition(osim_state, ball, Vec3(0), pos);
-		
-		double height = (start_h-dh)*cos(omega*osim_state.getTime())+dh;
-		ASSERT_EQUAL(height, pos(1), 1e-4);
-
-		//Now check that the force reported by spring
-		Array<double> model_force = spring.getRecordValues(osim_state);
-
-		// get the forces applied to the ground and ball
-		double analytical_force = -stiffness*height;
-		// analytical force corresponds in direction to the force on the ball Y index = 7
-		ASSERT_EQUAL(analytical_force, model_force[7], 1e-4);
-
-		manager.setInitialTime(dt*i);
-	}
-
-	manager.getStateStorage().print("coupled_bushing_model_states.sto");
-
-	// Save the forces
-	reporter->getForceStorage().print("coupled_bushing_forces.mot");  
-
-	// Before exiting lets see if copying the spring works
-	CoupledBushingForce *copyOfSpring = (CoupledBushingForce *)spring.copy();
-
-	bool isEqual = (*copyOfSpring == spring);
-	//ASSERT(isEqual);
-
-	return 0;
-}
-
-int testCoupledBushingForceEnergy()
-{
-	//define a new model properties
-	double mass = 10.0;
-	double angRange[2] = {-Pi, Pi};
-	double posRange[2] = {-1, 1};
-	
-	// construct a new OpenSim model
-	Model model;
-	model.setName("CoupledBushingEnergyTest");
-	//OpenSim bodies
-    OpenSim::Body& ground = model.getGroundBody();
-	OpenSim::Body tower("tower", mass ,Vec3(0),  mass*SimTK::Inertia::brick(0.1, 1.0, 0.2));
-	tower.addDisplayGeometry("box.vtp");
-	tower.scale(Vec3(0.1, 1.0, 0.2));
-
-	// Add joint connecting the tower to the ground and associate joint to tower body
-	FreeJoint freeJoint("groundTower", ground, Vec3(0), Vec3(0), tower, Vec3(0, -0.5, 0), Vec3(0));
-	// Rename coordinates for the free joint
-	CoordinateSet &freeCoords = freeJoint.getCoordinateSet();
-	for(int i=0; i< freeCoords.getSize(); ++i){
-		if(freeCoords[i].getMotionType() == Coordinate::Translational){
-			freeCoords[i].setRange(posRange);
-		}
-		else{
-			freeCoords[i].setRange(angRange);
-		}	
-	}
-
-	// add the tower body to the model
-	model.addBody(&tower);
-
-	Random::Uniform diagStiffness(100, 5000);
-	Random::Uniform offDiagStiffness(-20, 20);
-
-	Mat66 stiffnessMat(0);
-	for(int i=0; i<6; ++i){
-		for(int j=i; j<6; ++j){
-			stiffnessMat(i,j) = i==j ? diagStiffness.getValue(): offDiagStiffness.getValue();
-			if(i==j)
-				continue;
-			stiffnessMat(j,i) = stiffnessMat(i,j);
-		}
-	}
-	Mat66 dampingMat(0);
-	Vec6 stiffnessVec = stiffnessMat.diag();
-	Vec3 transStiff = stiffnessVec.getSubVec<3>(3);
-	Vec3 rotStiff = stiffnessVec.getSubVec<3>(0);
-
-	model.setGravity(gravity_vec);
-
-	CoupledBushingForce spring("ground", Vec3(0), Vec3(0), "tower", Vec3(0, -0.5, 0), Vec3(0), stiffnessMat, dampingMat);
-	//BushingForce spring("ground", Vec3(0), Vec3(0), "tower", Vec3(0, -0.5, 0), Vec3(0), transStiff, rotStiff, Vec3(0), Vec3(0));
-
-	model.addForce(&spring);
-
-	model.print("CoupledBushingEnergyTest.osim");
-
-	model.disownAllComponents();
-
-	SimTK::State &s = model.initSystem();
-
-	Random::Uniform qValues(-0.1, 0.1);
-	Random::Uniform qdotValues(-1.0, 1.0);
-	for(int i=0; i< model.getNumCoordinates(); ++i){
-		freeCoords[i].setValue(s, qValues.getValue());
-		freeCoords[i].setSpeedValue(s, qdotValues.getValue());
-	}
-
-	model.getMultibodySystem().realize(s, SimTK::Stage::Velocity);
-	double ke = model.getMultibodySystem().calcKineticEnergy(s);
-	double pes = spring.computePotentialEnergy(s);
-	double peg = model.getGravityForce().calcPotentialEnergyContribution(s);
-	double E = ke+pes+peg;
-
-    RungeKuttaMersonIntegrator integrator(model.getMultibodySystem());
-	integrator.setAccuracy(1e-6);
-	integrator.setAbsoluteTolerance(1e-6);
-    Manager manager(model,  integrator);
-
-	// Specify the initial and final times of the simulation.
-	// In this case, the initial and final times are set based on
-	// the range of times over which the controls are available.
-	//Control *control;
-	manager.setInitialTime(0.0);
-	manager.setFinalTime(5.0);
-
-	manager.integrate(s);
-
-	manager.getStateStorage().print("coupled_bushing_energy_model_states.sto");
-
-	model.getMultibodySystem().realize(s, SimTK::Stage::Velocity);
-	double ke_f = model.getMultibodySystem().calcKineticEnergy(s);
-	double pes_f = spring.computePotentialEnergy(s);
-	double peg_f = model.getGravityForce().calcPotentialEnergyContribution(s);
-	double E_f = ke_f+pes_f+peg_f;
-
-	ASSERT_EQUAL(E, E_f, 0.001);
-
-	return 0;
-}
-
-
 int testExternalForce()
 {
 	//define a new model properties
@@ -891,7 +682,7 @@ int testExternalForce()
 	
 	model.addForce(&xf3);
 	model.addForce(&xf4);
-	model.print("ExternalForceTest.osim");
+
 	// Everything allocated on the stack, so no need for model to own to free
 	model.disownAllComponents();
 
@@ -936,7 +727,6 @@ int testExternalForce()
 
 	model.addForce(&xf5);
 	model.addForce(&xf6);
-	model.print("ExternalForceTest4.osim");
 	// Everything allocated on the stack, so no need for model to own to free
 	model.disownAllComponents();
 
@@ -972,10 +762,7 @@ int testExternalForce()
 int main()
 {
 	testExternalForce();
-
-	testCoupledBushingForceEnergy();
-
-	testCoupledBushingForce();
+	cout << "external force passed"  << endl;
 
 	testSpringMass();
 	cout << "spring passed"  << endl;
