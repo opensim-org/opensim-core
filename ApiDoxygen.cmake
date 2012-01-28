72671d213aebc31042ce9062323aac32ca1d086a
Michael Sherman <sherm1@gmail.com>
2012-01-28 01:09:05 +0000

    Changed doxygen generation so that no generated files end up in the source tree. Now there is a "doxygen" target to build that puts html in the binary directory, and installation now proceeds from there.
    git-svn-id: file:///dev/shm/opensim/Trunk@6827 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- ApiDoxygen.cmake
+++ ApiDoxygen.cmake
@@ -8,13 +8,69 @@ ELSE(DOXYGEN_EXECUTABLE-NOTFOUND)
           ${DOXY_CONFIG}
           @ONLY )
 
-    ADD_CUSTOM_TARGET(doxygen ${DOXYGEN_EXECUTABLE} ${DOXY_CONFIG}) 
+    # The goal here is to run Doxygen to generate most of the API documentation
+    # under "html" in the binary directory, then apply a few hand-tweaked 
+    # hacks to it there.
+    # The result should be fully-functional documentation in the binary 
+    # directory (start with index.html) that can be examined while debugging 
+    # Doxygen comments. Then when we do an INSTALL later (see below), we just 
+    # need to copy over the binary html directory into sdk/doc.
+    # (sherm 20120127)
 
     FILE(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/html/")
+    FILE(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/html/images/")
+
+    ###############
+    # RUN DOXYGEN #
+    ###############
+    # Doxfile.in should take care of directing the output of Doxygen
+    # to ${PROJECT_BINARY_DIR}/html. Note that you have to invoke this
+    # "doxygen" target directly; it isn't run by default.
+    ADD_CUSTOM_TARGET(doxygen ${DOXYGEN_EXECUTABLE} ${DOXY_CONFIG}) 
+
+    ###################
+    # ADD LOCAL HACKS #
+    ###################
+    # OpenSim has its own main.html that should override the Doxygen-generated
+    # one (written by Sam Hamner and called "main_b").
+    FILE(TO_NATIVE_PATH
+        "${CMAKE_CURRENT_SOURCE_DIR}/OpenSim/doc/main_b.html"
+        mainb_source_file)
+    FILE(TO_NATIVE_PATH
+        "${PROJECT_BINARY_DIR}/html/main.html"
+        main_dest_file)
+
+    ADD_CUSTOM_COMMAND(TARGET doxygen POST_BUILD
+       COMMAND ${NATIVE_COPY_CMD} "${mainb_source_file}" "${main_dest_file}"
+       COMMENT "Replace Doxygen main.html with ours."
+       VERBATIM)
+
+    # There is an "images" directory containing the pictures needed by
+    # Sam's "main_b". Copy all the files into html/images.
+    FILE(GLOB ALL_IMAGES 
+         "${CMAKE_CURRENT_SOURCE_DIR}/OpenSim/doc/images/*.gif"
+         "${CMAKE_CURRENT_SOURCE_DIR}/OpenSim/doc/images/*.png")
+
+    FOREACH(image_pathname ${ALL_IMAGES})
+        # Retrieve just the filename (with extension).
+        GET_FILENAME_COMPONENT(image_name ${image_pathname} NAME)
+        FILE(TO_NATIVE_PATH "${image_pathname}" 
+            image_file_src)
+        FILE(TO_NATIVE_PATH "${PROJECT_BINARY_DIR}/html/images/${image_name}"
+            image_file_dest)
+        ADD_CUSTOM_COMMAND(TARGET doxygen POST_BUILD
+           COMMAND ${NATIVE_COPY_CMD} "${image_file_src}" "${image_file_dest}"
+           COMMENT "Copy ${image_file_src} -> ${image_file_dest}."
+           VERBATIM)
+    ENDFOREACH()
+
+    ################
+    # INSTALLATION #
+    ################
     INSTALL(DIRECTORY "${PROJECT_BINARY_DIR}/html/"
-            DESTINATION "${CMAKE_CURRENT_SOURCE_DIR}/OpenSim/doc/html"
+            DESTINATION "sdk/doc/html"
             )
     # This is just a shortcut to the Doxygen index.html.
-    #INSTALL(FILES "SimbodyAPI.html" DESTINATION "doc")
-ENDIF(DOXYGEN_EXECUTABLE-NOTFOUND)
+    INSTALL(FILES "OpenSimAPI.html" DESTINATION "sdk/doc")
 
+ENDIF(DOXYGEN_EXECUTABLE-NOTFOUND)
