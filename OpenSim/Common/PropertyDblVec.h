6e27badd02a309c7e332bfbb99903db158ed4d72
Michael Sherman <sherm1@gmail.com>
2012-05-03 23:10:29 +0000

    Fixed old school PropertyDblVec_<M> so that writing through the value reference actually does change the value!
    git-svn-id: file:///dev/shm/opensim/Trunk@7141 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Common/PropertyDblVec.h
+++ OpenSim/Common/PropertyDblVec.h
@@ -45,8 +45,7 @@
  * vector of doubles (i.e., SimTK::Vec<M>) and the methods for accessing
  * and modifying this Vec<M>.
  *
- * @version 1.0
- * @author Ayman Habib, Ajay Seth
+ * @author Ayman Habib, Ajay Seth, Michael Sherman
  */
 namespace OpenSim { 
 
@@ -57,13 +56,9 @@ template<int M> class PropertyDblVec_ : public Property_Deprecated
 // DATA
 //=============================================================================
 private:
-	/** Vector of doubles. */
-	SimTK::Vec<M> _vec;
-
-	/** Array representation for serialization */
-	mutable Array<double> _array;
-
-	char _typeAsString[7];
+	// Store in an Array for serialization; we'll fake up the Vec<M> when
+    // we need it using the Array's storage.
+	Array<double> _dblvec;
 
 //=============================================================================
 // METHODS
@@ -74,110 +69,87 @@ private:
 public:
 	/** Default constructor */
 	PropertyDblVec_() : Property_Deprecated(DblVec, "DblVec_PropertyName")
-		{ sprintf(_typeAsString, "DblVec%i", M);
-		  setAllowableListSize(M);	  
-		};
+		{ setAllowableListSize(M);
+          _dblvec.setSize(M);
+		}
 	/** Construct from name and value */
 	PropertyDblVec_(const std::string &aName, const SimTK::Vec<M>& aVec)
     :   Property_Deprecated(DblVec, aName) 
-		{ sprintf(_typeAsString, "DblVec%i", M);  setValue(aVec);
-		  setAllowableListSize(M);
-
-		};
+		{ setAllowableListSize(M);
+          _dblvec.setSize(M);
+          setValue(aVec);
+		}
 	/** Construct from name and value as an Array<double> */
 	PropertyDblVec_(const std::string &aName, const Array<double> &anArray)
     :   Property_Deprecated(DblVec, aName)
-		{ sprintf(_typeAsString, "DblVec%i", M);  setValue(anArray);
-		  setAllowableListSize(M);
-		};
-	/** Copy constructor */
-	PropertyDblVec_(const PropertyDblVec_ &aProperty)
-    :   Property_Deprecated(aProperty)
-		{ setValue(aProperty._vec);};
+		{ setAllowableListSize(M);
+          _dblvec.setSize(M);    
+          setValue(anArray);
+		}
+	
+    // default destructor, copy constructor, copy assignment
 
 	/* Return a copy of this property */
-	/*virtual*/ PropertyDblVec_* clone() const {
+	PropertyDblVec_* clone() const OVERRIDE_11 {
 		PropertyDblVec_* prop = new PropertyDblVec_<M>(*this);
 		return prop;
-	};
+	}
 
-	//--------------------------------------------------------------------------
-	// OPERATORS
-	//--------------------------------------------------------------------------
 public:
-	/** Assign this property to another */
-	PropertyDblVec_& operator=(const PropertyDblVec_ &aProperty){
-		Property_Deprecated::operator=(aProperty);
-		_vec = aProperty._vec;
-		return(*this);
-	};
 
 	//--------------------------------------------------------------------------
 	// GET AND SET
 	//--------------------------------------------------------------------------
 public:
 	/** Get the type of this property as a string. */
-	virtual std::string getTypeName() const OVERRIDE_11 {
+	std::string getTypeName() const OVERRIDE_11 {
 		return "double";
-	};
+	}
 
 	// VALUE
 	/** set value of property from an equivalently sized Vec */
-	virtual void setValue(const SimTK::Vec<M> &aVec) { 
-		assert(aVec.size()==M); _vec=aVec; 
-	};
+	void setValue(const SimTK::Vec<M> &aVec) { 
+		SimTK::Vec<M>::updAs(&_dblvec[0])=aVec; 
+	}
 	/** set value of this property from an array of doubles of equal or greater length */
-	virtual void setValue(const Array<double> &anArray){
+	void setValue(const Array<double> &anArray) OVERRIDE_11 {
 		assert(anArray.getSize() >= M);
 		for(int i=0;i<M; i++)
-			_vec[i] = anArray[i];
-		_array = anArray;
-	};
+			_dblvec[i] = anArray[i];
+	}
 	/** get writable reference to the value */
-	virtual SimTK::Vec<M>& getValueDblVec() {return _vec; };
+	SimTK::Vec<M>& getValueDblVec() {return SimTK::Vec3::updAs(&_dblvec[0]); }
 	/** get const (read-only) reference to the value */
-	virtual const SimTK::Vec<M>& getValueDblVec() const {return _vec; };
-	/** set value from double array */
-	virtual void setValue(int aSize, const double aArray[]){ // to be used by the serialization code
+	const SimTK::Vec<M>& getValueDblVec() const {return SimTK::Vec3::getAs(&_dblvec[0]); };
+	/** set value from double array */ // to be used by the serialization code
+	void setValue(int aSize, const double aArray[]) OVERRIDE_11 { 
 		assert(aSize == M);
 		setValue(SimTK::Vec<M>::getAs(aArray));
 	};
 #ifndef SWIG
 	/** get value as double array */
-	virtual const Array<double>& getValueDblArray() const {
-		_array.setSize(M);
-		for(int i=0;i<M; i++)
-			_array[i] = _vec[i];
-		return _array;
-	};
+    const Array<double>& getValueDblArray() const OVERRIDE_11 {return _dblvec;}
 #endif
-	/** Nonconst version of accessor for use by GUI.
-	 * Beware: changes to returned array will not change _vec member as it should
-	 * both _vec and _array should point to same memory to fix this issue
-	 */
-	virtual Array<double>& getValueDblArray() {
-		_array.setSize(M);
-		for(int i=0;i<M; i++)
-			_array[i] = _vec[i];
-		return _array;
-	};
+	/** Nonconst version of accessor for use by GUI. */
+    Array<double>& getValueDblArray() OVERRIDE_11 {return _dblvec;}
+
 	/** Get a constant String represeting the value of this property. */
-	virtual std::string toString() const {
+	std::string toString()  const OVERRIDE_11 {
 		std::string str = "(";
 		char dbl[256];
-			for(int i=0; i < _vec.size(); i++){
-				sprintf(dbl, "%g", _vec[i]);
+			for(int i=0; i < M; i++){
+				sprintf(dbl, "%g", _dblvec[i]);
 				str += (i>0?" ":"") + std::string(dbl);
 			}
 		str += ")";
 		return str;
 	};
 	// SIZE
-	virtual int getArraySize() const { return M; }
+	int getArraySize() const OVERRIDE_11 { return M; }
 
-    virtual bool isArrayProperty() const {return true;}
+    bool isArrayProperty() const OVERRIDE_11 {return true;}
 
-    virtual int getNumValues() const OVERRIDE_11 {return M;}
+    int getNumValues() const OVERRIDE_11 {return M;}
 //=============================================================================
 };	// END of class PropertyDblVec_
 
