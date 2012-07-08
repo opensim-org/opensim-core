04ac0bbb1f1e08264be7ef845a03756bf1eccb3f
Matthew Millard <millard.matthew@gmail.com>
2012-07-08 16:23:20 +0000

    A probe to get the active fiber power of a concrete class derived from Muscle.
    git-svn-id: file:///dev/shm/opensim/Trunk@7337 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ OpenSim/Simulation/Model/MuscleActiveFiberPowerProbe.cpp
@@ -0,0 +1,150 @@
+// MuscleActiveFiberPowerProbe.cpp
+// Author: Matt Millard
+/*  
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
+ * THE AUTHORS, CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,    *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      *
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  *
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
+ * -------------------------------------------------------------------------- */
+//=============================================================================
+// INCLUDES and STATICS
+//=============================================================================
+#include "MuscleActiveFiberPowerProbe.h"
+
+
+using namespace std;
+using namespace SimTK;
+using namespace OpenSim;
+
+
+//=============================================================================
+// CONSTRUCTOR(S) AND SETUP
+//=============================================================================
+//_____________________________________________________________________________
+// Default constructor.
+MuscleActiveFiberPowerProbe::MuscleActiveFiberPowerProbe() 
+{
+    setNull();
+    constructProperties();
+}
+
+//_____________________________________________________________________________
+// Convenience constructor.
+MuscleActiveFiberPowerProbe::
+MuscleActiveFiberPowerProbe(const Array<string> muscle_names)
+{
+    setNull();
+    constructProperties();
+    set_muscle_names(muscle_names);
+}
+
+
+//_____________________________________________________________________________
+// Set the data members of this MuscleActiveFiberPowerProbe to their null values.
+void MuscleActiveFiberPowerProbe::setNull(void)
+{
+    // No data members.
+}
+
+//_____________________________________________________________________________
+// Connect properties to local pointers.
+void MuscleActiveFiberPowerProbe::constructProperties(void)
+{
+    constructProperty_muscle_names();
+}
+
+//=============================================================================
+// GET AND SET
+//=============================================================================
+//_____________________________________________________________________________
+/**
+ * Returns the names of the Actuators being probed.
+ */
+const Property<string>& MuscleActiveFiberPowerProbe::getMuscleNames() const
+{
+    return getProperty_muscle_names();
+}
+
+//_____________________________________________________________________________
+/**
+ * Sets the names of the Actuators being probed.
+ */
+void MuscleActiveFiberPowerProbe::setMuscleNames(const Array<string>& muscleNames)
+{
+    set_muscle_names(muscleNames);
+}
+
+
+
+
+//=============================================================================
+// MODEL COMPONENT METHODS
+//=============================================================================
+//_____________________________________________________________________________
+/**
+ * Perform some set up functions that happen after the
+ * object has been deserialized or copied.
+ *
+ * @param aModel OpenSim model containing this MuscleActiveFiberPowerProbe.
+ */
+void MuscleActiveFiberPowerProbe::connectToModel(Model& model)
+{
+    Super::connectToModel(model);
+
+    // check that each Muscle in the actuator_names array exists in the model
+    int nA = getMuscleNames().size();
+    for (int i=0; i<nA; i++) {
+        string actName = getMuscleNames()[i];
+        int k = model.getMuscles().getIndex(actName);
+        if (k<0) {
+            string errorMessage = getConcreteClassName() + ": Invalid Muscle '" + actName + "' specified in <muscle_names>.";
+            throw (Exception(errorMessage.c_str()));
+        }
+    }
+}
+
+
+
+
+
+//=============================================================================
+// COMPUTATION
+//=============================================================================
+//_____________________________________________________________________________
+/**
+Compute the muscle fiberpower upon which the Probe operation will be based on.
+ */
+double MuscleActiveFiberPowerProbe::computeProbeValue(const State& s) const
+{
+    int nA = getMuscleNames().size();
+    double TotalP = 0;			// Initialize at zero
+
+    // Loop through each muscle in the list of actuator_names
+    for (int i=0; i<nA; i++)
+    {
+        string actName = getMuscleNames()[i];
+        int k = _model->getMuscles().getIndex(actName);
+
+        // Get the fiber power from the Actuator object
+        double fiberPower = _model->getMuscles().get(k).getFiberActivePower(s);
+        
+        // Append to total "Actuator" power
+        TotalP += fiberPower;
+    }
+
+    return(TotalP);
+}
+
---
+++ OpenSim/Simulation/Model/MuscleActiveFiberPowerProbe.h
@@ -0,0 +1,114 @@
+#ifndef OPENSIM_ACTIVE_FIBER_POWER_PROBE_H_
+#define OPENSIM_ACTIVE_FIBER_POWER_PROBE_H_
+
+// MuscleActiveFiberPowerProbe.h
+// Author: Matt Millard
+/* 
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
+ * THE AUTHORS, CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,    *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      *
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  *
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
+ * -------------------------------------------------------------------------- */
+
+
+// INCLUDE
+#include "Muscle.h"
+#include "Probe.h"
+#include "Model.h"
+
+
+namespace OpenSim {
+
+class Model;
+
+//===============================================================================
+//                         ACTIVE FIBER POWER PROBE
+//===============================================================================
+/**
+MuscleActiveFiberPowerProbe is a ModelComponent probe for computing an 
+operation on the active fiber power of a muscle.  
+  
+  @author Matt Millard
+ */
+class OSIMSIMULATION_API MuscleActiveFiberPowerProbe : public Probe {
+OpenSim_DECLARE_CONCRETE_OBJECT(MuscleActiveFiberPowerProbe, Probe);
+
+public:
+//==============================================================================
+// PROPERTIES
+//==============================================================================
+    /** @name Property declarations
+    These are the serializable properties associated with this class. **/
+    /**@{**/
+    /** List of Muscles to probe.  **/
+    OpenSim_DECLARE_LIST_PROPERTY(muscle_names, std::string,
+        "Specify a list of muscles whose work should be calculated. "
+        "If multiple muscles are given, the probe value will be the summation"
+        " of all actuator powers.");
+
+
+//==============================================================================
+// PUBLIC METHODS
+//==============================================================================
+    //--------------------------------------------------------------------------
+    // Constructor(s) and Setup
+    //--------------------------------------------------------------------------
+    /** Default constructor */
+    MuscleActiveFiberPowerProbe();
+    /** Convenience constructor */
+    MuscleActiveFiberPowerProbe(const Array<std::string> muscle_names);
+
+    // Uses default (compiler-generated) destructor, copy constructor, and copy
+    // assignment operator.
+
+    //--------------------------------------------------------------------------
+    // Get and Set
+    //--------------------------------------------------------------------------
+    /** Returns the names of the Actuators being probed. */
+    const Property<std::string>& getMuscleNames() const;
+
+    /** Sets the names of the Actuators being probed. */
+    void setMuscleNames(const Array<std::string>& muscleNames);
+
+    //--------------------------------------------------------------------------
+    // Computation
+    //--------------------------------------------------------------------------
+    /** Compute the Actuator power upon which the Probe operation will be based. */
+    double computeProbeValue(const SimTK::State& state) const OVERRIDE_11;
+
+
+//==============================================================================
+// PRIVATE
+//==============================================================================
+private:
+    //--------------------------------------------------------------------------
+    // ModelComponent Interface
+    //--------------------------------------------------------------------------
+    void connectToModel(Model& aModel) OVERRIDE_11;
+    
+    void setNull();
+    void constructProperties();
+
+//==============================================================================
+};	// END of class MuscleActiveFiberPowerProbe
+//==============================================================================
+//==============================================================================
+
+} // end of namespace OpenSim
+
+#endif // OPENSIM_ACTUATOR_POWER_PROBE_H_
+
+
