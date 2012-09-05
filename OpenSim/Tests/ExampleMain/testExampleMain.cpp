8c1a81b422eba54baf4efcc91568345dd2fbe3c4
Kevin Xu <kevunix@gmail.com>
2012-09-05 00:46:30 +0000

    API Examples: Separating test files with API examples. Moving test files to OpenSim/Test. CMake files also modified in Test. The tests should now reappear in nightly build
    git-svn-id: file:///dev/shm/opensim/Trunk@7556 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ OpenSim/Tests/ExampleMain/testExampleMain.cpp
@@ -0,0 +1,52 @@
+/* -------------------------------------------------------------------------- *
+ *                       OpenSim:  testExampleMain.cpp                        *
+ * -------------------------------------------------------------------------- *
+ * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
+ * See http://opensim.stanford.edu and the NOTICE file for more information.  *
+ * OpenSim is developed at Stanford University and supported by the US        *
+ * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
+ * through the Warrior Web program.                                           *
+ *                                                                            *
+ * Copyright (c) 2005-2012 Stanford University and the Authors                *
+ * Author(s): Cassidy Kelly                                                   *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.         *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied    *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
+
+// Author: Cassidy Kelly
+
+//==============================================================================
+//==============================================================================
+
+#include <OpenSim/OpenSim.h>
+#include <OpenSim/Auxiliary/auxiliaryTestFunctions.h>
+
+using namespace OpenSim;
+using namespace std;
+
+int main()
+{
+	try {
+		Storage result1("tugOfWar_states.sto"), standard1("std_tugOfWar_states.sto");
+		CHECK_STORAGE_AGAINST_STANDARD(result1, standard1, Array<double>(0.1, 24), __FILE__, __LINE__, "tugOfWar states failed");
+		cout << "tugOfWar states passed\n";
+
+		Storage result3("tugOfWar_forces.mot"), standard3("std_tugOfWar_forces.mot");
+		CHECK_STORAGE_AGAINST_STANDARD(result3, standard3, Array<double>(1.0, 24), __FILE__, __LINE__, "tugOfWar forces failed");
+		cout << "tugOfWar forces passed\n";
+	}
+	catch (const Exception& e) {
+        e.print(cerr);
+        return 1;
+    }
+    cout << "Done" << endl;
+    return 0;
+}
