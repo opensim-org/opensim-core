5e63e3b86623aed099383fa96c7ee079fdce8905
Christopher Dembia <cld72@cornell.edu>
2013-09-19 23:13:17 +0000

    Update to setup.py so that generated SWIG library is copied over to python installation appropriately. Somehow, I did not include this change in my previous commit.
    git-svn-id: file:///dev/shm/opensim/Trunk@8476 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- OpenSim/Wrapping/Python/setup.py
+++ OpenSim/Wrapping/Python/setup.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
 
-setup(name='pyOpenSim',
+from setuptools import setup, find_packages
+
+if os.name == 'posix':
+    # Linux, etc.
+    lib_name = '_opensim.so'
+elif os.name == 'nt':
+    # Windows.
+    lib_name = '_opensim.pyd'
+
+setup(name='opensim',
       version='3.1',
       description='OpenSim Simulation Framework',
       author='OpenSim Team',
       author_email='ahabib@stanford.edu',
-      url='http://opensim.stanford.edu/'
-     )
+      url='http://opensim.stanford.edu/',
+      license='Apache 2.0',
+      packages=find_packages(),
+      data_files=[('opensim', ['opensim/%s' % lib_name])],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Physics',
+          ],
+      )
