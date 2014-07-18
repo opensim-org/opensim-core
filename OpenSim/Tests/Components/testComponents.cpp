f7289ae32817c793b4edf05ac57a27df471723af
Tom Uchida <tom.uchida@gmail.com>
2014-07-17 18:13:52 -0700

    Report memory leaks even if they fall below 2% tolerance.

--- OpenSim/Tests/Components/testComponents.cpp
+++ OpenSim/Tests/Components/testComponents.cpp
@@ -30,6 +30,8 @@ using namespace OpenSim;
 using namespace std;
 
 static Model dummyModel;
+const double acceptableMemoryLeakPercent = 2.0;
+const bool displayMemoryInfo = true;
 
 template <typename T>
 void testComponent(const T& instanceToTest);
@@ -239,11 +241,15 @@ void testModelComponent(const T& instanceToTest, bool randomizePropertyValues,
         const int64_t increaseInMemory = getCurrentRSS() - initMemory;
         const long double leakPercent = 100.0 * increaseInMemory / initMemory;
 
-        ASSERT(leakPercent < 2.0, __FILE__, __LINE__,
+        ASSERT(leakPercent < acceptableMemoryLeakPercent, __FILE__, __LINE__,
                 "testComponents: memory leak greater than 2%. Initial memory: " +
                 to_string(initMemory/1024) + " KB, increase in memory: " +
                 to_string(increaseInMemory/1024) + " KB, " +
                 to_string(leakPercent) + "%.");
+
+        if (displayMemoryInfo && leakPercent>0)
+            std::cout << "\t--> memory increased by "
+                      << setprecision(3) << leakPercent << "%" << std::endl;
     }
 
     // 11. Test that repeated calls to initSystem do not change test results,
@@ -265,17 +271,14 @@ void testModelComponent(const T& instanceToTest, bool randomizePropertyValues,
                 1e-7, __FILE__, __LINE__, "testComponents: initial state "
                     "differs after repeated calls to initSystem().");
 
-        ASSERT(leakPercent < 2.0, __FILE__, __LINE__,
+        ASSERT(leakPercent < acceptableMemoryLeakPercent, __FILE__, __LINE__,
                 "testComponents: memory leak greater than 2%. Initial memory: " +
                 to_string(initMemory/1024) + " KB, increase in memory: " +
                 to_string(increaseInMemory/1024) + " KB, " +
                 to_string(leakPercent) + "%.");
+
+        if (displayMemoryInfo && leakPercent>0)
+            std::cout << "\t-->memory increased by "
+                      << setprecision(3) << leakPercent << "%" << std::endl;
     }
 }
-
-
-
-
-
-
-
