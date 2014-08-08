c79a3088fa79fe4c664168898a052333c60e0c2e
Soha Pouya <soha.pouya@gmail.com>
2014-08-08 10:30:27 -0700

    Made testMuscles back to its original version, using Probes. (Changes postponed to when eliminating probes)

--- OpenSim/Actuators/Test/testMuscles.cpp
+++ OpenSim/Actuators/Test/testMuscles.cpp
@@ -324,15 +324,15 @@ void simulateMuscle(
 //==========================================================================
 
 	// Add an ActuatorPowerProbe to measure the work done by the muscle actuator 
-    /*ActuatorPowerProbe * muscWorkProbe = new ActuatorPowerProbe(muscNames, true, 1);
+    ActuatorPowerProbe * muscWorkProbe = new ActuatorPowerProbe(muscNames, true, 1);
 	muscWorkProbe->setOperation("integrate");
-	model.addProbe(muscWorkProbe);*/
+	model.addProbe(muscWorkProbe);
 
 	// Add a JointInternalPowerProbe to measure the work done by the joint
 	// will be 0 unless joint has prescribed motion
-    /*JointInternalPowerProbe * jointWorkProbe = new JointInternalPowerProbe(jointNames, true, 1);
+    JointInternalPowerProbe * jointWorkProbe = new JointInternalPowerProbe(jointNames, true, 1);
 	jointWorkProbe->setOperation("integrate");
-	model.addProbe(jointWorkProbe);*/
+	model.addProbe(jointWorkProbe);
 
 	/* Since all components are allocated on the stack don't have model 
        own them (and try to free)*/
@@ -394,12 +394,12 @@ void simulateMuscle(
 
 	model.getMultibodySystem().realize(si, SimTK::Stage::Acceleration);
 
-	/*double Emuscle0 = muscWorkProbe->getProbeOutputs(si)(0);
+	double Emuscle0 = muscWorkProbe->getProbeOutputs(si)(0);
 	//cout << "Muscle initial energy = " << Emuscle0 << endl;
 	double Esys0 = model.getMultibodySystem().calcEnergy(si);
 	Esys0 += (Emuscle0 + jointWorkProbe->getProbeOutputs(si)(0));
 	double PEsys0 = model.getMultibodySystem().calcPotentialEnergy(si);
-	//cout << "Total initial system energy = " << Esys0 << endl; */
+	//cout << "Total initial system energy = " << Esys0 << endl; 
 
 //==========================================================================
 // 4. SIMULATION Integration
@@ -455,8 +455,8 @@ void simulateMuscle(
 	    muscleAnalysis->printResults(actuatorType, "testMuscleResults");
     }
 
-	/*double muscleWork = muscWorkProbe->getProbeOutputs(si)(0);
-	cout << "Muscle work = " << muscleWork << endl;*/
+	double muscleWork = muscWorkProbe->getProbeOutputs(si)(0);
+	cout << "Muscle work = " << muscleWork << endl;
 
 //==========================================================================
 // 6. SIMULATION Tests
@@ -499,7 +499,7 @@ void simulateMuscle(
 	    double xSpeed = modelCoordinateSet[0].getSpeedValue(si);
 	    double KEsysCheck =  0.5*ballMass*xSpeed*xSpeed;
 	    double PEsys =  model.getMultibodySystem().calcPotentialEnergy(si);
-        /*double jointWork = jointWorkProbe->computeProbeInputs(si)(0);
+        double jointWork = jointWorkProbe->computeProbeInputs(si)(0);
 	    double ESysMinusWork = Esys 
                                 - muscWorkProbe->computeProbeInputs(si)(0)
                                 - jointWork; 
@@ -511,7 +511,7 @@ void simulateMuscle(
         cout << "Esys - Work = " << ESysMinusWork 
              << " :: Esys0 = " << Esys0 << endl; 
 	    ASSERT_EQUAL(ESysMinusWork, Esys0, testTolerance, __FILE__, __LINE__, 
-	    	"testMuscles: System energy-work -not conserved.");*/
+	    	"testMuscles: System energy-work -not conserved.");
 	    	    
 	    //Minimum requirement to pass is simulation of single 
         //muscle on slider is real-time
