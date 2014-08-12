3dbbbd58d37b26092d1f9b53890d451da2eef3bf
Ajay Seth <aseth@stanford.edu>
2014-08-12 14:06:26 -0700

    minor formatting

--- OpenSim/Actuators/BodyActuator.h
+++ OpenSim/Actuators/BodyActuator.h
@@ -1,7 +1,7 @@
 #ifndef OPENSIM_BODY_ACTUATOR_H_
 #define OPENSIM_BODY_ACTUATOR_H_
-/* -------------------------------------------------------------------------- *
-*                         OpenSim:  BodyActuator.h                          *
+/* ------------------------------------------------------------------------- *
+*                         OpenSim:  BodyActuator.h                           *
 * -------------------------------------------------------------------------- *
 * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
 * See http://opensim.stanford.edu and the NOTICE file for more information.  *
@@ -28,76 +28,75 @@
 
 namespace OpenSim {
 
-	class Body;
-	class Model;
+class Body;
+class Model;
+
+//==============================================================================
+//                              BODY ACTUATOR
+//==============================================================================
+/**
+ * Apply a spatial force (that is, force and torque) on the origin of the given
+ * body. That is, the force is applied at the origin; torques don't have
+ * associated points. This actuator has no states; the control is simply the
+ * force/torque to be applied to the model and is passed through directly.
+ * The associated Controller is expected to generate forces/torques in the body
+ * frame.
+ *
+ * @author Soha Pouya, Michael Sherman
+ */
+class OSIMACTUATORS_API BodyActuator : public Actuator {
+	OpenSim_DECLARE_CONCRETE_OBJECT(BodyActuator, Actuator);
+public:
+	//==============================================================================
+	// PROPERTIES
+	//==============================================================================
+	/** @name Property declarations
+        There are no serializable properties associated with this class (except
+        inherited ones). **/
+    /**@{**/
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(body, std::string,
+        "Name of Body to which this actuator is applied.");
+    /**@}**/
 
 	//==============================================================================
-	//                              BODY ACTUATOR
+	// PUBLIC METHODS
 	//==============================================================================
-	/**
-	* Apply a spatial force (that is, force and torque) on the origin of the given
-	* body. That is, the force is applied at the origin; torques don't have
-	* associated points. This actuator has no states; the control is simply the
-	* force/torque to be applied to the model and is passed through directly.
-	* The associated Controller is expected to generate forces/torques in the body
-	* frame.
-	*
-	* @author Soha Pouya, Michael Sherman
-	*/
-	class OSIMACTUATORS_API BodyActuator : public Actuator {
-		OpenSim_DECLARE_CONCRETE_OBJECT(BodyActuator, Actuator);
-	public:
-		//==============================================================================
-		// PROPERTIES
-		//==============================================================================
-		/** @name Property declarations
-		There are no serializable properties associated with this class (except
-		inherited ones). **/
-		/**@{**/
-		OpenSim_DECLARE_OPTIONAL_PROPERTY(body, std::string,
-			"Name of Body to which this actuator is applied.");
-		/**@}**/
+	/** Default constructor or construct with body name given. An empty
+	name ("") is treated as though it were unspecified. **/
+	explicit BodyActuator(const std::string& bodyName = "");
 
-		//==============================================================================
-		// PUBLIC METHODS
-		//==============================================================================
-		/** Default constructor or construct with body name given. An empty
-		name ("") is treated as though it were unspecified. **/
-		explicit BodyActuator(const std::string& bodyName = "");
+	// Uses default (compiler-generated) destructor, copy constructor, copy 
+	// assignment operator.
 
-		// Uses default (compiler-generated) destructor, copy constructor, copy 
-		// assignment operator.
+private:
+	//--------------------------------------------------------------------------
+	// Implement Component interface
+	//--------------------------------------------------------------------------
+	void constructProperties();
+	void constructStructuralConnectors() override;
 
-	private:
-		void constructProperties();
-		//--------------------------------------------------------------------------
-		// Implement Component interface
-		//--------------------------------------------------------------------------
-		void constructStructuralConnectors() override;
+	//--------------------------------------------------------------------------
+	// Implement ModelComponent interface
+	//--------------------------------------------------------------------------
+	// Setup method to initialize Body reference
+	void connectToModel(Model& model) override;
 
-		//--------------------------------------------------------------------------
-		// Implement Force interface
-		//--------------------------------------------------------------------------
-		void computeForce(const SimTK::State& state,
-			SimTK::Vector_<SimTK::SpatialVec>& bodyForces,
-			SimTK::Vector& mobilityForces) const override;
+	//--------------------------------------------------------------------------
+	// Implement Force interface
+	//--------------------------------------------------------------------------
+	void computeForce(const SimTK::State& state,
+	SimTK::Vector_<SimTK::SpatialVec>& bodyForces,
+	SimTK::Vector& mobilityForces) const override;
 
-		//--------------------------------------------------------------------------
-		// Implement Actuator interface.
-		//--------------------------------------------------------------------------
-		int numControls() const override { return 6; }
+	//--------------------------------------------------------------------------
+	// Implement Actuator interface.
+	//--------------------------------------------------------------------------
+	int numControls() const override { return 6; }
+	double getPower(const SimTK::State& s) const { return 0; }
+	void overrideForce(SimTK::State& s, bool flag) const {}
 
-		double getPower(const SimTK::State& s) const { return 0; }
-		void overrideForce(SimTK::State& s, bool flag) const {}
-
-		//--------------------------------------------------------------------------
-		// Implement ModelComponent interface
-		//--------------------------------------------------------------------------
-		// Setup method to initialize Body reference
-		void connectToModel(Model& model) override;
-
-		//=============================================================================
-	};	// END of class BodyActuator
+	//=============================================================================
+};	// END of class BodyActuator
 
 }; //namespace
 //=============================================================================
