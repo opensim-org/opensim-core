b6305a31d6be1ed1dbedde305cf465e2e68e8c19
Christopher Dembia <cld72@cornell.edu>
2015-05-22 19:51:07 -0700

    Version number is only mentioned in one place.
    The OpenSim version number was repeated in a few different places. Now it only
    appears in CMakeLists.txt.
    
    There are some methods in version.h that relied on preprocessor definitions.
    However, these definitions were only in place for files within OpenSim/. That
    means if an exectuable called one of the functions in version.h, they would get
    the version number to be "GET_OSIM_VERSION". So I moved the definitions to the
    root CMakeLists.txt.
    
    version.h is not distributed with OpenSim, so clients of OpenSim cannot call
    those functions. It would be good if version.h was moved into OpenSim/Common so
    that our users could check the version of OpenSim they're using. However, that
    is beyond the scope of what I was trying to achieve here.

--- OpenSim/OpenSimDoxygenMain.h
+++ OpenSim/OpenSimDoxygenMain.h
@@ -9,7 +9,7 @@
  * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
  * through the Warrior Web program.                                           *
  *                                                                            *
- * Copyright (c) 2005-2012 Stanford University and the Authors                *                                                *
+ * Copyright (c) 2005-2012 Stanford University and the Authors                *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
@@ -28,7 +28,7 @@ Mainpage, the first page that a user sees when entering the Doxygen-
 generated API documentation. This is not actually included as part of the
 OpenSim source and it is not installed with OpenSim. **/
 
-/** @mainpage  OpenSim 3.2 Documentation
+/** @mainpage Overview
 
 \htmlonly
 <!-- ImageReady Slices -->
