8e76db88b787f6a88e73a8311c1cb15a8aa983ac
Christopher Dembia <cld72@cornell.edu>
2015-05-23 19:52:46 -0700

    Update copyright year

--- OpenSim/OpenSimDoxygenMain.h
+++ OpenSim/OpenSimDoxygenMain.h
@@ -9,7 +9,7 @@
  * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
  * through the Warrior Web program.                                           *
  *                                                                            *
- * Copyright (c) 2005-2012 Stanford University and the Authors                *
+ * Copyright (c) 2005-2015 Stanford University and the Authors                *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
