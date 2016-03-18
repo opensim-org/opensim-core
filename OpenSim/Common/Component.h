9248c5bb5b570860c8ef09b98b6fc51eaeb5839a
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-03-18 14:28:52 -0700

    No need to name the captured exception when unused.

--- OpenSim/Common/Component.h
+++ OpenSim/Common/Component.h
@@ -2373,7 +2373,7 @@ void Connector<C>::findAndConnect(const Component& root) {
             comp =  &getOwner().template getComponent<C>(path);
         }
     }
-    catch (const ComponentNotFoundOnSpecifiedPath& ex) {
+    catch (const ComponentNotFoundOnSpecifiedPath&) {
         // TODO leave out for hackathon std::cout << ex.getMessage() << std::endl;
         comp =  root.template findComponent<C>(path);
     }
