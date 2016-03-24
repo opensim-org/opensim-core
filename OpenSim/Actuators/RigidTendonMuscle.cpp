1f95b34a1c1d08b976ce096b8c787ec6bd9f5757
Shrinidhi KL <klshrinidhi@gmail.com>
2016-03-24 16:51:02 -0700

    Unused variable.

--- OpenSim/Actuators/RigidTendonMuscle.cpp
+++ OpenSim/Actuators/RigidTendonMuscle.cpp
@@ -37,7 +37,7 @@ using namespace OpenSim;
 using SimTK::Vec3; using SimTK::square; using SimTK::Eps; using SimTK::State;
 using SimTK::Vector;
 
-static int counter=0;
+// static int counter=0;
 //==============================================================================
 // CONSTRUCTOR
 //==============================================================================
@@ -155,7 +155,7 @@ void RigidTendonMuscle::calcMusclePotentialEnergyInfo(const SimTK::State& s,
     normalized velocities, pennation angular velocity, etc... */
 void RigidTendonMuscle::calcFiberVelocityInfo(const State& s, FiberVelocityInfo& fvi) const
 {
-    const MuscleLengthInfo &mli = getMuscleLengthInfo(s);
+    // const MuscleLengthInfo &mli = getMuscleLengthInfo(s);
     fvi.fiberVelocity = getGeometryPath().getLengtheningSpeed(s);
     fvi.normFiberVelocity = fvi.fiberVelocity / 
                             (getOptimalFiberLength()*getMaxContractionVelocity());
