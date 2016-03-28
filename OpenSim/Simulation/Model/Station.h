5d41e04d5cc7dda826bddd3f129c66ee07d04d73
James Dunne <jimmy@DN0a22f203.SUNet>
2016-03-28 13:54:06 -0700

    fixed indent

--- OpenSim/Simulation/Model/Station.h
+++ OpenSim/Simulation/Model/Station.h
@@ -31,64 +31,63 @@
 #include "OpenSim/Simulation/Model/PhysicalFrame.h"
 
 namespace OpenSim {
-    
-    class Body;
-    
-    //=============================================================================
-    //=============================================================================
-    /**
-     * A class implementing a Station. A Station is a point fixed to and defined with
-     * respect to a reference frame. The reference frame can be a Body (fixed
-     * to origin of a Body), a FixedFrame (affixed to another PhysicalFrame) or any
-     * other PhysicalFrame. The main functionality provided by Station is to find its
-     * location in any Frame.
-     *
-     * @author Ayman Habib
-     * @version 1.0
-     */
-    class OSIMSIMULATION_API Station : public ModelComponent {
-        OpenSim_DECLARE_CONCRETE_OBJECT(Station, ModelComponent);
-    public:
-        //==============================================================================
-        // PROPERTIES
-        //==============================================================================
-        OpenSim_DECLARE_PROPERTY(location, SimTK::Vec3,
-                                 "The location (Vec3) of the station in a reference frame. "
-                                 "Frame is specified as Connector.");
-        
-        OpenSim_DECLARE_OUTPUT(position, SimTK::Vec3 , findLocationInGround, SimTK::Stage::Position);
-        OpenSim_DECLARE_OUTPUT(velocity, SimTK::Vec3 , findVelocityInGround, SimTK::Stage::Velocity);
-        OpenSim_DECLARE_OUTPUT(acceleration, SimTK::Vec3 , findAccelerationInGround, SimTK::Stage::Acceleration);
-        
-    public:
-        //--------------------------------------------------------------------------
-        // CONSTRUCTION
-        //--------------------------------------------------------------------------
-        Station();
-        virtual ~Station();
-        /** getter of Reference Frame off which the Station is defined */
-        const OpenSim::PhysicalFrame& getReferenceFrame() const;
-        /** setter of Reference Frame off which the Station is defined */
-        void setReferenceFrame(const OpenSim::PhysicalFrame& aFrame);
-        /** Find this Station's location in any Frame */
-        SimTK::Vec3 findLocationInFrame(const SimTK::State& s, const OpenSim::Frame& aFrame) const;
-        /** Find this Station's location in Ground */
-        SimTK::Vec3 findLocationInGround(const SimTK::State& s) const;
-        /** Find this Station's Velocity in Ground */
-        SimTK::Vec3 findVelocityInGround(const SimTK::State& s) const;
-        /** Find this Station's Acceleration in Ground */
-        SimTK::Vec3 findAccelerationInGround(const SimTK::State& s) const;
+class Body;
 
-    private:
-        void setNull();
-        void constructProperties() override;
-        void constructConnectors() override;
-        
-        //=============================================================================
-    };  // END of class Station
-    //=============================================================================
-    //=============================================================================
+//=============================================================================
+//=============================================================================
+/**
+ * A class implementing a Station. A Station is a point fixed to and defined with
+ * respect to a reference frame. The reference frame can be a Body (fixed
+ * to origin of a Body), a FixedFrame (affixed to another PhysicalFrame) or any
+ * other PhysicalFrame. The main functionality provided by Station is to find its
+ * location in any Frame.
+ *
+ * @author Ayman Habib
+ * @version 1.0
+ */
+class OSIMSIMULATION_API Station : public ModelComponent {
+    OpenSim_DECLARE_CONCRETE_OBJECT(Station, ModelComponent);
+public:
+    //==============================================================================
+    // PROPERTIES
+    //==============================================================================
+    OpenSim_DECLARE_PROPERTY(location, SimTK::Vec3,
+                             "The location (Vec3) of the station in a reference frame. "
+                             "Frame is specified as Connector.");
     
+    OpenSim_DECLARE_OUTPUT(position, SimTK::Vec3 , findLocationInGround, SimTK::Stage::Position);
+    OpenSim_DECLARE_OUTPUT(velocity, SimTK::Vec3 , findVelocityInGround, SimTK::Stage::Velocity);
+    OpenSim_DECLARE_OUTPUT(acceleration, SimTK::Vec3 , findAccelerationInGround, SimTK::Stage::Acceleration);
+    
+public:
+    //--------------------------------------------------------------------------
+    // CONSTRUCTION
+    //--------------------------------------------------------------------------
+    Station();
+    virtual ~Station();
+    /** getter of Reference Frame off which the Station is defined */
+    const OpenSim::PhysicalFrame& getReferenceFrame() const;
+    /** setter of Reference Frame off which the Station is defined */
+    void setReferenceFrame(const OpenSim::PhysicalFrame& aFrame);
+    /** Find this Station's location in any Frame */
+    SimTK::Vec3 findLocationInFrame(const SimTK::State& s, const OpenSim::Frame& aFrame) const;
+    /** Find this Station's location in Ground */
+    SimTK::Vec3 findLocationInGround(const SimTK::State& s) const;
+    /** Find this Station's Velocity in Ground */
+    SimTK::Vec3 findVelocityInGround(const SimTK::State& s) const;
+    /** Find this Station's Acceleration in Ground */
+    SimTK::Vec3 findAccelerationInGround(const SimTK::State& s) const;
+
+private:
+    void setNull();
+    void constructProperties() override;
+    void constructConnectors() override;
+    
+    //=============================================================================
+};  // END of class Station
+//=============================================================================
+//=============================================================================
+
 } // end of namespace OpenSim
 
 #endif // OPENSIM_STATION_H_
