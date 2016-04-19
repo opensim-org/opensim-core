a29af1b797472b95c8a267bfcf1feacc0304ae69
Ayman Habib <ahabib@stanford.edu>
2016-04-19 12:19:06 -0700

    Reload mesh file only on Property change and reduce Edit testcase timeout to 30 sec.

--- OpenSim/Simulation/Model/Geometry.cpp
+++ OpenSim/Simulation/Model/Geometry.cpp
@@ -171,75 +171,76 @@ void FrameGeometry::implementCreateDecorativeGeometry(SimTK::Array_<SimTK::Decor
 
 void Mesh::extendFinalizeFromProperties() {
 
-    const Component* rootModel = nullptr;
-    if (!hasParent()) {
-        std::clog << "Mesh " << get_mesh_file() << " not connected to model..ignoring\n";
-        return;   // Orphan Mesh not part of a model yet
-    }
-    const Component* parent = &getParent();
-    while (parent != nullptr) {
-        if (dynamic_cast<const Model*>(parent) != nullptr) {
-            rootModel = parent;
-            break;
+    if (!isObjectUpToDateWithProperties()) {
+        const Component* rootModel = nullptr;
+        if (!hasParent()) {
+            std::clog << "Mesh " << get_mesh_file() << " not connected to model..ignoring\n";
+            return;   // Orphan Mesh not part of a model yet
         }
-        if (parent->hasParent())
-            parent = &(parent->getParent()); // traverse up Component tree
-        else
-            break; // can't traverse up.
+        const Component* parent = &getParent();
+        while (parent != nullptr) {
+            if (dynamic_cast<const Model*>(parent) != nullptr) {
+                rootModel = parent;
+                break;
+            }
+            if (parent->hasParent())
+                parent = &(parent->getParent()); // traverse up Component tree
+            else
+                break; // can't traverse up.
+        }
+
+        if (rootModel == nullptr) {
+            std::clog << "Mesh " << get_mesh_file() << " not connected to model..ignoring\n";
+            return;   // Orphan Mesh not descendent of a model
+        }
+        // Current interface to Visualizer calls generateDecorations on every frame.
+        // On first time through, load file and create DecorativeMeshFile and cache it
+        // so we don't load files from disk during live drawing/rendering.
+        const std::string& file = get_mesh_file();
+        bool isAbsolutePath; string directory, fileName, extension;
+        SimTK::Pathname::deconstructPathname(file,
+            isAbsolutePath, directory, fileName, extension);
+        const string lowerExtension = SimTK::String::toLower(extension);
+        if (lowerExtension != ".vtp" && lowerExtension != ".obj" && lowerExtension != ".stl") {
+            std::clog << "ModelVisualizer ignoring '" << file
+                << "'; only .vtp .stl and .obj files currently supported.\n";
+            return;
+        }
+
+        // File is a .vtp or .obj. See if we can find it.
+        Array_<string> attempts;
+        const Model& model = dynamic_cast<const Model&>(*rootModel);
+        bool foundIt = ModelVisualizer::findGeometryFile(model, file, isAbsolutePath, attempts);
+
+        if (!foundIt) {
+            std::clog << "ModelVisualizer couldn't find file '" << file
+                << "'; tried\n";
+            for (unsigned i = 0; i < attempts.size(); ++i)
+                std::clog << "  " << attempts[i] << "\n";
+            if (!isAbsolutePath &&
+                !Pathname::environmentVariableExists("OPENSIM_HOME"))
+                std::clog << "Set environment variable OPENSIM_HOME "
+                << "to search $OPENSIM_HOME/Geometry.\n";
+            return;
+        }
+
+        SimTK::PolygonalMesh pmesh;
+        try {
+            std::ifstream objFile;
+            objFile.open(attempts.back().c_str());
+            pmesh.loadFile(attempts.back().c_str());
+            // objFile closes when destructed
+
+        }
+        catch (const std::exception& e) {
+            std::clog << "Visualizer couldn't read "
+                << attempts.back() << " because:\n"
+                << e.what() << "\n";
+            return;
+        }
+
+        cachedMesh.reset(new DecorativeMeshFile(attempts.back().c_str()));
     }
-
-    if (rootModel == nullptr) {
-        std::clog << "Mesh " << get_mesh_file() << " not connected to model..ignoring\n";
-        return;   // Orphan Mesh not descendent of a model
-    }
-    // Current interface to Visualizer calls generateDecorations on every frame.
-    // On first time through, load file and create DecorativeMeshFile and cache it
-    // so we don't load files from disk during live drawing/rendering.
-    const std::string& file = get_mesh_file();
-    bool isAbsolutePath; string directory, fileName, extension;
-    SimTK::Pathname::deconstructPathname(file,
-        isAbsolutePath, directory, fileName, extension);
-    const string lowerExtension = SimTK::String::toLower(extension);
-    if (lowerExtension != ".vtp" && lowerExtension != ".obj" && lowerExtension != ".stl") {
-        std::clog << "ModelVisualizer ignoring '" << file
-            << "'; only .vtp .stl and .obj files currently supported.\n";
-        return;
-    }
-
-    // File is a .vtp or .obj. See if we can find it.
-    Array_<string> attempts;
-    const Model& model = dynamic_cast<const Model&>(*rootModel);
-    bool foundIt = ModelVisualizer::findGeometryFile(model, file, isAbsolutePath, attempts);
-
-    if (!foundIt) {
-        std::clog << "ModelVisualizer couldn't find file '" << file
-            << "'; tried\n";
-        for (unsigned i = 0; i < attempts.size(); ++i)
-            std::clog << "  " << attempts[i] << "\n";
-        if (!isAbsolutePath &&
-            !Pathname::environmentVariableExists("OPENSIM_HOME"))
-            std::clog << "Set environment variable OPENSIM_HOME "
-            << "to search $OPENSIM_HOME/Geometry.\n";
-        return;
-    }
-
-    SimTK::PolygonalMesh pmesh;
-    try {
-        std::ifstream objFile;
-        objFile.open(attempts.back().c_str());
-        pmesh.loadFile(attempts.back().c_str());
-        // objFile closes when destructed
-
-    }
-    catch (const std::exception& e) {
-        std::clog << "Visualizer couldn't read "
-            << attempts.back() << " because:\n"
-            << e.what() << "\n";
-        return;
-    }
-
-    cachedMesh.reset(new DecorativeMeshFile(attempts.back().c_str()));
-
 }
 
 
