b568a2ca252341ec69d385ca8ea46ed6d6b15244
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-18 17:37:49 -0700

    Revert "Revert "Remove function updateFromXMLNode.""
    This reverts commit 83fbfd8eb46f54c7b7772341c2258b3054bd6547.
    
    [ci skip]

--- OpenSim/Simulation/Model/PathActuator.cpp
+++ OpenSim/Simulation/Model/PathActuator.cpp
@@ -262,26 +262,6 @@ SimTK::Vec3 PathActuator::computePathColor(const SimTK::State& state) const {
 
 
 //=============================================================================
-// XML
-//=============================================================================
-//-----------------------------------------------------------------------------
-// UPDATE FROM XML NODE
-//-----------------------------------------------------------------------------
-//_____________________________________________________________________________
-/**
- * Update this object based on its XML node.
- *
- * This method simply calls Object::updateFromXMLNode(SimTK::Xml::Element& aNode, int versionNumber) and then calls
- * a few methods in this class to ensure that variable members have been
- * set in a consistent manner.
- */
-void PathActuator::updateFromXMLNode(SimTK::Xml::Element& aNode, int versionNumber)
-{
-    updGeometryPath().setOwner(this);
-    Super::updateFromXMLNode(aNode, versionNumber);
-}   
-
-//=============================================================================
 // SCALING
 //=============================================================================
 //_____________________________________________________________________________
