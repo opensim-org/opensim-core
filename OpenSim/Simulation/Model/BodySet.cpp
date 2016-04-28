56a97f9c6c23d819e7f1fe9ee66686617357c3ac
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:30:08 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Model/BodySet.cpp
+++ OpenSim/Simulation/Model/BodySet.cpp
@@ -20,9 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "BodySet.h"
-#include <OpenSim/Common/ScaleSet.h>
 
 using namespace std;
 using namespace OpenSim;
