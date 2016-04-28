5d86aab5aa2c1219355eee1cb9efcb9485dc4ee8
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:09:10 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Control/ControlSet.cpp
+++ OpenSim/Simulation/Control/ControlSet.cpp
@@ -29,14 +29,7 @@
 //=============================================================================
 // INCLUDES
 //=============================================================================
-#include <OpenSim/Common/Object.h>
-#include <OpenSim/Common/PropertyObjArray.h>
-#include <OpenSim/Common/Storage.h>
-#include "ControlSet.h"
-#include "ControlLinear.h"
-#include <OpenSim/Simulation/SimbodyEngine/Constraint.h>
-
-
+#include "../simulation.h"
 
 
 using namespace OpenSim;
