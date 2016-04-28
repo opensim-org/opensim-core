8a00cbc179b3aba924391c698c1011e5170ec418
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 20:13:01 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Wrap/PathWrapSet.cpp
+++ OpenSim/Simulation/Wrap/PathWrapSet.cpp
@@ -20,8 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "PathWrapSet.h"
 
 using namespace std;
 using namespace OpenSim;
