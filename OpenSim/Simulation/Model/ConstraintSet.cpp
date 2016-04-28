598c36f00a6de0375426908c3ddf24ca94c94b8d
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:34:43 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Model/ConstraintSet.cpp
+++ OpenSim/Simulation/Model/ConstraintSet.cpp
@@ -20,9 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "ConstraintSet.h"
-#include <OpenSim/Simulation/SimbodyEngine/Constraint.h>
 
 using namespace std;
 using namespace OpenSim;
