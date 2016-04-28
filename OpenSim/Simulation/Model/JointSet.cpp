e6cdbdbbd336e78402d99551c64d0b6c4e36ebc2
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:47:51 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Model/JointSet.cpp
+++ OpenSim/Simulation/Model/JointSet.cpp
@@ -20,10 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "JointSet.h"
-#include <OpenSim/Simulation/Model/Model.h>
-#include <OpenSim/Simulation/Model/ComponentSet.h>
 
 using namespace std;
 using namespace OpenSim;
