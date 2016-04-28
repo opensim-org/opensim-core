301e77abd018173435c639db2c845cb5c05d9468
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:34:43 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Model/ContactGeometry.cpp
+++ OpenSim/Simulation/Model/ContactGeometry.cpp
@@ -20,11 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "ContactGeometry.h"
-#include "BodySet.h"
-#include "Model.h"
-#include <OpenSim/Common/ScaleSet.h>
 
 using SimTK::Vec3;
 using SimTK::Rotation;
