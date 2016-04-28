97e4c51372ba78b21e34b869c84495c19e1bc772
Shrinidhi KL <klshrinidhi@gmail.com>
2016-04-27 17:39:12 -0700

    Remove all headers. Include PCH.

--- OpenSim/Simulation/Model/CoordinateSet.cpp
+++ OpenSim/Simulation/Model/CoordinateSet.cpp
@@ -20,11 +20,8 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
+#include "../simulation.h"
 
-#include "CoordinateSet.h"
-#include <OpenSim/Simulation/SimbodyEngine/Joint.h>
-#include <OpenSim/Simulation/SimbodyEngine/Coordinate.h>
-#include <OpenSim/Simulation/Model/Model.h>
 
 using namespace std;
 using namespace OpenSim;
