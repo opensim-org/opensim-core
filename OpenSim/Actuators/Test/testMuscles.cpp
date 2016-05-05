d9bda8dba0781cb0738886825720ba5b80ce00eb
Shrinidhi KL <klshrinidhi@gmail.com>
2016-05-04 17:25:23 -0700

    Meant to construct with dynamically allocated object.

--- OpenSim/Actuators/Test/testMuscles.cpp
+++ OpenSim/Actuators/Test/testMuscles.cpp
@@ -301,7 +301,8 @@ void simulateMuscle(
 
     // Create a prescribed controller that simply 
     //applies controls as function of time
-    std::unique_ptr<PrescribedController> muscleController{};
+    std::unique_ptr<PrescribedController> 
+        muscleController{new PrescribedController{}};
     if(control != NULL){
         muscleController->setActuators(model.updActuators());
         // Set the individual muscle control functions 
