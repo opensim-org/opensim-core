2099ffe9baada88401e09fa00c8f20c441709903
Shrinidhi KL <klshrinidhi@gmail.com>
2016-05-09 00:36:08 -0700

    Fix the call to appendRow(). Argument needs to be a RowVector.

--- OpenSim/Common/Reporter.h
+++ OpenSim/Common/Reporter.h
@@ -258,7 +258,8 @@ inline void TableReporter_<SimTK::Vector, SimTK::Real>::
         const_cast<Self*>(this)->_outputTable.setColumnLabels(labels);
     }
 
-    const_cast<Self*>(this)->_outputTable.appendRow(state.getTime(), ~result);
+    const_cast<Self*>(this)->_outputTable.appendRow(state.getTime(), 
+                                                    result.getAsRowVector());
 }
 
 /** @name Commonly used concrete TableReporters */
