3d17ab93db32e84a07a990a935770b594324bf5b
Shrinidhi KL <klshrinidhi@gmail.com>
2016-05-24 17:43:42 -0700

    First attempt to list unused cache versions.
    [skip appveyor]

---
+++ travis_scripts/RemoveOldUnusedCache.sh
@@ -0,0 +1,36 @@
+set -e
+
+# Arguments
+PROJECT=$1
+SOURCE_DIR=$2
+
+CURR_DIR=$(pwd)
+# Turn relative paths into absolute paths.
+if [ "${SOURCE_DIR:0:1}" != "/" ]; then
+  SOURCE_DIR=${CURR_DIR}/${SOURCE_DIR}
+fi
+
+if ! $USE_CACHE; then
+  echo "---- Caching disabled."
+  cd $CURR_DIR
+  return
+fi
+
+echo "---- Retrieving list of versions on cache."
+if [[ "$CC" == *gcc* ]]; then export COMPILER=gcc; fi
+if [[ "$CC" == *clang* ]]; then export COMPILER=clang; fi
+PACKAGENAME="${MACHTYPE}_${COMPILER}_${BTYPE}"
+URL="https://api.bintray.com/packages/opensim/${PROJECT}/${PACKAGENAME}"
+CACHED_VERSIONS=$(curl --silent -u$BINTRAY_CREDS $URL | jq .versions[] | sed 's/"//g')
+
+cd $SOURCE_DIR
+git fetch -q origin master:master
+BRANCHES=$(git ls-remote --heads origin | sed 's/.*\trefs\/heads\///') 
+for b in BRANCHES; do 
+  git fetch -q origin $b:$b 
+  BRANCHBASE=$(git merge-base master $b)
+  CACHED_VERSIONS="${CACHED_VERSIONS/ $BRANCHBASE/}"
+done
+echo $CACHED_VERSIONS
+
+cd $CURR_DIR
