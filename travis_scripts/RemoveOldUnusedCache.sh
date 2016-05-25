09177c24502d9bd28eca82c133fc43e3f645f8fe
Shrinidhi KL <klshrinidhi@gmail.com>
2016-05-24 20:30:33 -0700

    Add code to delete version. [skip appveyor]

--- travis_scripts/RemoveOldUnusedCache.sh
+++ travis_scripts/RemoveOldUnusedCache.sh
@@ -16,25 +16,29 @@ if ! $USE_CACHE; then
   return
 fi
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then brew update; brew install jq; fi
+
 echo "---- Retrieving list of versions on cache."
 if [[ "$CC" == *gcc* ]]; then export COMPILER=gcc; fi
 if [[ "$CC" == *clang* ]]; then export COMPILER=clang; fi
 PACKAGENAME="${MACHTYPE}_${COMPILER}_${BTYPE}"
 URL="https://api.bintray.com/packages/opensim/${PROJECT}/${PACKAGENAME}"
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then brew update; brew install jq; fi
 CACHED_VERSIONS=$(curl --silent -u$BINTRAY_CREDS $URL | jq .versions[] | sed 's/"//g')
 
-echo $CACHED_VERSIONS
-echo "--- Retrieving list of used versions."
+echo "---- Retrieving list of currently used versions."
 cd $SOURCE_DIR
 git fetch -q origin master:master
 BRANCHES=$(git ls-remote --heads origin | sed 's/.*refs\/heads\///')
-echo $BRANCHES
 for b in $BRANCHES; do 
   git fetch -q origin $b:$b 
   BRANCHBASE=$(git merge-base master $b)
   CACHED_VERSIONS="${CACHED_VERSIONS/$BRANCHBASE/}"
 done
-echo $CACHED_VERSIONS
 
+URL="$URL/versions"
+for v in $CACHED_VERSIONS; do
+  echo "---- Deleting cache for version $v."
+  curl --silent -X DELETE -u$BINTRAY_CREDS ${URL}/${v}
+  echo
+done
 cd $CURR_DIR
