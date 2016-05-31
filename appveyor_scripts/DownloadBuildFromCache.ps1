0102f4f58770da5c5d1efe0d58a7ef4f501da4f4
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-05-31 16:07:05 -0700

    Debugging.

--- appveyor_scripts/DownloadBuildFromCache.ps1
+++ appveyor_scripts/DownloadBuildFromCache.ps1
@@ -1,9 +1,14 @@
 $CURR_DIR = (pwd)
 
 New-Item -Path . -Name build -ItemType Directory -Force
+
 if(! $USE_CACHE) {
   Write-Host "---- Not downloading cache. Caching disabled."
   return
+}
+
+if($env:APPVEYOR_REPO_BRANCH -eq "master") {
+  Write-Host "---- Not downloading cache. This is master build."
 }
 
 Write-Host $env:APPVEYOR_PULL_REQUEST_NUMBER
