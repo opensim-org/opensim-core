fb20daa14f4b511a3a2503b60a68bb1161598c0e
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-06-02 15:00:16 -0700

    Debugging.

--- appveyor_scripts/DownloadDepBuildFromCache.ps1
+++ appveyor_scripts/DownloadDepBuildFromCache.ps1
@@ -8,3 +8,11 @@ if(! $USE_CACHE) {
   Write-Host "---- Not uploading cache. Caching disabled."
   return
 }
+
+Set-Location $BUILD_DIR
+# Extract the commit id of the dependency.
+$BRANCHTIP = (Select-String -Pattern 'COMMAND .*git.*checkout ([0-9a-z]{40})' -Path .\tmp\*gitclone.cmake).Matches.Groups[1].Value
+$SOURCEURL = (Select-String -Pattern 'COMMAND .*git.*clone .*("https://.*git")' -Path .\tmp\*gitclone.cmake).Matches.Groups[1].Value
+
+Write-Host $BRANCHTIP
+Write-Host $SOURCEURL
