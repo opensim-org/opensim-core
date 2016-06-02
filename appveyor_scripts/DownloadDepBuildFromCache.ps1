b6372b1a052bdc575e03df56414c72ff2a157b21
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-06-02 15:10:25 -0700

    Debugging.

--- appveyor_scripts/DownloadDepBuildFromCache.ps1
+++ appveyor_scripts/DownloadDepBuildFromCache.ps1
@@ -13,6 +13,8 @@ Set-Location $BUILD_DIR
 # Extract the commit id of the dependency.
 $BRANCHTIP = (Select-String -Pattern 'COMMAND .*git.*checkout ([0-9a-z]{40})' -Path .\tmp\*gitclone.cmake).Matches.Groups[1].Value
 $SOURCEURL = (Select-String -Pattern 'COMMAND .*git.*clone .*("https://.*git")' -Path .\tmp\*gitclone.cmake).Matches.Groups[1].Value
+Set-Location $SOURCE_DIR/..
+git clone $SOURCEURL (Get-Item $SOURCE_DIR).Name
 
 Write-Host $BRANCHTIP
 Write-Host $SOURCEURL
