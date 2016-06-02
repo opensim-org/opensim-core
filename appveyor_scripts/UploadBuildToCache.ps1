c87112a907e6077af94e71f4e705886d3f5919de
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-06-01 17:50:07 -0700

    Debugging. [skip ci]

--- appveyor_scripts/UploadBuildToCache.ps1
+++ appveyor_scripts/UploadBuildToCache.ps1
@@ -1,40 +1,46 @@
-$CURR_DIR = (pwd)
+$PROJECT = $args[0]
+$SOURCE_DIR = $args[1]
+$BUILD_DIR = $args[2]
+
+$CURR_DIR = (Get-Location)
 
 if(! $USE_CACHE) {
   Write-Host "---- Not uploading cache. Caching disabled."
   return
 }
 
-#if($env:APPVEYOR_REPO_BRANCH -neq "master") {
-#  Write-Host "---- Not uploading cache. This is not master build."
-#  return
-#}
-Set-Location $env:OPENSIM_SOURCE_DIR
-git fetch origin master:master
-git checkout (git merge-base master build_cache_dev_branch)
-Set-Location $env:OPENSIM_BUILD_DIR
-cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
+Set-Location $SOURCE_DIR
+if($PROJECT -eq "opensim-core" -and $env:APPVEYOR_REPO_BRANCH -neq "master") {
+  Write-Host "---- Not uploading cache. This is not master build."
+  Set-Location $CURR_DIR
+  return
+}
+
+#Set-Location $env:OPENSIM_SOURCE_DIR
+#git fetch origin master:master
+#git checkout (git merge-base master build_cache_dev_branch)
+#Set-Location $env:OPENSIM_BUILD_DIR
+#cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
 
 $MASTERTIP = (git log -n1 --format="%H")
-# $MASTERTIP = $env:APPVEYOR_REPO_COMMIT
 if($env:CMAKE_GENERATOR -like "*Win64") {
   $COMPILER = "msvc_win64"  
 } else {
   $COMPILER = "msvc_win32"
 }
 $PACKAGE_NAME = $env:Platform + "_" + $COMPILER + "_" + "Release"
-$URL="https://dl.bintray.com/opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}"
-$BUILD_DIRNAME = (get-item $env:OPENSIM_BUILD_DIR).name
+$URL="https://dl.bintray.com/opensim/${PROJECT}/${PACKAGENAME}/${MASTERTIP}"
+$BUILD_DIRNAME = (get-item $BUILD_DIR).name
 $ZIP = $BUILD_DIRNAME + ".zip"
 # If already cached, return.
 try {
   if ( (Invoke-WebRequest -Method Head "${URL}/${ZIP}_a").StatusCode -eq 200 ) {
-    Write-Host "---- Already cached: opensim-core."
+    Write-Host "---- Already cached: ${PROJECT}."
     return
   }
 } catch {}
 
-Set-Location $env:OPENSIM_BUILD_DIR/..
+Set-Location $BUILD_DIR/..
 Write-Host '---- Compressing build directory into a zip.'
 Compress-Archive -Path $BUILD_DIRNAME -DestinationPath $ZIP
 Write-Host '---- Splitting zip into smaller pieces for upload.'
@@ -55,13 +61,13 @@ Remove-Item $ZIP
 
 $PASSWORD = ConvertTo-SecureString "440061321dba00a68210b482261154ea58d03f00" -AsPlainText -Force
 $CREDS = New-Object System.Management.Automation.PSCredential("klshrinidhi", $PASSWORD)
-$URL = "https://api.bintray.com/content/opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}/${PACKAGENAME}/${MASTERTIP}"
+$URL = "https://api.bintray.com/content/opensim/${PROJECT}/${PACKAGENAME}/${MASTERTIP}/${PACKAGENAME}/${MASTERTIP}"
 (Get-Item "${ZIP}_*").ForEach({
-  Write-Host "---- Uploading piece $_ to opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}"
+  Write-Host "---- Uploading piece $_ to opensim/${PROJECT}/${PACKAGENAME}/${MASTERTIP}"
   Invoke-WebRequest -Credential $CREDS -Method PUT -InFile $_ $URL/$_ | Out-Null
 })
 
-$URL = "https://api.bintray.com/content/opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}/publish"
+$URL = "https://api.bintray.com/content/opensim/${PROJECT}/${PACKAGENAME}/${MASTERTIP}/publish"
 Write-Host '---- Publishing uploaded build directory.'
 Invoke-WebRequest -Credential $CREDS -Method POST $URL | Out-Null
 Write-Host '---- Cleaning up.'
