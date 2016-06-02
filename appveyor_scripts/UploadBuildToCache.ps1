d86e7237ed72bf4607b8e71be4c619946a7ba4a9
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-06-01 19:49:49 -0700

    Debugging.

--- appveyor_scripts/UploadBuildToCache.ps1
+++ appveyor_scripts/UploadBuildToCache.ps1
@@ -43,7 +43,7 @@ try {
 Set-Location $BUILD_DIR/..
 Write-Host '---- Compressing build directory into a zip.'
 Add-Type -AssemblyName "System.IO.Compression.FileSystem"
-[System.IO.Compression.ZipFile]::CreateFromDirectory((Get-Item "opensim_build"), "$BUILD_DIR/$ZIP")
+[System.IO.Compression.ZipFile]::CreateFromDirectory($BUILD_DIR, "$BUILD_DIR/$ZIP")
 Write-Host '---- Splitting zip into smaller pieces for upload.'
 $FILESTREAM = [System.IO.File]::OpenRead((Get-Item $ZIP))
 $BUFFER = New-Object byte[] 200mb
