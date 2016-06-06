8016760097d98be4f316188c3687395ae73d282d
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-06-05 18:18:42 -0700

    Debugging.

--- appveyor_scripts/FindTestsToExclude.ps1
+++ appveyor_scripts/FindTestsToExclude.ps1
@@ -5,10 +5,9 @@ $COUNTER = 0
 ForEach($TEST in $TESTS) {
   $TESTNAME = $TEST.Matches.Groups[1].Value
   $TESTEXE = (Get-ChildItem -Recurse -Include "${TESTNAME}.exe")
-  Write-Host $TESTNAME --- $TESTEXE.LastWriteTime
   if($TESTEXE -and 
      $TESTEXE.LastWriteTime -lt $BUILD_START_TIMESTAMP) {
-    $env:OPENSIM_EXCLUDE_TESTS = "${env:OPENSIM_EXCLUDE_TESTS}|$TESTNAME"
+    $env:OPENSIM_EXCLUDE_TESTS = "${env:OPENSIM_TESTS_TO_EXCLUDE}|$TESTNAME"
     Write-Host "---- Excluding test $TESTNAME"
     $COUNTER = $COUNTER + 1
   }
