61aabba0c5b4664c5b3d97831c0d2405df310b05
Shrinidhi KL <klshrinidhi@gmail.com>
2016-07-05 18:27:27 -0700

    Fix names in tests.

--- OpenSim/Simulation/SimbodyEngine/Test/testJoints.cpp
+++ OpenSim/Simulation/SimbodyEngine/Test/testJoints.cpp
@@ -1208,12 +1208,15 @@ void testBallJoint()
 
     // Test accessors.
     //TODO: Ideally, "hip.getCoordinate(BallJoint::Rotation1)"
-    ASSERT(hip.getCoordinate(0)==hip.getCoordinate(BallJoint::BallJointCoordIdx::Rotation1),
-        __FILE__, __LINE__, "Coordinate accessor failed");
-    ASSERT(hip.getCoordinate(1)==hip.getCoordinate(BallJoint::BallJointCoordIdx::Rotation2),
-        __FILE__, __LINE__, "Coordinate accessor failed");
-    ASSERT(hip.getCoordinate(2)==hip.getCoordinate(BallJoint::BallJointCoordIdx::Rotation3),
-        __FILE__, __LINE__, "Coordinate accessor failed");
+    ASSERT(hip.getCoordinate(0) == 
+           hip.getCoordinate(BallJoint::Coord::Rotation1),
+           __FILE__, __LINE__, "Coordinate accessor failed");
+    ASSERT(hip.getCoordinate(1) == 
+           hip.getCoordinate(BallJoint::Coord::Rotation2),
+           __FILE__, __LINE__, "Coordinate accessor failed");
+    ASSERT(hip.getCoordinate(2) ==
+           hip.getCoordinate(BallJoint::Coord::Rotation3),
+           __FILE__, __LINE__, "Coordinate accessor failed");
     ASSERT_THROW(OpenSim::Exception, hip.getCoordinate());
 
 } // end testBallJoint
