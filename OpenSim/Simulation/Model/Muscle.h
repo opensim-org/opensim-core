9f5313621e7ba8c49fb7b9b31ee3977b06f15393
Tom Uchida <tom.uchida@gmail.com>
2016-08-19 17:00:35 -0700

    Added "detail" string argument to MuscleCannotEquilibrate exception.

--- OpenSim/Simulation/Model/Muscle.h
+++ OpenSim/Simulation/Model/Muscle.h
@@ -44,8 +44,9 @@ public:
     MuscleCannotEquilibrate(const std::string& file,
                             size_t line,
                             const std::string& func,
-                            const Object& obj) :
-        Exception(file, line, func, obj) {
+                            const Object& obj,
+                            const std::string& detail) :
+        Exception(file, line, func, obj, detail) {
         std::string msg = "Unable to compute equilibrium for this muscle.\n";
         msg += "Please verify that the initial activation is valid and that ";
         msg += "the length of the musculotendon actuator doesn't produce a ";
