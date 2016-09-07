282b57a59ff95585077f5c4e92cb10251dba83c5
Shrinidhi KL <klshrinidhi@gmail.com>
2016-09-07 01:46:19 -0700

    Remove quote. [skip ci]

--- OpenSim/Examples/ExampleHopperDevice/exampleHopperDevice_answers.cpp
+++ OpenSim/Examples/ExampleHopperDevice/exampleHopperDevice_answers.cpp
@@ -250,9 +250,9 @@ int main(int argc, char* argv[]) {
     // Command line argument parsing.
     //==========================================================================
     // Following are arguments to this executable:
-    // (1) 'noVisualizer' -- [Optional] Do not show visualizer. Default
+    // (1) noVisualizer -- [Optional] Do not show visualizer. Default
     //                       behavior is to show visualizer.
-    // (2) 'simulateOnce' -- [Optional] Simulate once and exit the program.
+    // (2) simulateOnce -- [Optional] Simulate once and exit the program.
     //                       Default behavior is to simulate multiple times
     //                       until user requests to stop.
     
