1e08f22a89b99851655e9ec83d6ca59d656ace9d
Shrinidhi KL <klshrinidhi@gmail.com>
2016-09-07 19:16:26 -0700

    Untabify.

--- OpenSim/Common/TableSource.h
+++ OpenSim/Common/TableSource.h
@@ -203,29 +203,29 @@ protected:
                          time, timeCol.front(), timeCol.back());
 
         const auto colInd = 
-			static_cast<int>(_table.getColumnIndex(columnLabel));
+            static_cast<int>(_table.getColumnIndex(columnLabel));
         auto lb = std::lower_bound(timeCol.begin(), timeCol.end(), time);
         if(lb == timeCol.begin())
             return _table.getMatrix().getElt(0, colInd);
         else if(lb == timeCol.end())
             return _table.
-				   getMatrix().
-				   getElt(static_cast<int>(timeCol.size() - 1), colInd);
+                   getMatrix().
+                   getElt(static_cast<int>(timeCol.size() - 1), colInd);
         else if(*lb == time)
             return _table.
-				   getMatrix().
-				   getElt(static_cast<int>(lb - timeCol.begin()), colInd);
+                   getMatrix().
+                   getElt(static_cast<int>(lb - timeCol.begin()), colInd);
         else {
             auto prevTime = *(lb - 1);
             auto nextTime = *lb;
             auto prevElt = _table.
-						   getMatrix().
-						   getElt(static_cast<int>(lb - 1 - timeCol.begin()),
-                                                   colInd);
+                           getMatrix().
+                           getElt(static_cast<int>(lb - 1 - timeCol.begin()),
+                                  colInd);
             auto nextElt = _table.
-						   getMatrix().
-						   getElt(static_cast<int>(lb - timeCol.begin()), 
-                                                   colInd);
+                           getMatrix().
+                           getElt(static_cast<int>(lb - timeCol.begin()), 
+                                  colInd);
             auto elt = ((time - prevTime) / (nextTime - prevTime)) * 
                        (nextElt - prevElt) + prevElt;
             return elt;
