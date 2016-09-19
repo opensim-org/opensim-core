3bacb918ab674415124331bdbc5c1a8f75a8c15c
Ayman Habib <ahabib@stanford.edu>
2016-09-19 14:07:58 -0700

    Add test case that exercises tables

---
+++ Bindings/Java/tests/TestTables.java
@@ -0,0 +1,20 @@
+import org.opensim.modeling.*;
+import org.opensim.modeling.DataTable;
+import org.opensim.modeling.RowVector;
+import org.opensim.modeling.StdVectorString;
+
+class TestTables {
+  public static void main(String[] args) {
+        DataTable table = new DataTable();
+        StdVectorString labels = new StdVectorString();
+        labels.add("0");
+        labels.add("1");
+        labels.add("2");
+        table.setColumnLabels(labels);
+        RowVector row0 = new RowVector(3, 7.);
+        table.appendRow(0, row0);
+        double value = row0.__getitem__(2);
+		System.out.println("Value ="+value);
+
+  }
+}
