9ce0386984b3c521bfacdb46c830a5ac7b487404
Shrinidhi KL <klshrinidhi@gmail.com>
2016-09-27 15:36:40 -0700

    Get Java test to use the added methods.

--- Bindings/Java/tests/TestTables.java
+++ Bindings/Java/tests/TestTables.java
@@ -4,17 +4,18 @@ import org.opensim.modeling.RowVector;
 import org.opensim.modeling.StdVectorString;
 
 class TestTables {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         DataTable table = new DataTable();
         StdVectorString labels = new StdVectorString();
         labels.add("0");
         labels.add("1");
         labels.add("2");
         table.setColumnLabels(labels);
-        RowVector row0 = new RowVector(3, 7.);
+        RowVector row0 = new RowVector(3, 7.0);
         table.appendRow(0, row0);
-        double value = row0.__getitem__(2);
-        System.out.println("Value ="+value);
-        double shouldThrow = row0.__getitem__(4);
-  }
+        double value = row0.get(2);
+        try {
+            double shouldThrow = row0.get(4);
+        } catch (java.lang.RuntimeException exc) {}
+    }
 }
