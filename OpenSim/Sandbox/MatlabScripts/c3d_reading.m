f7493f00490fe2ee02cba64ee29b5168a5478904
jimmyDunne <james.dunne@stanford.edu>
2016-10-10 10:04:42 -0700

    updated c3d read using PR 1278

--- OpenSim/Sandbox/MatlabScripts/c3d_reading.m
+++ OpenSim/Sandbox/MatlabScripts/c3d_reading.m
@@ -11,7 +11,7 @@
 import org.opensim.modeling.*
 
 %% Use a c3dAdapter to turn read a c3d file
-adapter = C3DFileAdapter()
+adapter = C3DFileAdapter();
 
 tables = adapter.read('test_walking.c3d');
 
@@ -25,13 +25,19 @@ trcfileadapter.write(markers,'test_walking.trc');
 %% Get the force and convert the times series table to type double
 forces = tables.get('forces');
 
-% Flatten the data to a times series table
-% THE BELOW LINE GENERATES AN EXCEPTION
-forcesdouble = forces.flatten()  ;
+% make a vector of strings to be used for a custom suffix
+suffixes = StdVectorString();
+suffixes.add('_x'); 
+suffixes.add('_y'); 
+suffixes.add('_z');
+
+% Flatten (Vec3 to double) the data to a times series table 
+forces_double = forces.flatten(suffixes);
+
 
 % Write flattened forces table to .mot file format
 stofileadapter = STOFileAdapter();
-stofileadapter.write(forces,'test_walking_grf.mot')
+stofileadapter.write(forces_double,'test_walking_grf.mot')
 
 %% Define a rotation matix
 Rot = 90;
@@ -39,31 +45,55 @@ rotationMatrix = [1,0,0;...
                   0,cos(Rot*pi/180),-(sin(Rot*pi/180));...
                   0,sin(Rot*pi/180),cos(Rot*pi/180)];
 
+%% make a copy of the marker and grf tables
+markerTable_rotated = markers;
+forcesTable_rotated = forces;
 
 %% Rotate marker data
-% MAKE A COPY OF THE TABLE
-
-for iMarker = 0 : length(markerdata) - 1
-
+for iMarker = 0 : markers.getNumColumns - 1
     % get the column data for the marker
-    marker = markers.updDependentColumn(markerlabels(iMarker+1) );
-
+    marker = markerTable_rotated.getDependentColumnAtIndex(iMarker);
+    
     % go through each element of the table column, rotate the Vec3, and write
     % back to the column.
     for iRow = 0 : markers.getNumRows - 1
-        % get Matlab vector marker position
-        vectorData = [marker.getElt(0,iRow).get(0)...
+       % get Matlab vector marker position
+       vectorData = [marker.getElt(0,iRow).get(0)...
                       marker.getElt(0,iRow).get(1)...
                       marker.getElt(0,iRow).get(2)];
 
-        % rotate the marker data
-        rotatedData = [rotationMatrix'*vectorData']';
-
-        % WRITE THE ROTATED DATA TO THE TABLE COPY
-
+       % rotate the marker data
+       rotatedData = [rotationMatrix'*vectorData']';
+       % write the rotated data to the table copy
+       marker.set(iRow, Vec3(rotatedData(1),rotatedData(2),rotatedData(3)));        
     end
 end
 
 %% Print the rotated markers to trc file
-trcfileadapter = TRCFileAdapter();
-trcfileadapter.write(markers,'test_walking_rotated.trc');
+trcfileadapter.write(markerTable_rotated,'test_walking_rotated.trc');
+
+%% Rotate force data
+for iforces = 0 : forces.getNumColumns - 1
+    % get the column data for the marker
+    force = forcesTable_rotated.getDependentColumnAtIndex(iforces);
+    
+    % go through each element of the table column, rotate the Vec3, and write
+    % back to the column.
+    for iRow = 0 : forces.getNumRows - 1
+       % get Matlab vector marker position
+       vectorData = [force.getElt(0,iRow).get(0)...
+                      force.getElt(0,iRow).get(1)...
+                      force.getElt(0,iRow).get(2)];
+
+       % rotate the force data
+       rotatedData = [rotationMatrix'*vectorData']';
+       % write the rotated data to the table copy
+       force.set(iRow, Vec3(rotatedData(1),rotatedData(2),rotatedData(3)));        
+    end
+end
+
+%% Print the rotated forces to trc file
+% Flatten (Vec3 to double) the data to a times series table 
+forcesTable_double = forcesTable_rotated.flatten(suffixes);
+% Write flattened forces table to .mot file format
+stofileadapter.write(forcesTable_double,'test_walking_grf_rotated.mot')
