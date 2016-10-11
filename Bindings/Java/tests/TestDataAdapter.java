1956f3ed4d1f3fe5cf8d53ac5799c483b9fdba12
Shrinidhi KL <klshrinidhi@gmail.com>
2016-10-11 13:34:30 -0700

    Java tests for table metadata & column metadata.

--- Bindings/Java/tests/TestDataAdapter.java
+++ Bindings/Java/tests/TestDataAdapter.java
@@ -11,6 +11,9 @@ class TestDataAdapter {
         TimeSeriesTableVec3 markerTable = tables.get("markers");
         assert markerTable.getNumRows()    == 1103;
         assert markerTable.getNumColumns() == 40;
+        assert markerTable.
+               getTableMetaDataString("DataRate").equals("250.000000");
+        assert markerTable.getTableMetaDataString("Units").equals("mm");
 
         TimeSeriesTable markerTableFlat = markerTable.flatten();
         assert markerTableFlat.getNumRows()    == 1103;
@@ -28,6 +31,16 @@ class TestDataAdapter {
         TimeSeriesTableVec3 forceTable = tables.get("forces");
         assert forceTable.getNumRows()    == 8824;
         assert forceTable.getNumColumns() == 6;
+        assert forceTable.
+               getTableMetaDataString("DataRate").equals("2000.000000");
+        StdVectorString units = forceTable.getDependentsMetaDataString("units");
+        assert units.size() == 6;
+        assert units.get(0).equals("N");
+        assert units.get(1).equals("Nmm");
+        assert units.get(2).equals("mm");
+        assert units.get(3).equals("N");
+        assert units.get(4).equals("Nmm");
+        assert units.get(5).equals("mm");
 
         TimeSeriesTable forceTableFlat = forceTable.flatten();
         assert forceTableFlat.getNumRows()    == 8824;
