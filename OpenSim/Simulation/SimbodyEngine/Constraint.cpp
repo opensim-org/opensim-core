a6e17e1f24e2ac229d2c0cd7e9c62101247bfddd
Shrinidhi KL <klshrinidhi@gmail.com>
2016-11-18 16:01:32 -0800

    Toggle value only if there is one.

--- OpenSim/Simulation/SimbodyEngine/Constraint.cpp
+++ OpenSim/Simulation/SimbodyEngine/Constraint.cpp
@@ -87,7 +87,7 @@ void Constraint::updateFromXMLNode(SimTK::Xml::Element& node,
                 elem.setElementTag(newName);
                 if(elem.getValue().find("true") != std::string::npos)
                     elem.setValue("false");
-                else
+                else if(elem.getValue().find("false") != std::string::npos)
                     elem.setValue("true");
             }
         }
