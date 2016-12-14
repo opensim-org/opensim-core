8142e81b36a430e2133d6b097498a1436aa61a91
jimmyDunne <james.dunne@stanford.edu>
2016-12-14 13:49:37 -0800

    addressed feedback, made some updates

--- OpenSim/Sandbox/MatlabScripts/pendulum_simulation.m
+++ OpenSim/Sandbox/MatlabScripts/pendulum_simulation.m
@@ -1,4 +1,3 @@
-%  *                    OpenSim:  testREADME.cpp                                *
 %  * -------------------------------------------------------------------------- *
 %  * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
 %  * See http://opensim.stanford.edu and the NOTICE file for more information.  *
@@ -7,8 +6,7 @@
 %  * through the Warrior Web program.                                           *
 %  *                                                                            *
 %  * Copyright (c) 2005-2016 Stanford University and the Authors                *
-%  * Author(s): James Dunne, Thomas Uchida                                      *
-%  * Contributor(s): Chris Dembia                                               *
+%  * Author(s): James Dunne, Thomas Uchida, Chris Dembia                        *
 %  *                                                                            *
 %  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
 %  * not use this file except in compliance with the License. You may obtain a  *
@@ -21,36 +19,35 @@
 %  * limitations under the License.                                             *
 %  * -------------------------------------------------------------------------- */
 
-%% Read a model into memory, run a fwd simulation,
-clear all; close all; clc;
-
+%% Read a model into memory, run a fwd simulation, and print results to file
 import org.opensim.modeling.*
 
-%   public static void test_TableReporter_2() throws java.io.IOException {
-timeInterval = 0.1;
-
-% add a model
+% read double pendulum model into memory
 modelFileName = 'double_pendulum_markers.osim';
 model = Model(modelFileName);
+
+%% set the time interval that reporters will use
+timeInterval = 0.1;
 
 %% add a console reporter
 consoleReporter = ConsoleReporter();
 consoleReporter.set_report_time_interval(timeInterval);
 
 % When connecting the outputs, set the alias names 'pin1_anlge' and 'q2'.
-% These will appear as the colomn labels
-consoleReporter.updInput('inputs').connect(model.getCoordinateSet().get(0).getOutput('value'),'pin1_angle');
-consoleReporter.updInput('inputs').connect(model.getCoordinateSet().get(1).getOutput('value'),'q2');
+% These will appear as the column labels
+consoleReporter.addToReport(model.getCoordinateSet().get(0).getOutput('value'),'q1');
+consoleReporter.addToReport(model.getCoordinateSet().get(1).getOutput('value'),'q2');
 model.addComponent(consoleReporter);
 
-%% add a table reporter for the pendulum coordiantes
+
+%% add a table reporter for the pendulum coordinates
 coordinateReporter= TableReporter();
 coordinateReporter.set_report_time_interval(timeInterval);
 
 % When connecting the outputs, set the alias names 'q1' and 'q2'.
-% These will be the colomn labels when printed to file
-coordinateReporter.updInput('inputs').connect(model.getCoordinateSet().get(0).getOutput('value'),'q1');
-coordinateReporter.updInput('inputs').connect(model.getCoordinateSet().get(1).getOutput('value'),'q2');
+% These will be the column labels when printed to file
+coordinateReporter.addToReport(model.getCoordinateSet().get(0).getOutput('value'),'q1');
+coordinateReporter.addToReport(model.getCoordinateSet().get(1).getOutput('value'),'q2');
 model.addComponent(coordinateReporter);
 
 %% add a Vec3 table reporter for the pendulum markers
@@ -59,13 +56,9 @@ markerReporter.set_report_time_interval(timeInterval);
 
 % When connecting the outputs, set the alias names 'marker_1' and 'marker_2'.
 % These will be the colomn labels when printed to file
-markerReporter.updInput('inputs').connect(model.getMarkerSet().get(0).getOutput('location'),'marker_1');
-markerReporter.updInput('inputs').connect(model.getMarkerSet().get(1).getOutput('location'),'marker_2');
+markerReporter.addToReport(model.getMarkerSet().get(0).getOutput('location'),'marker_1')
+markerReporter.addToReport(model.getMarkerSet().get(1).getOutput('location'),'marker_1')
 model.addComponent(markerReporter);
-
-%% define the adapters
-stoAdapter = STOFileAdapter();
-stoAdapterVec3 = STOFileAdapterVec3();
 
 % run a fwd simulation using the manager
 state = model.initSystem();
@@ -74,13 +67,21 @@ manager.setInitialTime(0);
 manager.setFinalTime(2);
 manager.integrate(state);
 
-% get the coordinate table from the reporter and write to file
+%% define the adapters
+stoAdapter = STOFileAdapter();
+stoAdapterVec3 = STOFileAdapterVec3();
+trcAdapter = TRCFileAdapter();
+
+% write coordinate data to .sto file
 table = coordinateReporter.getTable();
 stoAdapter.write(table, 'pendulum_coordinates.sto');
 
-% get the marker table from the reporter and write to file
+% write marker data to .sto file
 tableVec3 = markerReporter.getTable();
 stoAdapterVec3.write(tableVec3 , 'marker_locations.sto')
 
-trcAdapter = TRCFileAdapter();
+% write marker data to .trc file
+% trc adapter requires DataRate and Units be included in the metadata. 
+tableVec3.addTableMetaDataString('DataRate','10');
+tableVec3.addTableMetaDataString('Units','m');
 trcAdapter.write(tableVec3 , 'marker_locations.trc')
