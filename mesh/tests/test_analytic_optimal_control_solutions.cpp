eef284c9da161738d7fea8432f8707fdb32e2506
Christopher Dembia <cld72@cornell.edu>
2016-12-16 18:12:34 -0800

    Minimize time for sliding mass works!!

--- mesh/tests/test_analytic_optimal_control_solutions.cpp
+++ mesh/tests/test_analytic_optimal_control_solutions.cpp
@@ -80,7 +80,7 @@ TEST_CASE("Second order linear min effort", "[adolc][trapezoidal]") {
 
     auto ocp = std::make_shared<SecondOrderLinearMinEffort<adouble>>();
     DirectCollocationSolver<adouble> dircol(ocp, "trapezoidal", "ipopt", 1000);
-    OptimalControlSolution solution = dircol.solve();
+i    OptimalControlSolution solution = dircol.solve();
     solution.write("second_order_linear_min_effort_solution.csv");
 
     MatrixXd expected_states = ocp->states_solution(solution.time);
--- mesh/tests/test_sliding_mass_minimum_time.cpp
+++ mesh/tests/test_sliding_mass_minimum_time.cpp
@@ -13,15 +13,16 @@ using namespace mesh;
 template<typename T>
 class SlidingMassMinimumTime : public mesh::OptimalControlProblemNamed<T> {
 public:
+    const double mass = 10.0;
+    const double Fmax = 10;
     SlidingMassMinimumTime()
     {
         // TODO when time is a variable, this has to be more advanced:
-        this->set_time({0}, {1.5, 10});
-        this->add_state("x", {0, 2}, {0}, {1});
-        this->add_state("u", {-10, 10}, {0}, {0});
-        this->add_control("F", {-50, 50});
+        this->set_time({0}, {0, 10});
+        this->add_state("x", {0, 1}, {0}, {1});
+        this->add_state("u", {-100, 100}, {0}, {0});
+        this->add_control("F", {-Fmax, Fmax});
     }
-    const double mass = 10.0;
     void dynamics(const VectorX<T>& states, const VectorX<T>& controls,
                   Ref<VectorX<T>> derivatives) const override
     {
@@ -35,27 +36,60 @@ public:
     {
         cost = final_time;
     }
+    OptimalControlSolution actual_solution(const VectorXd& time) const
+    {
+        OptimalControlSolution sol;
+        sol.time = time;
+        sol.states.resize(2, time.size());
+        sol.controls.resize(1, time.size());
+
+        auto x = [&](const double& t) -> double {
+            if (t < 0.5 * time[time.size()-1]) { return 0.5*t*t; }
+            else { return -0.5*(t - 1)*(t - 1) + 1*(t - 1) + 0.5; }
+        };
+        sol.states.row(0) = sol.time.unaryExpr(x);
+
+        auto u = [&](const double& t) -> double {
+            if (t < 0.5 * time[time.size()-1]) { return t; }
+            else { return 2 - t; }
+        };
+        sol.states.row(1) = sol.time.unaryExpr(u);
+
+        auto F = [&](const double& t) -> double {
+            if (t < 0.5 * time[time.size()-1]) { return Fmax; }
+            else { return -Fmax; }
+        };
+        sol.controls = sol.time.unaryExpr(F);
+
+        return sol;
+    }
 };
 
 TEST_CASE("Sliding mass minimum time.")
 {
-
     auto ocp = std::make_shared<SlidingMassMinimumTime<adouble>>();
-    DirectCollocationSolver<adouble> dircol(ocp, "euler", "snopt");
+    const int halfN = 25;
+    const int N = 2 * halfN;
+    DirectCollocationSolver<adouble> dircol(ocp, "trapezoidal", "ipopt", N);
     OptimalControlSolution solution = dircol.solve();
     solution.write("sliding_mass_minimum_time_solution.csv");
 
-    // Initial and final position.
-    REQUIRE(Approx(solution.states(0, 0)) == 0.0);
-    REQUIRE(Approx(solution.states.rightCols<1>()[0]) == 1.0);
-    // Initial and final speed.
-    REQUIRE(Approx(solution.states(1, 0)) == 0.0);
-    REQUIRE(Approx(solution.states.rightCols<1>()[1]) == 0.0);
+    OptimalControlSolution expected = ocp->actual_solution(solution.time);
 
-    int N = solution.time.size();
-    std::cout << "DEBUG solution.controls " << solution.controls << std::endl;
-    //RowVectorXd expected = RowVectorXd::LinSpaced(N - 1, 14.25, -14.25);
-    //RowVectorXd errors = solution.controls.rightCols(N - 1) - expected;
-    //REQUIRE(Approx(errors.norm()) == 0);
+//    REQUIRE_EIGEN(solution.states, expected.states, 0.001);
+//    REQUIRE_EIGEN(solution.controls, expected.controls, 0.001);
+
+    for (int im = 0; im < solution.time.size(); ++im) {
+        for (int is = 0; is < solution.states.rows(); ++is) {
+            const auto abs_error = std::abs(
+                    solution.states(is, im) - expected.states(is, im));
+            REQUIRE(abs_error < 0.001);
+        }
+        for (int ic = 0; ic < solution.controls.rows(); ++ic) {
+            const auto abs_error = std::abs(
+                    solution.controls(ic, im) - expected.controls(ic, im));
+            REQUIRE(abs_error < 0.001);
+        }
+    }
 }
 
