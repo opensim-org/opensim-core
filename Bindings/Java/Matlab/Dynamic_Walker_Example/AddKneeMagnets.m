aeec4813342657f5c56de835617bf6f727e08765
jimmyDunne <james.dunne@stanford.edu>
2017-01-25 13:16:42 -0800

    Addressed comments for AddKneeMagnets.m

--- Bindings/Java/Matlab/Dynamic_Walker_Example/AddKneeMagnets.m
+++ Bindings/Java/Matlab/Dynamic_Walker_Example/AddKneeMagnets.m
@@ -6,7 +6,7 @@
 % R24 HD065690) and by DARPA through the Warrior Web program.             %
 %                                                                         %
 % Copyright (c) 2005-2016 Stanford University and the Authors             %
-% Author(s): Daniel A. Jacobs, James Dunne
+% Author(s): Daniel A. Jacobs
 %                                                                         %
 % Licensed under the Apache License, Version 2.0 (the "License");         %
 % you may not use this file except in compliance with the License.        %
@@ -19,8 +19,9 @@
 % implied. See the License for the specific language governing            %
 % permissions and limitations under the License.                          %
 % ----------------------------------------------------------------------- %
-% This script uses a ExpressionBasedPointToPointForce to realize a magnet
-% force model.  The ExpressionPointToPointForce calculates the relative
+
+% This script uses an ExpressionBasedPointToPointForce to realize a magnet
+% force model.  The ExpressionBasedPointToPointForce calculates the relative
 % translation and relative velocity between two points in the global frame
 % and allows the user to specify a force based on a string of the symbols
 % d and ddot.
@@ -37,27 +38,27 @@ new_model = walkerModel;
 % Change the name
 new_model.setName('WalkerModel_magnet');
 
-% Create an ExpressionBasedBushingForce for the magnets
+% Create an ExpressionBasedPointToPointForce for the magnets
 leftKneeMagnet = ExpressionBasedPointToPointForce();
 leftKneeMagnet.setName('LeftKneeMagnet');
 rightKneeMagnet = ExpressionBasedPointToPointForce();
 rightKneeMagnet.setName('RightKneeMagnet')
 
-% Define the point locations for where the bushing will attach
-locBody1 = Vec3(0.05,-0.2, 0);
-locBody2 = Vec3(0.05, 0.2, 0);
+% Define the point locations for where the PointToPointForce will attach
+locThigh = Vec3(0.05,-0.2, 0);
+locShank = Vec3(0.05, 0.2, 0);
 
 % Set the first body and the reference frame properties
 leftKneeMagnet.setBody1Name('LeftThigh');
-leftKneeMagnet.setPoint1(locBody1);
+leftKneeMagnet.setPoint1(locThigh);
 rightKneeMagnet.setBody1Name('RightThigh');
-rightKneeMagnet.setPoint1(locBody1);
+rightKneeMagnet.setPoint1(locThigh);
 
 % Set the second body and the location of the reference point
 leftKneeMagnet.setBody2Name('LeftShank');
-leftKneeMagnet.setPoint2(locBody2);
+leftKneeMagnet.setPoint2(locShank);
 rightKneeMagnet.setBody2Name('RightShank');
-rightKneeMagnet.setPoint2(locBody2);
+rightKneeMagnet.setPoint2(locShank);
 
 % Set the expression to represent a magnet (0.01/d^2) force.
 % d is the distance between the two points of ExpressionBasedPointToPointForce
@@ -69,4 +70,7 @@ new_model.addComponent(leftKneeMagnet);
 new_model.addComponent(rightKneeMagnet);
 
 % Print a new model file
-new_model.print(['../Model/WalkerModel_KneeMagnet.osim']);
+newFilename = 'WalkerModel_KneeMagnet.osim';
+isSuccessful = model.print(['../Model/',newFilename]);
+if (~isSuccessful), error('Model printed to file failed'); end
+fprintf('Model printed to file successfully\n');
