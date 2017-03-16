d08907ca17a6f8c2453d22a8d82bc461ce675c6d
Christopher Dembia <cld72@cornell.edu>
2017-03-16 13:53:45 -0700

    Create DeGroote2016Muscle.h

---
+++ MuscleRedundancySolver/MuscleRedundancySolver/DeGroote2016Muscle.h
@@ -0,0 +1,154 @@
+#ifndef MRS_DEGROOTE2016MUSCLE_H_
+#define MRS_DEGROOTE2016MUSCLE_H_
+
+/// This class template implements the muscle model from De Groote et al.,
+/// 2016, Evaluation of Direct Collocation Optimal Control Problem
+/// Formulations for Solving the Muscle Redundancy Problem (see supplemental
+/// materials), and is partially based on the implementation at
+/// http://simtk.org/projects/optcntrlmuscle.
+/// This class template is not part of OpenSim.
+template <typename T>
+class DeGroote2016Muscle {
+public:
+    DeGroote2016Muscle() = default;
+    DeGroote2016Muscle(const double& max_isometric_force,
+                       const double& optimal_fiber_length,
+                       const double& tendon_slack_length,
+                       const double& pennation_angle_at_optimal,
+                       const double& max_contraction_velocity) :
+            m_max_isometric_force(max_isometric_force),
+            m_optimal_fiber_length(optimal_fiber_length),
+            m_tendon_slack_length(tendon_slack_length),
+            m_pennation_angle_at_optimal(pennation_angle_at_optimal),
+            m_max_contraction_velocity(max_contraction_velocity) {
+        m_fiber_width = optimal_fiber_length*sin(pennation_angle_at_optimal);
+    }
+    T calcNormTendonForce(const T& musTenLength,
+                          const T& normFiberLength) const {
+        // Tendon force-length curve.
+        static const double kT = 35;
+        static const double c1 = 0.200;
+        static const double c2 = 0.995;
+        static const double c3 = 0.250;
+        // TODO computing tendon force is why we'd want a single "continuous"
+        // function rather than separate dynamics and path constraints
+        // functions.
+        const T fibLen = normFiberLength * m_optimal_fiber_length;
+        // TODO cache this somewhere; this is constant.
+        const double fibWidth = m_optimal_fiber_length
+                * sin(m_pennation_angle_at_optimal);
+        // Tendon length.
+        // lT = lMT - sqrt(lM^2 - w^2)
+        const T tenLen = musTenLength - sqrt(fibLen*fibLen - fibWidth*fibWidth);
+        const T normTenLen = tenLen / m_tendon_slack_length;
+        return c1 * exp(kT * (normTenLen - c2)) - c3;
+    }
+    void calcTendonForce(const T& musTenLength, const T& normFiberLength,
+                         T& tendonForce) const {
+        tendonForce = m_max_isometric_force
+                * calcNormTendonForce(musTenLength, normFiberLength);
+    }
+    void calcActivationDynamics(const T& excitation, const T& activation,
+                                T& activationDot) const {
+        static const double actTimeConst   = 0.015;
+        static const double deactTimeConst = 0.060;
+        static const double tanhSteepness  = 0.1;
+        //     f = 0.5 tanh(b(e - a))
+        //     z = 0.5 + 1.5a
+        // da/dt = [(f + 0.5)/(tau_a * z) + (-f + 0.5)*z/tau_d] * (e - a)
+        const T timeConstFactor = 0.5 + 1.5 * activation;
+        const T tempAct = 1.0 / (actTimeConst * timeConstFactor);
+        const T tempDeact = timeConstFactor / deactTimeConst;
+        const T f = 0.5 * tanh(tanhSteepness * (excitation - activation));
+        const T timeConst = tempAct * (f + 0.5) + tempDeact * (-f + 0.5);
+        activationDot = timeConst * (excitation - activation);
+    }
+    void calcEquilibriumResidual(const T& musTenLength,
+                                 const T& activation,
+                                 const T& normFiberLength,
+                                 const T& normFiberVelocity,
+                                 T& residual) const {
+        // Curve parameters.
+        // -----------------
+        // Active force-length curve.
+        static const double b11 =  0.815;
+        static const double b21 =  1.055;
+        static const double b31 =  0.162;
+        static const double b41 =  0.063;
+        static const double b12 =  0.433;
+        static const double b22 =  0.717;
+        static const double b32 = -0.030;
+        static const double b42 =  0.200;
+        static const double b13 =  0.100;
+        static const double b23 =  1.000;
+        static const double b33 =  0.354;
+        static const double b43 =  0.000;
+
+        // Passive force-length curve.
+        static const double kPE = 4.0;
+        static const double e0  = 0.6;
+
+        // Muscle force-velocity.
+        static const double d1 = -0.318;
+        static const double d2 = -8.149;
+        static const double d3 = -0.374;
+        static const double d4 =  0.886;
+
+        // Intermediate quantities.
+        // ------------------------
+        const T fibLength = normFiberLength*m_optimal_fiber_length;
+        // Tendon length.
+        // lT = lMT - sqrt(lM^2 - w^2)
+        const T tenLength = musTenLength
+                - sqrt(fibLength*fibLength - m_fiber_width*m_fiber_width);
+        const T cosPenn = (musTenLength - tenLength)/fibLength;
+
+        // Curves/multipliers.
+        // -------------------
+        // Tendon force-length curve.
+        const T normTenForce = calcNormTendonForce(musTenLength,
+                                                   normFiberLength);
+
+        // Active force-length curve.
+        // Sum of 3 gaussians.
+        const T activeForceLenMult =
+                gaussian(normFiberLength, b11, b21, b31, b41) +
+                        gaussian(normFiberLength, b12, b22, b32, b42) +
+                        gaussian(normFiberLength, b13, b23, b33, b43);
+
+        // Passive force-length curve.
+        const T passiveFibForce = (exp(kPE*(normFiberLength - 1)/e0) - 1)/
+                (exp(kPE) - 1);
+
+        // Force-velocity curve.
+        const T tempV = d2*normFiberVelocity + d3;
+        const T tempLogArg = tempV + sqrt(pow(tempV, 2) + 1);
+        const T forceVelMult = d1*log(tempLogArg) + d4;
+
+        // Equilibrium constraint.
+        // -----------------------
+        const T normFibForce =
+                activation*activeForceLenMult*forceVelMult
+                        + passiveFibForce;
+        const T normFibForceAlongTen = normFibForce*cosPenn;
+
+        residual = normFibForceAlongTen - normTenForce;
+    }
+
+protected:
+    static T gaussian(const T& x, const double& b1, const double& b2,
+                      const double& b3, const double& b4) {
+        return b1*exp((-0.5*pow(x - b2, 2))/(b3 + b4*x));
+    };
+
+private:
+    constexpr static double NaN = std::numeric_limits<double>::quiet_NaN();
+    double m_max_isometric_force = NaN;
+    double m_optimal_fiber_length = NaN;
+    double m_tendon_slack_length = NaN;
+    double m_pennation_angle_at_optimal = NaN;
+    double m_max_contraction_velocity = NaN;
+    double m_fiber_width = NaN;
+};
+
+#endif // MRS_DEGROOTE2016MUSCLE_H_
