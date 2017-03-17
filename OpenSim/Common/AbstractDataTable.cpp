39819287d2fa0334aac438df943e3ed5990bf4e6
Shrinidhi KL <klshrinidhi@gmail.com>
2017-03-17 16:08:34 -0700

    Add appendColumnLabel.

--- OpenSim/Common/AbstractDataTable.cpp
+++ OpenSim/Common/AbstractDataTable.cpp
@@ -25,163 +25,177 @@
 
 namespace OpenSim {
 
-    size_t AbstractDataTable::getNumRows() const {
-        return implementGetNumRows();
-    }
+size_t AbstractDataTable::getNumRows() const {
+    return implementGetNumRows();
+}
 
-    size_t AbstractDataTable::getNumColumns() const {
-        return implementGetNumColumns();
-    }
+size_t AbstractDataTable::getNumColumns() const {
+    return implementGetNumColumns();
+}
 
-    void 
-    AbstractDataTable::removeTableMetaDataKey(const std::string& key) {
-        _tableMetaData.removeValueForKey(key);
-    }
+void 
+AbstractDataTable::removeTableMetaDataKey(const std::string& key) {
+    _tableMetaData.removeValueForKey(key);
+}
 
-    std::vector<std::string> 
-    AbstractDataTable::getTableMetaDataKeys() const {
-        return _tableMetaData.getKeys();
-    }
+std::vector<std::string> 
+AbstractDataTable::getTableMetaDataKeys() const {
+    return _tableMetaData.getKeys();
+}
 
-    const AbstractDataTable::TableMetaData& 
-    AbstractDataTable::getTableMetaData() const {
-        return _tableMetaData;
-    }
+const AbstractDataTable::TableMetaData& 
+AbstractDataTable::getTableMetaData() const {
+    return _tableMetaData;
+}
 
-    AbstractDataTable::TableMetaData& 
-    AbstractDataTable::updTableMetaData() {
-        return _tableMetaData;
-    }
+AbstractDataTable::TableMetaData& 
+AbstractDataTable::updTableMetaData() {
+    return _tableMetaData;
+}
 
-    const AbstractDataTable::IndependentMetaData& 
-    AbstractDataTable::getIndependentMetaData() const {
-        return _independentMetaData;
-    }
-    
-    void 
-    AbstractDataTable::setIndependentMetaData(const IndependentMetaData& 
-                                              independentMetaData) {
-        _independentMetaData = independentMetaData;
-        validateIndependentMetaData();
-    }
+const AbstractDataTable::IndependentMetaData& 
+AbstractDataTable::getIndependentMetaData() const {
+    return _independentMetaData;
+}
 
-    const AbstractDataTable::DependentsMetaData& 
-    AbstractDataTable::getDependentsMetaData() const {
-        return _dependentsMetaData;
-    }
+void 
+AbstractDataTable::setIndependentMetaData(const IndependentMetaData& 
+                                          independentMetaData) {
+    _independentMetaData = independentMetaData;
+    validateIndependentMetaData();
+}
 
-    void 
-    AbstractDataTable::setDependentsMetaData(const DependentsMetaData& 
-                                             dependentsMetaData) {
-        _dependentsMetaData = dependentsMetaData;
-        validateDependentsMetaData();
-    }
+const AbstractDataTable::DependentsMetaData& 
+AbstractDataTable::getDependentsMetaData() const {
+    return _dependentsMetaData;
+}
 
-    bool
-    AbstractDataTable::hasColumnLabels() const {
-        return _dependentsMetaData.hasKey("labels");
-    }
+void 
+AbstractDataTable::setDependentsMetaData(const DependentsMetaData& 
+                                         dependentsMetaData) {
+    _dependentsMetaData = dependentsMetaData;
+    validateDependentsMetaData();
+}
 
-    std::vector<std::string> 
-    AbstractDataTable::getColumnLabels() const {
-        OPENSIM_THROW_IF(!hasColumnLabels(),
-                         NoColumnLabels);
-        
-        const auto& absArray = 
-            _dependentsMetaData.getValueArrayForKey("labels");
-        std::vector<std::string> labels{};
-        for(size_t i = 0; i < absArray.size(); ++i)
-            labels.push_back(absArray[i].getValue<std::string>());
+void
+AbstractDataTable::removeDependentsMetaDataForKey(const std::string& key) {
+    _dependentsMetaData.removeValueForKey(key);
+}
 
-        return labels;
-    }
+bool
+AbstractDataTable::hasColumnLabels() const {
+    return _dependentsMetaData.hasKey("labels");
+}
 
-    const std::string& 
-    AbstractDataTable::getColumnLabel(const size_t columnIndex) const {
-        OPENSIM_THROW_IF(!hasColumnLabels(),
-                         NoColumnLabels);
+std::vector<std::string> 
+AbstractDataTable::getColumnLabels() const {
+    OPENSIM_THROW_IF(!hasColumnLabels(),
+                     NoColumnLabels);
 
-        const auto& labels = 
-            _dependentsMetaData.getValueArrayForKey("labels");
-        
-        OPENSIM_THROW_IF(columnIndex >= labels.size(),
-                         ColumnIndexOutOfRange,
-                         columnIndex, 0, 
-                         static_cast<unsigned>(labels.size() - 1));
+    const auto& absArray = 
+        _dependentsMetaData.getValueArrayForKey("labels");
+    std::vector<std::string> labels{};
+    for(size_t i = 0; i < absArray.size(); ++i)
+        labels.push_back(absArray[i].getValue<std::string>());
 
-        return labels[columnIndex].getValue<std::string>();
-    }
+    return labels;
+}
 
-    void 
-    AbstractDataTable::setColumnLabels(const std::initializer_list<std::string>&
-                                       columnLabels) {
-        setColumnLabels(columnLabels.begin(), columnLabels.end());
-    }
+const std::string& 
+AbstractDataTable::getColumnLabel(const size_t columnIndex) const {
+    OPENSIM_THROW_IF(!hasColumnLabels(),
+                     NoColumnLabels);
 
-    void 
-    AbstractDataTable::setColumnLabel(const size_t columnIndex,
-                                      const std::string& columnLabel) {
-        using namespace SimTK;
-        using namespace std;
+    const auto& labels = 
+        _dependentsMetaData.getValueArrayForKey("labels");
 
-        OPENSIM_THROW_IF(!hasColumnLabels(),
-                         NoColumnLabels);
+    OPENSIM_THROW_IF(columnIndex >= labels.size(),
+                     ColumnIndexOutOfRange,
+                     columnIndex, 0, 
+                     static_cast<unsigned>(labels.size() - 1));
 
-        ValueArray<std::string> newLabels{};
-        const auto& oldLabels = 
-            _dependentsMetaData.getValueArrayForKey("labels");
+    return labels[columnIndex].getValue<std::string>();
+}
 
-        OPENSIM_THROW_IF(columnIndex >= oldLabels.size(),
-                         ColumnIndexOutOfRange,
-                         columnIndex, 0, 
-                         static_cast<unsigned>(oldLabels.size() - 1));
+void 
+AbstractDataTable::setColumnLabels(const std::initializer_list<std::string>&
+                                   columnLabels) {
+    setColumnLabels(columnLabels.begin(), columnLabels.end());
+}
 
-        for(unsigned i = 0; i < oldLabels.size(); ++i) {
-            if(i == columnIndex) {
-                newLabels.upd().push_back(Value<string>(columnLabel));
-            } else {
-                auto value = Value<string>(oldLabels[i].getValue<string>());
-                newLabels.upd().push_back(value);
-            }
+void 
+AbstractDataTable::setColumnLabel(const size_t columnIndex,
+                                  const std::string& columnLabel) {
+    using namespace SimTK;
+    using namespace std;
+
+    OPENSIM_THROW_IF(!hasColumnLabels(),
+                     NoColumnLabels);
+
+    ValueArray<std::string> newLabels{};
+    const auto& oldLabels = 
+        _dependentsMetaData.getValueArrayForKey("labels");
+
+    OPENSIM_THROW_IF(columnIndex >= oldLabels.size(),
+                     ColumnIndexOutOfRange,
+                     columnIndex, 0, 
+                     static_cast<unsigned>(oldLabels.size() - 1));
+
+    for(unsigned i = 0; i < oldLabels.size(); ++i) {
+        if(i == columnIndex) {
+            newLabels.upd().push_back(Value<string>(columnLabel));
+        } else {
+            auto value = Value<string>(oldLabels[i].getValue<string>());
+            newLabels.upd().push_back(value);
         }
-
-        _dependentsMetaData.removeValueArrayForKey("labels");
-        _dependentsMetaData.setValueArrayForKey("labels", newLabels);
-
-        validateDependentsMetaData();
     }
 
-    size_t 
-    AbstractDataTable::getColumnIndex(const std::string& columnLabel) const {
-        OPENSIM_THROW_IF(!hasColumnLabels(),
-                         NoColumnLabels);
+    _dependentsMetaData.removeValueArrayForKey("labels");
+    _dependentsMetaData.setValueArrayForKey("labels", newLabels);
 
-        const auto& absArray = 
-            _dependentsMetaData.getValueArrayForKey("labels");
-        for(size_t i = 0; i < absArray.size(); ++i)
-            if(absArray[i].getValue<std::string>() == columnLabel)
-                return i;
-        
-        OPENSIM_THROW(KeyNotFound, columnLabel);
-    }
+    validateDependentsMetaData();
+}
 
-    bool 
-    AbstractDataTable::hasColumn(const std::string& columnLabel) const {
-        OPENSIM_THROW_IF(!hasColumnLabels(),
-                         NoColumnLabels);
+size_t 
+AbstractDataTable::getColumnIndex(const std::string& columnLabel) const {
+    OPENSIM_THROW_IF(!hasColumnLabels(),
+                     NoColumnLabels);
 
-        const auto& absArray = 
-            _dependentsMetaData.getValueArrayForKey("labels");
-        for(size_t i = 0; i < absArray.size(); ++i)
-            if(absArray[i].getValue<std::string>() == columnLabel)
-                return true;
+    const auto& absArray = 
+        _dependentsMetaData.getValueArrayForKey("labels");
+    for(size_t i = 0; i < absArray.size(); ++i)
+        if(absArray[i].getValue<std::string>() == columnLabel)
+            return i;
 
-        return false;
-    }
+    OPENSIM_THROW(KeyNotFound, columnLabel);
+}
 
-    bool 
-    AbstractDataTable::hasColumn(const size_t columnIndex) const {
-        return columnIndex < getNumColumns();
-    }
+bool 
+AbstractDataTable::hasColumn(const std::string& columnLabel) const {
+    OPENSIM_THROW_IF(!hasColumnLabels(),
+                     NoColumnLabels);
+
+    const auto& absArray = 
+        _dependentsMetaData.getValueArrayForKey("labels");
+    for(size_t i = 0; i < absArray.size(); ++i)
+        if(absArray[i].getValue<std::string>() == columnLabel)
+            return true;
+
+    return false;
+}
+
+bool 
+AbstractDataTable::hasColumn(const size_t columnIndex) const {
+    return columnIndex < getNumColumns();
+}
+
+void
+AbstractDataTable::appendColumnLabel(const std::string& columnLabel) {
+    auto& absArray = _dependentsMetaData.updValueArrayForKey("labels");
+    auto& labels = static_cast<ValueArray<std::string>&>(absArray);
+    labels.upd().push_back(SimTK::Value<std::string>{columnLabel});
+
+    validateDependentsMetaData();
+}
 
 } // namespace OpenSim
