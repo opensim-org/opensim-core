9b59eda5e450117462f74bd9e52e45e49f708430
jimmyDunne <james.dunne@stanford.edu>
2017-04-20 15:39:21 -0700

    moved files

--- Bindings/Java/Matlab/Utilities/prescribeMotionInModel.m
+++ Bindings/Java/Matlab/Utilities/prescribeMotionInModel.m
@@ -1,30 +1,4 @@
-% ----------------------------------------------------------------------- %
-% The OpenSim API is a toolkit for musculoskeletal modeling and           %
-% simulation. See http://opensim.stanford.edu and the NOTICE file         %
-% for more information. OpenSim is developed at Stanford University       %
-% and supported by the US National Institutes of Health (U54 GM072970,    %
-% R24 HD065690) and by DARPA through the Warrior Web program.             %
-%                                                                         %   
-% Copyright (c) 2005-2017 Stanford University and the Authors             %
-% Author(s): Dominic Farris                                               %
-%                                                                         %
-% Licensed under the Apache License, Version 2.0 (the "License");         %
-% you may not use this file except in compliance with the License.        %
-% You may obtain a copy of the License at                                 %
-% http://www.apache.org/licenses/LICENSE-2.0.                             %
-%                                                                         % 
-% Unless required by applicable law or agreed to in writing, software     %
-% distributed under the License is distributed on an "AS IS" BASIS,       %
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
-% implied. See the License for the specific language governing            %
-% permissions and limitations under the License.                          %
-% ----------------------------------------------------------------------- %
-
-% prescribeMotionInModel.m                                                        
-% Author: Dominic Farris
-
 function prescribeMotionInModel(Model_In, Mot_In, Model_Out)
-
 % Function to take an existing model file and coordinate data accessed from an IK solution
 % and write it as a Natural Cubic Spline Function to the Prescribed
 % Function method of a Coordinate to a model file. Based off work done by
@@ -35,19 +9,19 @@ function prescribeMotionInModel(Model_In, Mot_In, Model_Out)
 %        - Model_Out - The output file with prescribed motion
 %
 % e.g. prescribedMotionInModel('myInputModel.osim','myMotionFile', 'myOutputModel.osim')
-%
+
 % Author - Dominic Farris (North Carolina State University). Please
 % acknowledge contribution in published academic works
 % last updated - 17/07/2012
 
+%% load java libraries
 import org.opensim.modeling.*
 
-% Argument checking
+%% Argument checking
 error(nargchk(0, 3, nargin));
 
 % If there aren't enough arguments passed in system will ask user to
 % manually select file(s)
-
 if nargin < 1
     [Model_In, modelpath] = uigetfile('.osim', 'Please select a model file');
     [Mot_In, motpath] = uigetfile('.mot', 'Please select a motion file');
