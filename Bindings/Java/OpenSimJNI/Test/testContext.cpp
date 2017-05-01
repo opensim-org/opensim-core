409bd7ca5756ce13fdae5e485bd5056bac5515b9
Ayman Habib <ahabib@stanford.edu>
2017-05-01 11:36:01 -0700

    Exercise methods to deletePathPoint, addPathPoint and replacePathPoint

--- Bindings/Java/OpenSimJNI/Test/testContext.cpp
+++ Bindings/Java/OpenSimJNI/Test/testContext.cpp
@@ -190,13 +190,33 @@ int main()
 
     // Exercise PathPoint operations used to edit Path in GUI
     PathPointSet& pathPoints = dTRIlong->updGeometryPath().updPathPointSet();
+    std::string pathBeforeInXML = dTRIlong->updGeometryPath().dump();
+    std::cout << pathBeforeInXML << endl;
+    int origSize = pathPoints.getSize();
     AbstractPathPoint& savePoint = pathPoints.get("TRIlong-P2");
-    const PhysicalFrame& saveBody = savePoint.getParentFrame();
+    const std::string& saveFrameName = savePoint.getParentFrame().getName();
     AbstractPathPoint* clonedPoint = savePoint.clone();
     // Delete second PathPoint from TRIlong muscle
     context->deletePathPoint(dTRIlong->updGeometryPath(), 2); 
+    assert(pathPoints.getSize() == origSize - 1);
+    std::string pathAfterDeletionInXML = dTRIlong->updGeometryPath().dump();
+    std::cout << pathAfterDeletionInXML << endl;
     // TODO Insert new PathPoint same location 
-    // TODO change type of a path point from Stationary to Via or Moving or vice versa
+    Component& frame = model->updComponent(saveFrameName);
+    PhysicalFrame* physFrame = PhysicalFrame::safeDownCast(&frame);
+    context->addPathPoint(dTRIlong->updGeometryPath(), 3, *physFrame);
+    assert(pathPoints.getSize() == origSize);
+    std::string pathAfterReinsertionInXML = dTRIlong->updGeometryPath().dump();
+    std::cout << pathAfterReinsertionInXML << endl;
+    ConditionalPathPoint* newPoint = new ConditionalPathPoint();
+    AbstractPathPoint& oldPoint = pathPoints.get(2);
+    newPoint->setCoordinate(model->getCoordinateSet().get(0));
+    newPoint->setParentFrame(oldPoint.getParentFrame());
+    context->replacePathPoint(dTRIlong->updGeometryPath(), oldPoint, *newPoint);
+    assert(pathPoints.getSize() == origSize);
+    std::string pathAfterTypeChangeToViaInXML = dTRIlong->updGeometryPath().dump();
+    std::cout << pathAfterTypeChangeToViaInXML << endl;
+ 
     return status;
   } catch (const std::exception& e) {
       cout << "Exception: " << e.what() << endl;
