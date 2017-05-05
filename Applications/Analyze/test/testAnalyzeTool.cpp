562a3bf71c0ac2d756f8537906b867ebf9abf2da
Ajay Seth <aseth@stanford.edu>
2017-05-05 15:42:11 -0700

    Restore testTutorialOne() test case
    And update header to reflect rename and authors.

--- Applications/Analyze/test/testAnalyzeTool.cpp
+++ Applications/Analyze/test/testAnalyzeTool.cpp
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------- *
- *                    OpenSim:  testAnalyzeTutorialOne.cpp                    *
+ *                       OpenSim:  testAnalyzeTool.cpp                        *
  * -------------------------------------------------------------------------- *
  * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
  * See http://opensim.stanford.edu and the NOTICE file for more information.  *
@@ -8,7 +8,7 @@
  * through the Warrior Web program.                                           *
  *                                                                            *
  * Copyright (c) 2005-2017 Stanford University and the Authors                *
- * Author(s): Ayman Habib                                                     *
+ * Author(s): Ayman Habib, Ajay Seth                                          *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
@@ -43,7 +43,7 @@ int main()
 {
     SimTK::Array_<std::string> failures;
 
-    try { /*testTutorialOne();*/ }
+    try { testTutorialOne(); }
     catch (const std::exception& e) {
         cout << e.what() << endl; failures.push_back("testTutorialOne");
     }
