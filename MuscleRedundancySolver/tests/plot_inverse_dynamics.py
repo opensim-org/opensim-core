f7515ee585a3c22872dc8952e9b06347995b0cdd
Christopher Dembia <cld72@cornell.edu>
2017-05-19 13:00:56 -0700

    Generalize plot_inverse_dynamics.py

--- MuscleRedundancySolver/tests/plot_inverse_dynamics.py
+++ MuscleRedundancySolver/tests/plot_inverse_dynamics.py
@@ -1,15 +1,22 @@
+import sys
+
 import pylab as pl
 import pandas as pd
 
+if len(sys.argv) != 2:
+    raise Exception("Requires actual inverse dynamics csv file as argument.")
+
 filtered = pd.read_csv('DEBUG_desiredMoments.csv', index_col=0, header=None)
 
-actual = pd.read_csv('DEBUG_testTugOfWar_GSO_actualInvDyn.csv', index_col=0,
-                     skiprows=3)
+actual = pd.read_csv(sys.argv[1], index_col=0, skiprows=3)
 
 fig = pl.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.plot(filtered.index, filtered[filtered.columns[0]], label='filtered')
-ax.plot(actual.index, actual[actual.columns[0]], label='actual')
-pl.legend()
+num_columns = len(filtered.columns)
+for i in range(num_columns):
+
+    ax = fig.add_subplot(num_columns, 1, i + 1)
+    ax.plot(filtered.index, filtered[filtered.columns[i]], label='filtered')
+    ax.plot(actual.index, actual[actual.columns[i]], label='actual')
+    pl.legend()
 
 pl.show()
