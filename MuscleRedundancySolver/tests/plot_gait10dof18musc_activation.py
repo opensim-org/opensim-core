1944d677197a57e9f5aa185ac554a377a88c26f6
Christopher Dembia <cld72@cornell.edu>
2017-05-26 18:29:18 -0700

    Add testGait10do18musc (though it fails).

---
+++ MuscleRedundancySolver/tests/plot_gait10dof18musc_activation.py
@@ -0,0 +1,58 @@
+import pylab as pl
+import pandas as pd
+import math
+
+cycle_start = 0.58
+cycle_end = 1.81
+duration_of_gait_cycle = cycle_end - cycle_start
+half_gait_cycle = 0.5 * duration_of_gait_cycle 
+
+muscles = ['glut_max', 'iliopsoas', 'rect_fem',
+           'hamstrings', 'bifemsh', 'vasti',
+           'gastroc', 'soleus', 'tib_ant']
+
+sol = pd.read_csv('GlobalStaticOptimizationSolver_OCP_solution.csv', index_col=0,
+        skiprows=2)
+#sol = pd.read_csv('MuscleRedundancySolver_OCP_solution.csv', index_col=0,
+#        skiprows=2)
+# sol.plot()
+#num_muscles = 0
+#plot_names = list()
+#for col in sol.columns:
+#    if col.endswith('activation'):
+#        num_muscles += 1
+#        plot_names.append(col)
+col_indices_r = list()
+col_indices_l = list()
+for muscle in muscles:
+    for i, col in enumerate(sol.columns):
+        if muscle + '_r' in col and 'activation' in col:
+            col_indices_r.append(i)
+        if muscle + '_l' in col and 'activation' in col:
+            col_indices_l.append(i)
+
+
+
+num_cols = 3
+num_rows = 3 #math.ceil(float(num_muscles) / num_cols)
+pl.figure(figsize=(4 * num_cols, 3 * num_rows))
+pgc_r = 100.0 * (sol.index - cycle_start) / duration_of_gait_cycle
+for i in range(len(muscles)):
+    ax = pl.subplot(num_rows, num_cols, i + 1)
+    col_label_r = sol.columns[col_indices_r[i]]
+    ax.plot(pgc_r, sol[col_label_r])
+    #col_label_l = sol.columns[col_indices_l[i]]
+    #ax.plot(sol.index + half_gait_cycle, sol[col_label_l])
+    ax.set_title(col_label_r.split('/')[-1].replace('_r_activation', ''))
+    ax.set_ylim(0, 1)
+    if i == 3:
+        ax.set_ylabel('activation')
+    if i < 6:
+        ax.set_xticklabels([])
+    i_col = i % num_cols
+    if i_col > 0:
+        ax.set_yticklabels([])
+    if i == 7:
+        ax.set_xlabel('time (% gait cycle)')
+
+pl.savefig('gait10dof18musc_activation.png')
---
+++ MuscleRedundancySolver/tests/testGait10dof18musc.cpp
@@ -0,0 +1,28 @@
+
+#include <GlobalStaticOptimizationSolver.h>
+#include <MuscleRedundancySolver.h>
+#include "testing.h"
+
+using namespace OpenSim;
+
+void testGait10dof18musc_GSO() {
+    // TODO muscle-tendon velocities are zero, as expected.
+    // TODO why is this not an issue for other test cases? because we provide
+    // the speed states.
+    GlobalStaticOptimizationSolver gso("testGait10dof18musc_GSO_setup.xml");
+    GlobalStaticOptimizationSolver::Solution solution = gso.solve();
+    solution.write("testGait10dof18musc_GSO_solution");
+}
+
+void testGait10dof18musc_MRS() {
+    MuscleRedundancySolver mrs("testGait10dof18musc_MRS_setup.xml");
+    MuscleRedundancySolver::Solution solution = mrs.solve();
+    solution.write("testGait10dof18musc_MRS_solution");
+}
+
+int main() {
+    SimTK_START_TEST("testGait10dof18musc");
+        // TODO SimTK_SUBTEST(testGait10dof18musc_GSO);
+        SimTK_SUBTEST(testGait10dof18musc_MRS);
+    SimTK_END_TEST();
+}
