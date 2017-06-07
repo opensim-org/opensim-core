e30e19de46b5aeca002fe174181dc204ec0871aa
Ajay Seth <aseth@stanford.edu>
2017-06-07 14:25:04 -0700

    Change the shoulder model with muscles wrapping to non-stiff PathSprings. The PathSprings were tuned so that reset-lengths are equivalent to the paths lengths in the default pose. Decreased simulation time from 80s for a 0.02s to 3s for a 0.1s simulation.

--- OpenSim/Tests/Wrapping/testWrapping.cpp
+++ OpenSim/Tests/Wrapping/testWrapping.cpp
@@ -61,8 +61,8 @@ int main()
 
     try{
         testWrapCylinder();
-        // performance with multiple muscles and wrapping in upper-extremity
-        simulateModelWithMusclesNoViz("TestShoulderModel.osim", 0.02);}
+        // performance of multiple paths with wrapping in upper-extremity
+        simulateModelWithMusclesNoViz("TestShoulderWrapping.osim", 0.1);}
     catch (const std::exception& e) {
         std::cout << "Exception: " << e.what() << std::endl;
         failures.push_back("TestShoulderModel (multiple wrap)"); }
