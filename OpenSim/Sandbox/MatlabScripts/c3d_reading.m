025f55b781e91f989648fa2b413606dbb6f22819
jimmyDunne <james.dunne@stanford.edu>
2017-06-13 15:02:18 -0700

    updates

--- OpenSim/Sandbox/MatlabScripts/c3d_reading.m
+++ OpenSim/Sandbox/MatlabScripts/c3d_reading.m
@@ -1,32 +1,6 @@
-% ----------------------------------------------------------------------- %
-% The OpenSim API is a toolkit for musculoskeletal modeling and           %
-% simulation. See http://opensim.stanford.edu and the NOTICE file         %
-% for more information. OpenSim is developed at Stanford University       %
-% and supported by the US National Institutes of Health (U54 GM072970,    %
-% R24 HD065690) and by DARPA through the Warrior Web program.             %
-%                                                                         %
-% Copyright (c) 2005-2017 Stanford University and the Authors             %
-% Author(s): James Dunne                                                  %
-%                                                                         %
-% Licensed under the Apache License, Version 2.0 (the "License");         %
-% you may not use this file except in compliance with the License.        %
-% You may obtain a copy of the License at                                 %
-% http://www.apache.org/licenses/LICENSE-2.0.                             %
-%                                                                         %
-% Unless required by applicable law or agreed to in writing, software     %
-% distributed under the License is distributed on an "AS IS" BASIS,       %
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
-% implied. See the License for the specific language governing            %
-% permissions and limitations under the License.                          %
-% ----------------------------------------------------------------------- %
-
-% Author: James Dunne, Tom Uchida, Shrinidhi K. Lakshmikanth, Chris Dembia, 
-% Ajay Seth, Ayman Habib, Jen Hicks.
-
-%% Utility function for converting c3d data to .trc and .mot format. 
-% c3d_reading(varargin)
+function c3d_reading(varargin)
+% Utility function for converting c3d data to OpenSim format.
 % Inputs to function are pairs of string-string or string-number pairs.
-%
 % 'filepath', 'path2file'   reads c3d data at path2file.
 % 'firstRotation'           num applies first rotation of num (dble)
 % 'axis', 'X'               applies first rotation to axis X (string)
@@ -38,7 +12,10 @@
 %                 'firstRotation', '90'...
 %                 'axis', 'X')
 
-function c3d_reading(varargin)
+% Author: James Dunne, Tom Uchida, Shrinidhi K. Lakshmikanth, Chris Dembia, 
+% Ajay Seth, Ayman Habib, Jen Hicks.
+
+
 p = inputParser;
 value = 0;
 defaultFilePath = '';
