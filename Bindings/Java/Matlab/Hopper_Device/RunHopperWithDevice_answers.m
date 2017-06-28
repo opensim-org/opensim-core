8e9b5cecc4d5efee7c25cfe80cea5eaaa84b42ce
Christopher Dembia <cld72@cornell.edu>
2017-06-28 00:26:55 -0700

    RunHopperWithDevice: Add helpful notes to comments.

--- Bindings/Java/Matlab/Hopper_Device/RunHopperWithDevice_answers.m
+++ Bindings/Java/Matlab/Hopper_Device/RunHopperWithDevice_answers.m
@@ -73,6 +73,7 @@ anchorB.connectSocket_parent_frame(shankAttach);
 % ANSWER{
 hopper.addComponent(device);
 % }
+% Note: After this step, simulating will not work until after completing Task F.
 
 % (Done for you) Configure the device to wrap over the patella.
 if hopper.hasComponent('device_active') || hopper.hasComponent('device_passive') 
@@ -105,8 +106,9 @@ contr.connectInput_activation(...
 %% Report quantities of interest.
 % -------------------------------
 % Configure the outputs we wish to display during the simulation.
-% TODO: Create a TableReporter, give it a name, and set its reporting interval
-%       to 0.2 seconds. Wire the following outputs to the reporter:
+% TODO: Create a TableReporter (assign it to a variable named `reporter`),
+%       set a name for the reporter, and set its reporting interval to 0.2
+%       seconds. Wire the following outputs to the reporter:
 %         - hopper's height,
 %         - vastus muscle activation,
 %         - device controller's control signal output.
