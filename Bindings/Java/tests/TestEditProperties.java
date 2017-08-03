d1ada1f2d4a2a75aa52a81985b21737ebf57a5c7
aymanhab <ahabib@stanford.edu>
2017-08-03 10:55:10 -0700

    Clarify what block tests what

--- Bindings/Java/tests/TestEditProperties.java
+++ Bindings/Java/tests/TestEditProperties.java
@@ -40,15 +40,19 @@ class TestEditProperties {
         }
         // Now edit a Vec6 Property (inertia)
         AbstractProperty inertiaProp = bdy.getPropertyByName("inertia");
+        // This block populates a stock Vec6 from a prestored inertia property
+        // 0.10423369488353 0.08831473564548 0.05870749935560 0 0 0
         Vec6 oldInertia = new Vec6();
         for(int i=0; i<6; i++){
             oldInertia.set(i, PropertyHelper.getValueVec6(inertiaProp, i));
             System.out.println(oldInertia.get(i));
         }
+        // This block sets inertia to [0,1,2,3,4,5]
         for(int i=0; i<6; i++){
             // Bad inertia matrix but we're just testing set/get'
             PropertyHelper.setValueVec6(i, inertiaProp, i); 
         }
+        // This block makes sure we can successfully retrieve what we set.
         Vec6 newInertia = new Vec6();
         for(int i=0; i<6; i++){
             double ret = PropertyHelper.getValueVec6(inertiaProp, i);
