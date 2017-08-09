3a4a2112f93871818e18105839e50ea76c8ec19f
Christopher Dembia <cld72@cornell.edu>
2017-08-09 15:46:48 -0700

    Flesh out command-line tool opensim-mrs.

--- MuscleRedundancySolver/MuscleRedundancySolver/opensim-mrs.cpp
+++ MuscleRedundancySolver/MuscleRedundancySolver/opensim-mrs.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "GlobalStaticOptimizationSolver.h"
 #include "MuscleRedundancySolver.h"
 
+using namespace OpenSim;
+
 int main(int argc, char* argv[]) {
-    OpenSim::MuscleRedundancySolver mrs;
-    mrs.solve();
+    OPENSIM_THROW_IF(argc != 2, Exception,
+            "opensim-mrs requires exactly 1 argument (path to setup file).");
+
+    Object::registerType(GlobalStaticOptimizationSolver());
+    Object::registerType(MuscleRedundancySolver());
+
+    const std::string setupFile(argv[1]);
+    auto obj = std::unique_ptr<Object>(Object::makeObjectFromFile(setupFile));
+
+    OPENSIM_THROW_IF(obj == nullptr, Exception,
+        "A problem occurred when trying to load file '" + setupFile + "'.");
+
+    if (const auto* gso =
+            dynamic_cast<const GlobalStaticOptimizationSolver*>(obj.get())) {
+        auto solution = gso->solve();
+    } else if (const auto* mrs =
+            dynamic_cast<const MuscleRedundancySolver*>(obj.get())) {
+        auto solution = mrs->solve();
+    } else {
+        throw Exception("The provided file '" + setupFile + "' does not "
+                "define an InverseMuscleSolver.");
+    }
     return EXIT_SUCCESS;
 }
