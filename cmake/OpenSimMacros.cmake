81fd584aa6b0a1d911c9606e0f2244d8d8754d70
Christopher Lee Dembia <cld72@cornell.edu>
2017-08-25 10:14:45 -0700

    Copy DLLs: specify DEPENDS, install from dep's installation.

--- cmake/OpenSimMacros.cmake
+++ cmake/OpenSimMacros.cmake
@@ -354,24 +354,22 @@ function(OpenSimCopyDependencyDLLsForWin DEP_NAME DEP_INSTALL_DIR)
                                 "${DEP_INSTALL_DIR}.")
         endif()
         set(DEST_DIR "${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
-        # https://stackoverflow.com/questions/21198030/installfiles-cmake-cfg-intdir-abc-win-dll-destination-bin
-        set(DEST_DIR_FOR_INST "${CMAKE_BINARY_DIR}/$<CONFIG>")
         set(DLL_NAMES "")
         foreach(DLL IN LISTS DLLS)
             get_filename_component(DLL_NAME ${DLL} NAME)
             list(APPEND DLLS_DEST "${DEST_DIR}/${DLL_NAME}")
-            list(APPEND DLLS_FOR_INST "${DEST_DIR_FOR_INST}/${DLL_NAME}")
             set(DLL_NAMES "${DLL_NAMES} ${DLL_NAME}")
         endforeach()
         add_custom_command(OUTPUT ${DLLS_DEST}
             COMMAND ${CMAKE_COMMAND} -E make_directory ${DEST_DIR}
             COMMAND ${CMAKE_COMMAND} -E copy ${DLLS} ${DEST_DIR}
+            DEPENDS ${DLLS}
             COMMENT "Copying DLLs from ${DEP_INSTALL_DIR} to ${DEST_DIR}:${DLL_NAMES}")
         add_custom_target(Copy_${DEP_NAME}_DLLs ALL DEPENDS ${DLLS_DEST})
         set_target_properties(Copy_${DEP_NAME}_DLLs PROPERTIES
             PROJECT_LABEL "Copy ${DEP_NAME} DLLs")
         if(OPENSIM_COPY_DEPENDENCIES)
-            install(FILES ${DLLS_FOR_INST} DESTINATION ${CMAKE_INSTALL_BINDIR})
+            install(FILES ${DLLS} DESTINATION ${CMAKE_INSTALL_BINDIR})
         endif()
     endif()
 endfunction()
