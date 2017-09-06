b17d6af7a2fbdd548616972e01375fbfd3dc6817
Christopher Dembia <cld72@cornell.edu>
2017-09-05 17:25:46 -0700

    Add template instantiations for double.

--- tropter/tropter/optimalcontrol/DirectCollocation.cpp
+++ tropter/tropter/optimalcontrol/DirectCollocation.cpp
@@ -3,10 +3,12 @@
 
 namespace tropter {
 
+template class DirectCollocationSolver<double>;
 template class DirectCollocationSolver<adouble>;
 
 namespace transcription {
 
+template class LowOrder<double>;
 template class LowOrder<adouble>;
 
 } // namespace transcription
