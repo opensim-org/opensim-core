71571170af5c458fc78220d786b7ad19c1ab09b2
aymanhab <ahabib@stanford.edu>
2017-09-07 10:43:00 -0700

    Add comments to test case to clarify what GUI functionality is exercised.

--- Bindings/Java/tests/TestModelScaling.java
+++ Bindings/Java/tests/TestModelScaling.java
@@ -6,13 +6,16 @@ class TestModelScaling {
 
   try {
         ScaleTool scaleTool = null;
+        // Live current model in GUI would be used for unscaledModel
         Model unscaledModel = new Model("gait2354_simbody.osim");
+        // Exercise "loadsettings" from file"
         try {
             scaleTool = new ScaleTool("subject01_Setup_Scale.xml");
         } catch (IOException ex) {
             System.exit(-1);
         }
         System.out.println("ScaleTool created!");
+        // Populate the tabs of Scale factors and measurements
         MarkerSet extraMarkerSet = new MarkerSet(unscaledModel, "gait2354_Scale_MarkerSet.xml");
         System.out.println("Extra MarkerSet created!");
         unscaledModel.updateMarkerSet(extraMarkerSet);
@@ -25,6 +28,14 @@ class TestModelScaling {
         double scaleFactor = context.computeMeasurementScaleFactor(scaleTool.getModelScaler(), unscaledModel, 
                 measurementTrial, measurementSet.get(0));
         System.out.println("Measurement computed !");
+        // When we run the tool we create a copy of the unscaled model and scale in place using the
+        // calls below which exercise the "run" button
+        Model processedModel = new Model(unscaledModel);
+        processedModel.setName(scaleTool.getName());
+        OpenSimContext peocessedContext = new OpenSimContext(processedModel.initSystem(), processedModel);
+        peocessedContext.processModelScale(scaleTool.getModelScaler(), 
+                processedModel, "", scaleTool.getSubjectMass());
+        peocessedContext.processModelMarkerPlacer(scaleTool.getMarkerPlacer(), processedModel, "");
         System.out.println("Test finished!");
     }
     catch (IOException ex){
