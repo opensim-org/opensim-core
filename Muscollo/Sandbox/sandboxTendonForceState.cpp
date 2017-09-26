cb372fab55ac0261a44f957cc182b0ef24ba5d29
Christopher Dembia <cld72@cornell.edu>
2017-09-25 18:05:31 -0700

    Compare inverse tendon force and fiber length states

--- Muscollo/Sandbox/sandboxTendonForceState.cpp
+++ Muscollo/Sandbox/sandboxTendonForceState.cpp
@@ -1,4 +1,5 @@
-#include <OpenSim/OpenSim.h>
+#include <OpenSim/Common/osimCommon.h>
+#include <OpenSim/Simulation/osimSimulation.h>
 #include <tropter/tropter.h>
 #include <INDYGO.h>
 #include <DeGrooteFregly2016Muscle.h>
@@ -151,6 +152,180 @@ solveForTrajectoryINDYGO() {
     return {ocpSolution, kinematics};
 }
 
+class SimpleInverseTendonForceState
+        : public tropter::OptimalControlProblem<adouble> {
+public:
+    using T = adouble;
+    const double max_isometric_force = 30;
+    const double optimal_fiber_length = 0.10;
+    const double tendon_slack_length = 0.05;
+    const double pennation_angle_at_optimal = 0.1;
+    // optimal fiber lengths per second:
+    const double max_contraction_velocity = 10;
+    const double duration = 2.0;
+
+    SimpleInverseTendonForceState() :
+            tropter::OptimalControlProblem<T>("simple_inverse") {
+        this->set_time(0, duration);
+        // TODO these functions should return indices for these variables.
+        this->add_state("activation", {0, 1});
+        this->add_state("norm_tendon_force", {0, 5});
+        this->add_control("excitation", {0, 1});
+        this->add_control("tendon_force_control", {-50, 50}/*, 0*/);
+        this->add_path_constraint("net_generalized_force", 0);
+        this->add_path_constraint("fiber_equilibrium", 0);
+        m_muscle = DeGrooteFregly2016Muscle<T>(
+                max_isometric_force, optimal_fiber_length, tendon_slack_length,
+                pennation_angle_at_optimal, max_contraction_velocity);
+    }
+    void calc_differential_algebraic_equations(
+            const tropter::DAEInput<T>& in,
+            tropter::DAEOutput<T> out) const override {
+        // TODO compare directly with fiber length state.
+        // Unpack variables.
+        const T& activation = in.states[0];
+        const T& normTenForce = in.states[1];
+        const T& excitation = in.controls[0];
+        const T& tenForceControl = in.controls[1];
+
+        const T speed = 0.08 / duration;
+        const T position = 0.112 + speed * in.time;
+        const T netGeneralizedForce = 10 + 10 * in.time / duration;
+        //const T position = 0.15 + 0.04 * in.time;
+        //const T speed = 0.04;
+        //const T netGeneralizedForce = 10 + 5 * in.time;
+
+        // Multibody dynamics.
+        // s_F in De Groote, et al. 2016
+        const double tendon_force_dynamics_scaling_factor = 10.0;
+        const T normTenForceRate =
+                tendon_force_dynamics_scaling_factor * tenForceControl;
+        T tendonForce;
+        // This also computes the fiber equilibrium path constraint.
+        m_muscle.calcTendonForceStateEquilibriumResidual(
+                activation, position, speed, normTenForce, normTenForceRate,
+                out.path[1], tendonForce);
+        // TODO might make more sense to use fiber force; might be a more
+        // direct relationship (that, or make tendon length a variable).
+        out.path[0] = tendonForce - netGeneralizedForce;
+
+        // Activation dynamics.
+        m_muscle.calcActivationDynamics(excitation, activation,
+                out.dynamics[0]);
+
+        // Fiber dynamics.
+        out.dynamics[1] = normTenForceRate;
+    }
+    void calc_integral_cost(const T& /*time*/,
+            const VectorX<T>& /*states*/,
+            const VectorX<T>& controls, T& integrand) const override {
+        integrand = controls[0] * controls[0];
+    }
+private:
+    DeGrooteFregly2016Muscle<T> m_muscle;
+};
+
+class SimpleInverseFiberLengthState
+        : public tropter::OptimalControlProblem<adouble> {
+public:
+    using T = adouble;
+    const double max_isometric_force = 30;
+    const double optimal_fiber_length = 0.10;
+    const double tendon_slack_length = 0.05;
+    const double pennation_angle_at_optimal = 0.1;
+    // optimal fiber lengths per second:
+    const double max_contraction_velocity = 10;
+    const double duration = 2.0;
+
+    SimpleInverseFiberLengthState() :
+            tropter::OptimalControlProblem<T>("simple_inverse") {
+        this->set_time(0, duration);
+        // TODO these functions should return indices for these variables.
+        this->add_state("activation", {0, 1});
+        this->add_state("norm_fiber_length", {0.2, 1.8});
+        this->add_control("excitation", {0, 1});
+        this->add_control("norm_fiber_velocity", {-1, 1}/*, 0*/);
+        this->add_path_constraint("net_generalized_force", 0);
+        this->add_path_constraint("fiber_equilibrium", 0);
+        m_muscle = DeGrooteFregly2016Muscle<T>(
+                max_isometric_force, optimal_fiber_length, tendon_slack_length,
+                pennation_angle_at_optimal, max_contraction_velocity);
+    }
+    void calc_differential_algebraic_equations(
+            const tropter::DAEInput<T>& in,
+            tropter::DAEOutput<T> out) const override {
+        // TODO compare directly with fiber length state.
+        // Unpack variables.
+        const T& activation = in.states[0];
+        const T& normFibLen = in.states[1];
+        const T& excitation = in.controls[0];
+        const T& normFibVel = in.controls[1];
+
+        const T speed = 0.08 / duration;
+        const T position = 0.112 + speed * in.time;
+        const T netGeneralizedForce = 10 + 10 * in.time / duration;
+        //const T position = 0.15 + 0.04 * in.time;
+        //const T speed = 0.04;
+        //const T netGeneralizedForce = 10 + 5 * in.time;
+
+
+        T normTenForce;
+        m_muscle.calcEquilibriumResidual(
+                activation, position, normFibLen, normFibVel, out.path[1],
+                normTenForce);
+        T tendonForce = m_muscle.get_max_isometric_force() * normTenForce;
+
+        out.path[0] = tendonForce - netGeneralizedForce;
+
+        // Activation dynamics.
+        m_muscle.calcActivationDynamics(excitation, activation,
+                out.dynamics[0]);
+
+        // Fiber dynamics.
+        out.dynamics[1] = max_contraction_velocity * normFibVel;
+    }
+    void calc_integral_cost(const T& /*time*/,
+            const VectorX<T>& /*states*/,
+            const VectorX<T>& controls, T& integrand) const override {
+        integrand = controls[0] * controls[0];
+    }
+private:
+    DeGrooteFregly2016Muscle<T> m_muscle;
+};
+
 int main() {
-    solveForTrajectoryINDYGO();
+    //solveForTrajectoryINDYGO();
+    tropter::OptimalControlSolution tendonForceStateSolution;
+    {
+        auto ocp = std::make_shared<SimpleInverseTendonForceState>();
+        int N = 100;
+        tropter::DirectCollocationSolver<adouble> dircol(ocp, "trapezoidal",
+                "ipopt", N);
+        tendonForceStateSolution = dircol.solve();
+        tendonForceStateSolution.write("DEBUG_sandboxTendonForceState.csv");
+    }
+    tropter::OptimalControlSolution fiberLengthStateSolution;
+    {
+        auto ocp = std::make_shared<SimpleInverseFiberLengthState>();
+        int N = 100;
+        tropter::DirectCollocationSolver<adouble> dircol(ocp, "trapezoidal",
+                "ipopt", N);
+        fiberLengthStateSolution = dircol.solve();
+        fiberLengthStateSolution.write(
+                "DEBUG_sandboxTendonForceState_fiberLengthState.csv");
+    }
+
+    // Activation.
+
+//    std::cout << "DEUBG " << tendonForceStateSolution.states.row(0) -
+//    fiberLengthStateSolution.states.row(0) << std::endl;
+    SimTK_TEST(tendonForceStateSolution.states.row(0).isApprox(
+                    fiberLengthStateSolution.states.row(0), 0.02));
+    SimTK_TEST(tendonForceStateSolution.states.row(0).isApprox(
+            fiberLengthStateSolution.states.row(0), 0.02));
+
+    SimTK_TEST(tendonForceStateSolution.controls.row(0).isApprox(
+            fiberLengthStateSolution.controls.row(0), 0.02));
+    SimTK_TEST(tendonForceStateSolution.controls.row(0).isApprox(
+            fiberLengthStateSolution.controls.row(0), 0.02));
 }
