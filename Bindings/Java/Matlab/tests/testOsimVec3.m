0f0d48386c7b507c45798dd041abc8c89ade6c31
jimmyDunne <dunne.jimmy@gmail.com>
2017-10-07 15:47:24 -0700

    changed example to a test

---
+++ Bindings/Java/Matlab/tests/testOsimVec3.m
@@ -0,0 +1,28 @@
+%% clear working space
+clear all;close all;clc;
+%% import opensim libraries
+import org.opensim.modeling.*
+%% 
+%% Define an abriatory an OpenSim Vec3 instance
+ov = Vec3(2,3,4);
+% Convert to a Matlab Vector
+mv = osimVec3(ov);
+
+% Convert back to an OpenSim Vec3
+n_ov = osimVec3(mv);
+% Convert back to an OpenSim Vec3 Type
+n_mv = osimVec3(n_ov);
+
+%% Test the changes for consistency
+% Test OpenSim Vec3() instances
+if n_ov().get(0) ~= ov().get(0) | n_ov().get(1) ~= ov().get(1) | n_ov().get(2) ~= ov().get(2)
+    error('OpenSim Vec3s are different. Should be the same')
+end
+% Test Matlab Vector instances
+if ~isequal(mv,n_mv)
+    error('Matlab arrays are not equal between conversions')
+end
+
+
+
+
