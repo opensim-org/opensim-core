c9a80deb6b40b48216b23fb06fa6359697e5e060
Christopher Dembia <cld72@cornell.edu>
2017-10-11 14:37:46 -0700

    Add Apache license for Muscollo

--- Muscollo/Tests/testSingleMuscleDeGrooteFregly2016MomentArms.cpp
+++ Muscollo/Tests/testSingleMuscleDeGrooteFregly2016MomentArms.cpp
@@ -1,3 +1,20 @@
+/* -------------------------------------------------------------------------- *
+ * OpenSim Muscollo: testSingleMuscleDeGrooteFregly2016MomentArms.cpp         *
+ * -------------------------------------------------------------------------- *
+ * Copyright (c) 2017 Stanford University and the Authors                     *
+ *                                                                            *
+ * Author(s): Christopher Dembia                                              *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0          *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
 #include <INDYGO.h>
 #include <GlobalStaticOptimization.h>
 #include <DeGrooteFregly2016Muscle.h>
