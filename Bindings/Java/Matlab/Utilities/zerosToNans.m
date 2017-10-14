755d4b8fd0bb89eabdc5d288ecff19fd98b11082
jimmyDunne <james.dunne@stanford.edu>
2017-10-13 17:12:40 -0700

    clean up

--- Bindings/Java/Matlab/Utilities/zerosToNans.m
+++ Bindings/Java/Matlab/Utilities/zerosToNans.m
@@ -4,14 +4,14 @@ function osimTable = zerosToNans(osimTable)
 %   zero's are interpreted literally.
 %   This replaces zero values of marker sdata with NaN's
 import org.opensim.modeling.*
-notification = 0;
+notifcation = 0;
 for i = 0 : osimTable.getNumRows() - 1
-    row = osimTable.getRowAtIndex(i)
+    row = osimTable.getRowAtIndex(i);
     for u = 0 : osimTable.getNumColumns() - 1
         p = row.get(u);
         % If the Marker components all equal 0, then replace with NaN
         if p.get(0) == 0 & p.get(1) == 0 & p.get(2) == 0 
-            p.setToNaN()
+            p.setToNaN();
             notifcation = 1;
         end  
         % Set the Vec3 for the row. 
@@ -21,7 +21,7 @@ end
 
 % Send a notification to the console to warn the user that the data has
 % beeb changed. 
-if notification == 1
+if notifcation == 1
     warning('Some Marker components were [0,0,0]. They were changed to [NaN,Nan,Nan]')
 end
 
