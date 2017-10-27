19372c63ca8b1aabcd6ee0c619208740b35a45d9
Christopher Dembia <cld72@cornell.edu>
2017-10-26 18:05:37 -0700

    Avoid duplicate instantiation

--- tropter/tropter/optimalcontrol/DirectCollocation.cpp
+++ tropter/tropter/optimalcontrol/DirectCollocation.cpp
@@ -21,10 +21,4 @@ namespace tropter {
 template class DirectCollocationSolver<double>;
 template class DirectCollocationSolver<adouble>;
 
-namespace transcription {
-
-template class Trapezoidal<double>;
-template class Trapezoidal<adouble>;
-
-} // namespace transcription
 } // namespace tropter
