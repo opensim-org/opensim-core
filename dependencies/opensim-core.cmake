882b2807410da1977a4a87a8670623da9b00b12a
Christopher Lee Dembia <cld72@cornell.edu>
2017-11-08 13:13:01 -0800

    Fix commit hashes for opensim-core.

--- dependencies/opensim-core.cmake
+++ dependencies/opensim-core.cmake
@@ -1,8 +1,7 @@
 # This file is included by the CMakeLists.txt in this directory.
 
 AddDependency(NAME       opensim-core
-              # StatesTrajectory::createFromStatesStorage() assembles the states.
-              URL        https://github.com/opensim-org/opensim-core/archive/7513accaa13a9431cf37e2bbc35ee7111f50baa6.zip
+              URL        https://github.com/opensim-org/opensim-core/archive/bf69d42f33a9363067a147bf1f60260f323f41f8.zip
               CMAKE_ARGS -DBUILD_API_EXAMPLES:BOOL=OFF
                          -DBUILD_TESTING:BOOL=OFF
                          -DSIMBODY_HOME:PATH=${CMAKE_INSTALL_PREFIX}/simbody
@@ -25,7 +24,7 @@ if(SUPERBUILD_opensim-core)
 
     AddDependency(NAME       docopt
                   GIT_URL    https://github.com/docopt/docopt.cpp.git
-                  GIT_TAG    68b814282252d75d7c98d073e5958c5b1a964241
+                  GIT_TAG    af03fa044ee1eff20819549b534ea86829a24a54
                   CMAKE_ARGS -DCMAKE_DEBUG_POSTFIX:STRING=_d)
 
     add_dependencies(opensim-core simbody docopt)
