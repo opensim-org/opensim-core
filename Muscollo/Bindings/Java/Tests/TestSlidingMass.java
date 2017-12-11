6977f633b2db5e0028b690488d377f521ff6b318
Christopher Dembia <cld72@cornell.edu>
2017-12-10 20:31:59 -0800

    Add Matlab testSlidingMass.m.

--- Muscollo/Bindings/Java/Tests/TestSlidingMass.java
+++ Muscollo/Bindings/Java/Tests/TestSlidingMass.java
@@ -1,6 +1,23 @@
+/* -------------------------------------------------------------------------- *
+ * OpenSim Muscollo: TestSlidingMass.java                                     *
+ * -------------------------------------------------------------------------- *
+ * Copyright (c) 2017 Stanford University and the Authors                     *
+ *                                                                            *
+ * Author(s): Christopher Dembia                                              *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0          *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
 import org.opensim.modeling.*;
 
-class TestMuscolloBasics {
+class TestSlidingMass {
 
   public static Model createSlidingMassModel() {
     Model model = new Model();
@@ -72,9 +89,9 @@ class TestMuscolloBasics {
 
     // Solve the problem.
     // ==================
-    MucoSolution solution = muco.solve();
+    // TODO MucoSolution solution = muco.solve();
 
-    solution.write("sliding_mass_solution.sto");
+    // TODO solution.write("sliding_mass_solution.sto");
   }
   public static void main(String[] args) {
     testSlidingMass();
