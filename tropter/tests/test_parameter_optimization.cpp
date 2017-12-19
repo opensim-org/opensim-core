e97057eed6c42926439d298ba555c9eb78ade890
nickbianco <nbianco@stanford.edu>
2017-12-19 10:26:29 -0800

    Remove generic optimization problem from test_parameter_optimization

--- tropter/tests/test_parameter_optimization.cpp
+++ tropter/tests/test_parameter_optimization.cpp
@@ -23,32 +23,16 @@
 #include "testing_optimalcontrol.h"
 
 using Eigen::VectorXd;
-using Eigen::Vector2d;
 
 using namespace tropter;
-
-/// Generic unconstrained optimization problem (borrowed from 
-/// test_generic_optimization.cpp).
-template<typename T>
-class UnconstrainedNLP : public OptimizationProblem<T> {
-public:
-    Unconstrained() : OptimizationProblem<T>(2, 0) {
-        this->set_variable_bounds(Vector2d(-5, -5), Vector2d(5, 5));
-    }
-    void calc_objective(
-        const VectorX<T>& x, T& obj_value) const override {
-        obj_value = (x[0] - 1.5) * (x[0] - 1.5)
-            + (x[1] + 2.0) * (x[1] + 2.0);
-    }
-};
 
 /// Pose the generic unconstrained optimization defined in
 /// test_generic_optimization.cpp as an optimal control problem, now using
 /// parameters instead of directly defining optimization variables.
 template<typename T>
-class UnconstrainedOCP : public tropter::OptimalControlProblem<T> {
+class Unconstrained : public tropter::OptimalControlProblem<T> {
 public:
-    UnconstrainedOCP() {
+    Unconstrained() {
         this->set_time({0}, {1});
         this->add_parameter("x0", {-5, 5});
         this->add_parameter("x1", {-5, 5});
@@ -68,30 +52,14 @@ public:
 TEST_CASE("IPOPT") {
 
     SECTION("ADOL-C") {
-        // Solve optimization problem.
-        UnconstrainedNLP<adouble> problem;
-        IPOPTSolver solver(problem);
-        VectorXd guess = Vector2d(0, 0);
-        auto nlp_solution = solver.optimize(guess);
-
         // Solve the optimal control problem.
-        auto ocp = std::make_shared<UnconstrainedOCP<adouble>>();
+        auto ocp = std::make_shared<Unconstrained<adouble>>();
         DirectCollocationSolver<adouble> dircol(ocp, "trapezoidal", "ipopt");
-        OptimalControlSolution ocp_solution = dircol.solve();
-        ocp_solution.write("unconstrained_solution.csv");
+        OptimalControlSolution solution = dircol.solve();
+        solution.write("unconstrained_solution.csv");
 
         // Check correct optimization solution is obtained.
-        REQUIRE(Approx(nlp_solution.parameters[0]) == 1.5);
-        REQUIRE(Approx(nlp_solution.parameters[1]) == -2.0);
-        REQUIRE(Approx(nlp_solution.objective) == 0);
-
-        // Compare optimal control solution to optimization solution.
-        REQUIRE(Approx(ocp_solution.parameters[0]) == 
-                Approx(nlp_solution.parameters[0]));
-        REQUIRE(Approx(ocp_solution.parameters[1]) == 
-                Approx(nlp_solution.parameters[1]));
-        REQUIRE(Approx(ocp_solution.objective) == 
-                Approx(nlp_solution.objective));
-    }
-
+        REQUIRE(Approx(solution.parameters[0]) == 1.5);
+        REQUIRE(Approx(solution.parameters[1]) == -2.0);
+        REQUIRE(Approx(solution.objective) == 0);
 }
