4ed0cb0ba035a1bc116d638e55dbf5b1a102ebc0
Christopher Dembia <cld72@cornell.edu>
2018-02-12 10:59:57 -0800

    Create "DGF2016Muscle" with activation dynamics

--- Muscollo/Sandbox/sandboxMuscle.cpp
+++ Muscollo/Sandbox/sandboxMuscle.cpp
@@ -23,8 +23,59 @@
 #include <OpenSim/Simulation/SimbodyEngine/SliderJoint.h>
 #include <OpenSim/Actuators/Millard2012EquilibriumMuscle.h>
 #include <MuscolloSandboxShared.h>
+#include <OpenSim/Simulation/Model/PathActuator.h>
 
 using namespace OpenSim;
+
+/// DGF stands for DeGroote-Fregly. The abbreviation is temporary, and is used
+/// to avoid a name conflict with the existing DeGrooteFregly2016Muscle class
+/// (which doesn't inherit from OpenSim::Force).
+class /*OSIMMUSCOLLO_API*/DGF2016Muscle : public PathActuator {
+    OpenSim_DECLARE_CONCRETE_OBJECT(DGF2016Muscle, PathActuator);
+public:
+    OpenSim_DECLARE_PROPERTY(activation_time_constant, double,
+    "Smaller value means activation can change more rapidly (units: seconds).");
+
+    OpenSim_DECLARE_PROPERTY(default_activation, double,
+    "Value of activation in the default state returned by initSystem().");
+
+    DGF2016Muscle() {
+        constructProperties();
+    }
+
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override {
+        Super::extendAddToSystem(system);
+        addStateVariable("activation", SimTK::Stage::Dynamics);
+    }
+
+    void extendInitStateFromProperties(SimTK::State& s) const override {
+        Super::extendInitStateFromProperties(s);
+        setStateVariableValue(s, "activation", get_default_activation());
+    }
+
+    void extendSetPropertiesFromState(const SimTK::State& s) override {
+        Super::extendSetPropertiesFromState(s);
+        set_default_activation(getStateVariableValue(s, "activation"));
+    }
+
+    // TODO no need to do clamping, etc; CoordinateActuator is bidirectional.
+    void computeStateVariableDerivatives(const SimTK::State& s) const override {
+        const auto& tau = get_activation_time_constant();
+        const auto& u = getControl(s);
+        const auto& a = getStateVariableValue(s, "activation");
+        const SimTK::Real adot = (u - a) / tau;
+        setStateVariableDerivativeValue(s, "activation", adot);
+    }
+
+    double computeActuation(const SimTK::State& s) const override {
+        return getStateVariableValue(s, "activation") * get_optimal_force();
+    }
+private:
+    void constructProperties() {
+        constructProperty_activation_time_constant(0.010);
+        constructProperty_default_activation(0.5);
+    }
+};
 
 Model createHangingMuscleModel() {
     Model model;
@@ -39,18 +90,22 @@ Model createHangingMuscleModel() {
     coord.setName("height");
     model.addComponent(joint);
 
-    auto* actu = new Millard2012EquilibriumMuscle();
+    auto* actu = new DGF2016Muscle();
+    actu->set_optimal_force(30);
+
+    // auto* actu = new Thelen2003Muscle();
+
+    // auto* actu = new Millard2012EquilibriumMuscle();
+    // actu->set_fiber_damping(0); // TODO
     actu->setName("actuator");
-    actu->set_max_isometric_force(30.0);
-    actu->set_optimal_fiber_length(0.10);
-    actu->set_tendon_slack_length(0.05);
-    actu->set_pennation_angle_at_optimal(0.1);
-    actu->set_max_contraction_velocity(10);
+    //actu->set_max_isometric_force(30.0);
+    //actu->set_optimal_fiber_length(0.10);
+    //actu->set_tendon_slack_length(0.05);
+    //actu->set_pennation_angle_at_optimal(0.1);
+    //actu->set_max_contraction_velocity(10);
     actu->addNewPathPoint("origin", model.updGround(), SimTK::Vec3(0));
     actu->addNewPathPoint("insertion", *body, SimTK::Vec3(0));
     model.addComponent(actu);
-    /*
-     */
 
     /*
     auto* actu = new ActivationCoordinateActuator();
@@ -59,14 +114,13 @@ Model createHangingMuscleModel() {
     actu->set_activation_time_constant(0.001);
     actu->set_optimal_force(30);
     model.addComponent(actu);
-    */
 
-    // TODO
     auto* contr = new PrescribedController();
     contr->setName("controller");
     contr->addActuator(*actu);
     contr->prescribeControlForActuator("actuator", new Constant(1.0));
     model.addComponent(contr);
+    */
 
     body->attachGeometry(new Sphere(0.05));
 
@@ -79,35 +133,33 @@ int main() {
     Model model = createHangingMuscleModel();
     SimTK::State state = model.initSystem();
     model.setStateVariableValue(state, "joint/height/value", 0.15);
-    model.equilibrateMuscles(state);
+    // model.equilibrateMuscles(state);
 
     // TODO
-    Manager manager(model, state);
-    manager.integrate(2.0);
+    //Manager manager(model, state);
+    //manager.integrate(2.0);
+    //
+    //visualize(model, manager.getStateStorage());
+    //std::exit(-1);
 
-    visualize(model, manager.getStateStorage());
-    std::exit(-1);
-
-    /*
-    std::cout << "DEBUG " <<
-            model.getStateVariableValue(state, "actuator/fiber_length")
-            << std::endl;
-    model.equilibrateMuscles(state);
-    std::cout << "DEBUG " <<
-            model.getStateVariableValue(state, "actuator/fiber_length")
-            << std::endl;
-    */
+    //std::cout << "DEBUG " <<
+    //        model.getStateVariableValue(state, "actuator/fiber_length")
+    //        << std::endl;
+    //model.equilibrateMuscles(state);
+    //std::cout << "DEBUG " <<
+    //        model.getStateVariableValue(state, "actuator/fiber_length")
+    //        << std::endl;
     mp.setModel(model);
     mp.setTimeBounds(0, {0.1, 1.0});
     mp.setStateInfo("joint/height/value", {0, 0.3}, 0.15, 0.14);
     mp.setStateInfo("joint/height/speed", {-10, 10}, 0, 0);
     // TODO initial fiber length?
     // TODO how to enforce initial equilibrium?
-    //mp.setStateInfo("actuator/fiber_length", {0, 0.3},
-    //        model.getStateVariableValue(state, "actuator/fiber_length"));
+    // mp.setStateInfo("actuator/fiber_length", {0, 0.3},
+    //         model.getStateVariableValue(state, "actuator/fiber_length"));
     // OpenSim might not allow activations of 0.
-    mp.setStateInfo("actuator/activation", {-1, 1}, 0); // TODO {0, 1}, 0);
-    mp.setControlInfo("actuator", {-1, 1}); // TODO {0, 1});
+    mp.setStateInfo("actuator/activation", {0, 1}, 0);
+    mp.setControlInfo("actuator", {0, 1});
 
     mp.addCost(MucoFinalTimeCost());
 
@@ -115,7 +167,7 @@ int main() {
     // TODO i feel like the force-velocity effect is much more strict than it
     // should be.
 
-    MucoTropterSolver& ms = muco.initSolver();
+    // MucoTropterSolver& ms = muco.initSolver();
     MucoSolution solution = muco.solve().unseal();
     solution.write("sandboxMuscle_solution.sto");
     std::cout << "DEBUG " << solution.getState("joint/height/value") << std::endl;
