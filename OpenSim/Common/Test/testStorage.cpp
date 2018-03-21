01de85d7eb50d341d4d89f752ece74ecf5b70be3
Ajay Seth <aseth@stanford.edu>
2018-03-20 18:50:38 -0700

    Include sub tests to verify that OutputReporter result are correctly flattened and loaded.

--- OpenSim/Common/Test/testStorage.cpp
+++ OpenSim/Common/Test/testStorage.cpp
@@ -27,77 +27,113 @@
 using namespace OpenSim;
 using namespace std;
 
+
+void testStorageLoadingFromFile(const std::string& fileName, const int ncols);
+
+void testStorageLegacy() {
+    // Create a storage from a std file "std_storage.sto"
+    //ofstream checkRunDirFile("rundir.txt");
+    //checkRunDirFile << "Run from here:\n\n";
+    //checkRunDirFile.close();
+    string stdLabels[] = { "time", "v1", "v2" };
+    std::unique_ptr<Storage> st(new Storage("test.sto"));
+    // time[\t]v1[\t]v2
+    // 1.[\t]   10.0[Space]20
+    // 2.[\t\t] 20.0[\t]40
+    ASSERT(st->getSize() == 2);
+    const Array<std::string> &lbls = st->getColumnLabels();
+    ASSERT(lbls.getSize() == 3);
+    int i = 0;
+    for (i = 0; i<lbls.getSize(); i++) {
+        ASSERT(lbls[i] == stdLabels[i]);
+    }
+
+    double val;
+    for (i = 0; i<st->getSize(); i++) {
+        StateVector& row = (*st->getStateVector(i));
+        ASSERT(row.getTime() == i + 1);
+        ASSERT(row.getData()[0] == row.getTime()*10.0);
+        row.getDataValue(0, val);
+        ASSERT(val == row.getTime()*10.0);
+        ASSERT(row.getData()[0] == row.getTime()*10.0);
+        ASSERT(row.getData()[1] == row.getTime()*20.0);
+    }
+    int ncol = st->getSmallestNumberOfStates();
+    ASSERT(ncol == 2);
+    Array<double> col(SimTK::CNT<SimTK::Real>::getNaN(), 4);
+    st->getDataColumn(1, col);
+    ASSERT(col[0] == 20.);
+    ASSERT(col[1] == 40.0);
+
+    ASSERT(st->getStateIndex("v2") == 1);
+
+    Storage st2("testDiff.sto");
+    // Test Comparison
+    double diff = st->compareColumn(st2, stdLabels[1], 0.);
+    ASSERT(fabs(diff) < 1E-7);
+    diff = st->compareColumn(st2, stdLabels[2], 0.);
+    ASSERT(fabs(diff) < 1E-7);
+
+    // Loading version 2 storage file with Storage class.
+    auto table = st->exportToTable();
+    STOFileAdapter::write(table, "testStorage_version2.sto");
+    {
+        // Now read using Storage() constructor.
+        Storage stVersion2("testStorage_version2.sto");
+        // Compare with st.
+        SimTK_TEST_EQ(table.getMatrix(),
+            stVersion2.exportToTable().getMatrix());
+    }
+
+    // The version 2 storage file does not require nRows and nColumns
+    // metadata (Issue #2120).
+    {
+        table.removeTableMetaDataKey("nRows");
+        table.removeTableMetaDataKey("nColumns");
+        STOFileAdapter::write(table,
+            "testStorage_version2_short_header.sto");
+        Storage stVersion2("testStorage_version2_short_header.sto");
+        SimTK_TEST_EQ(table.getMatrix(),
+            stVersion2.exportToTable().getMatrix());
+    }
+}
+
 int main() {
-    try {
-        // Create a storage from a std file "std_storage.sto"
-        //ofstream checkRunDirFile("rundir.txt");
-        //checkRunDirFile << "Run from here:\n\n";
-        //checkRunDirFile.close();
-        string stdLabels[] = {"time", "v1", "v2"};
-        std::unique_ptr<Storage> st(new Storage("test.sto"));
-        // time[\t]v1[\t]v2
-        // 1.[\t]   10.0[Space]20
-        // 2.[\t\t] 20.0[\t]40
-        ASSERT(st->getSize()==2);
-        const Array<std::string> &lbls = st->getColumnLabels();
-        ASSERT(lbls.getSize()==3);
-        int i=0;
-        for(i=0; i<lbls.getSize(); i++){
-            ASSERT(lbls[i]==stdLabels[i]);
-        }
-        double val;
-        for(i=0; i<st->getSize(); i++){
-            StateVector& row = (*st->getStateVector(i));
-            ASSERT(row.getTime()==i+1);
-            ASSERT(row.getData()[0]==row.getTime()*10.0);
-            row.getDataValue(0, val);
-            ASSERT(val==row.getTime()*10.0);
-            ASSERT(row.getData()[0]==row.getTime()*10.0);
-            ASSERT(row.getData()[1]==row.getTime()*20.0);
-        }
-        int ncol = st->getSmallestNumberOfStates();
-        ASSERT(ncol==2);
-        Array<double> col(SimTK::CNT<SimTK::Real>::getNaN(),4);
-        st->getDataColumn(1, col);
-        ASSERT(col[0]==20.);
-        ASSERT(col[1]==40.0);
-    
-        ASSERT(st->getStateIndex("v2")==1);
+    SimTK_START_TEST("testStorage");
 
-        Storage st2("testDiff.sto");
-        // Test Comparison
-        double diff = st->compareColumn(st2, stdLabels[1], 0.);
-        ASSERT(fabs(diff) < 1E-7);
-        diff = st->compareColumn(st2, stdLabels[2], 0.);
-        ASSERT(fabs(diff) < 1E-7);
+        // Verify the loading scalar Outputs (2) from ..sto into a Storage
+        SimTK_SUBTEST2(testStorageLoadingFromFile, "sampleOutputs.sto", 2+1);
 
-        // Loading version 2 storage file with Storage class.
-        auto table = st->exportToTable();
-        STOFileAdapter::write(table, "testStorage_version2.sto");
-        {
-            // Now read using Storage() constructor.
-            Storage stVersion2("testStorage_version2.sto");
-            // Compare with st.
-            SimTK_TEST_EQ(table.getMatrix(),
-                    stVersion2.exportToTable().getMatrix());
-        }
+        // Verify the loading Vec3 Outputs (2) from .sto into a Storage
+        SimTK_SUBTEST2(testStorageLoadingFromFile, "sampleOutputsVec3.sto", 2*3+1);
 
-        // The version 2 storage file does not require nRows and nColumns
-        // metadata (Issue #2120).
-        {
-            table.removeTableMetaDataKey("nRows");
-            table.removeTableMetaDataKey("nColumns");
-            STOFileAdapter::write(table,
-                    "testStorage_version2_short_header.sto");
-            Storage stVersion2("testStorage_version2_short_header.sto");
-            SimTK_TEST_EQ(table.getMatrix(),
-                    stVersion2.exportToTable().getMatrix());
-        }
+        // Verify the loading SpatialVec Outputs (2) from .sto into a Storage
+        SimTK_SUBTEST2(testStorageLoadingFromFile, "sampleOutputsSpatialVec.sto", 2*6+1);
+
+        // Verify the loading of marker data (14) from .trc into a Storage
+        SimTK_SUBTEST2(testStorageLoadingFromFile, "TRCFileWithNANs.trc", 43);
+
+        // Verify the loading of forces from .c3d into a Storage. Includes 3
+        // force-plates with force, point, moment vectors (Vec3 flattened)
+        SimTK_SUBTEST2(testStorageLoadingFromFile, "TestGRFData.c3d", 3*9+1);
+
+        SimTK_SUBTEST(testStorageLegacy);
+    SimTK_END_TEST();
+}
+
+void testStorageLoadingFromFile(const std::string& fileName, const int numCols)
+{
+    Storage storage{ fileName};
+    // remove extension
+    auto ix = fileName.rfind(".");
+    if (ix == std::string::npos) {
+        throw Exception("File name for Storage loading '" + fileName +
+            "' must have a valid extension.");
     }
-    catch (const Exception& e) {
-        e.print(cerr);
-        return 1;
-    }
-    cout << "Done" << endl;
-    return 0;
+    std::string name = fileName.substr(0, ix-1);
+    Array<std::string> labels = storage.getColumnLabels();
+
+    storage.print("test_" + name + ".sto");
+
+    ASSERT(numCols == labels.size());
 }
