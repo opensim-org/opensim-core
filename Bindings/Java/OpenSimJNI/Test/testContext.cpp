c2ec72c1a4429eb3ff900958c9200e5b3c2325e3
aymanhab <ahabib@stanford.edu>
2018-09-12 15:44:18 -0700

    Test case to reproduce crash editing joint parent frame

--- Bindings/Java/OpenSimJNI/Test/testContext.cpp
+++ Bindings/Java/OpenSimJNI/Test/testContext.cpp
@@ -238,6 +238,19 @@ int main()
         socket.setConnecteeName(originalConnecteeName);
         context->restoreStateFromCachedModel();
     }
+    AbstractSocket& psocket = shoulder.updSocket("parent_frame");
+    const std::string poriginalConnecteeName = psocket.getConnecteeName();
+    try {
+        // create an invalid model
+        psocket.setConnecteeName("r_ulna_radius_hand"); 
+        context->restoreStateFromCachedModel();
+    }
+    catch (...) {
+        // undo the change
+        socket.setConnecteeName(originalConnecteeName);
+        context->restoreStateFromCachedModel();
+    }
+
     return status;
   } catch (const std::exception& e) {
       cout << "Exception: " << e.what() << endl;
