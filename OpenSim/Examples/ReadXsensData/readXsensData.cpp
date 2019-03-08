47d2b5707a73851a2473701ca7e501ebf9346a46
aymanhab <ahabib@stanford.edu>
2019-03-07 19:22:01 -0800

    Refactor utility program to exercise XsensDataReader to account for class changes/serialization.

--- OpenSim/Examples/ReadXsensData/readXsensData.cpp
+++ OpenSim/Examples/ReadXsensData/readXsensData.cpp
@@ -31,39 +31,41 @@ static void PrintUsage(const char *aProgName, ostream &aOStream);
 
 int main(int argc, char* argv[]) {
     using namespace OpenSim;
-    if (argc < 4) {
+    if (argc < 2) {
         PrintUsage(argv[0], cout);
         return(-1);
     }
     std::string folder = "";
-    std::string trial = "MT_012005D6_025-000_"; //e.g. MT_012005D6_025-000_
-    std::string mappingFile = "mapIMUNamesToFilenames.xml";
+    std::string trial = ""; 
+    std::string xsensReaderConfigFile = "";
     for (int i = 1; i < argc; i++) {
         string option = argv[i];
         if (option == "-F")
             folder = argv[i + 1];
         else if (option == "-T")
             trial = argv[i + 1];
-        else if (option == "-M")
-            mappingFile = argv[i + 1];
+        else if (option == "-S")
+            xsensReaderConfigFile = argv[i + 1];
     }
     try {
-        ExperimentalSensors mapXsensName2ModelName(mappingFile);
-        DataAdapter::OutputTables tables = XsensDataReader::readTrial(folder, trial, mapXsensName2ModelName);
+        XsensDataReader reader(xsensReaderConfigFile);
+        if (!trial.empty()) reader.upd_trial_prefix() = trial;
+        if (!folder.empty()) reader.upd_data_folder() = folder;
+        DataAdapter::OutputTables tables = reader.readTrial();
         // Write tables to sto files
         // Accelerations
         std::shared_ptr<AbstractDataTable> accelTable = tables.at(XsensDataReader::LinearAccelerations);
         const TimeSeriesTableVec3& accelTableTyped = dynamic_cast<const TimeSeriesTableVec3&>(*accelTable);
         STOFileAdapterVec3::write(accelTableTyped, folder + trial + "accelerations.sto");
 
-        // Magenometer
-        std::shared_ptr<AbstractDataTable> magTable = tables.at(XsensDataReader::MagneticHeading);
-        const TimeSeriesTableVec3& magTableTyped = dynamic_cast<const TimeSeriesTableVec3&>(*magTable);
+        // MagneticHeading
+        std::shared_ptr<AbstractDataTable> magneticHeadingTable = tables.at(XsensDataReader::MagneticHeading);
+        const TimeSeriesTableVec3& magTableTyped = dynamic_cast<const TimeSeriesTableVec3&>(*magneticHeadingTable);
         STOFileAdapterVec3::write(magTableTyped, folder + trial + "magnetometers.sto");
  
-        // Gyro
-        std::shared_ptr<AbstractDataTable> gyroTable = tables.at(XsensDataReader::AngularVelocity);
-        const TimeSeriesTableVec3& gyroTableTyped = dynamic_cast<const TimeSeriesTableVec3&>(*gyroTable);
+        // AngularVelocity
+        std::shared_ptr<AbstractDataTable> angularVelocityTable = tables.at(XsensDataReader::AngularVelocity);
+        const TimeSeriesTableVec3& gyroTableTyped = dynamic_cast<const TimeSeriesTableVec3&>(*angularVelocityTable);
         STOFileAdapterVec3::write(gyroTableTyped, folder + trial + "gyros.sto");
 
         // Orientation
@@ -87,7 +89,7 @@ void PrintUsage(const char *aProgName, std::ostream &aOStream)
     aOStream << "\n\n" << aProgName << "\n\n";
     aOStream << "Option              Argument         Action / Notes\n";
     aOStream << "------              --------         --------------\n";
-    aOStream << "-F folderName       Folder containing trial data. \n";
-    aOStream << "-T trialName        Common prefix to data file names e.g.MT_012005D6_026-000 will be used to prefix output files. \n";
-    aOStream << "-M mappingFileName  Name of XML file used to map filenames to names in output files. \n";
+    aOStream << "-S readerSpec.xml  Name of XML file used to configure the reader. \n";
+    aOStream << "-F folderName       Folder containing trial data, to override value in readerSpec.xml. \n";
+    aOStream << "-T trialName        Common prefix to data file names to override value in readerSpec.xml.\n";
 }
