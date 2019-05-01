bcd91b6f133c3426d5507efb3d08ce576eae0742
nickbianco <nbianco@stanford.edu>
2019-04-30 22:18:31 -0700

    Clean up and finalize test

--- Moco/Moco/MocoCost/MocoJointReactionCost.cpp
+++ Moco/Moco/MocoCost/MocoJointReactionCost.cpp
@@ -1,25 +1,25 @@
 /* -------------------------------------------------------------------------- *
- * OpenSim Moco: MocoJointReactionNormCost.h                                  *
- * -------------------------------------------------------------------------- *
- * Copyright (c) 2017 Stanford University and the Authors                     *
- *                                                                            *
- * Author(s): Nicholas Bianco                                                 *
- *                                                                            *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
- * not use this file except in compliance with the License. You may obtain a  *
- * copy of the License at http://www.apache.org/licenses/LICENSE-2.0          *
- *                                                                            *
- * Unless required by applicable law or agreed to in writing, software        *
- * distributed under the License is distributed on an "AS IS" BASIS,          *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- * See the License for the specific language governing permissions and        *
- * limitations under the License.                                             *
- * -------------------------------------------------------------------------- */
+* OpenSim Moco: MocoJointReactionCost.h                                  *
+* -------------------------------------------------------------------------- *
+* Copyright (c) 2019 Stanford University and the Authors                     *
+*                                                                            *
+* Author(s): Nicholas Bianco                                                 *
+*                                                                            *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+* not use this file except in compliance with the License. You may obtain a  *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0          *
+*                                                                            *
+* Unless required by applicable law or agreed to in writing, software        *
+* distributed under the License is distributed on an "AS IS" BASIS,          *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+* See the License for the specific language governing permissions and        *
+* limitations under the License.                                             *
+* -------------------------------------------------------------------------- */
 
 #include "MocoJointReactionCost.h"
 #include "../MocoUtilities.h"
 #include <OpenSim/Simulation/Model/Model.h>
-    
+
 using namespace OpenSim;
 
 MocoJointReactionCost::MocoJointReactionCost() {
@@ -28,25 +28,118 @@ MocoJointReactionCost::MocoJointReactionCost() {
 
 void MocoJointReactionCost::constructProperties() {
     constructProperty_joint_path("");
+    constructProperty_loads_frame("child");
+    constructProperty_expressed_in_frame_path("");
+    constructProperty_reaction_components();
+    constructProperty_reaction_weights(MocoWeightSet());
 }
 
-void MocoJointReactionCost::initializeOnModelImpl(
-        const Model& model) const {
+void MocoJointReactionCost::initializeOnModelImpl(const Model& model) const {
 
+    // Cache the joint.
     OPENSIM_THROW_IF_FRMOBJ(get_joint_path().empty(), Exception,
         "Empty model joint path detected. Please provide a valid joint path.");
-
     OPENSIM_THROW_IF_FRMOBJ(!model.hasComponent<Joint>(get_joint_path()),
         Exception,
         format("Joint at path %s not found in the model. "
                "Please provide a valid joint path.", get_joint_path()));
+    m_joint = &getModel().getComponent<Joint>(get_joint_path());
+  
+    checkPropertyInSet(*this, getProperty_loads_frame(), {"parent", "child"});
+    std::string expressedInFrame;
+    if (get_expressed_in_frame_path().empty()) {
+        if (get_loads_frame() == "child") {
+            expressedInFrame = m_joint->getChildFrame().getAbsolutePathString();
+        } else if(get_loads_frame() == "parent") {
+            expressedInFrame = 
+                m_joint->getParentFrame().getAbsolutePathString();
+        }
+    } else {
+        expressedInFrame = get_expressed_in_frame_path();
+    }
+
+    // Cache the expressed-in frame.
+    OPENSIM_THROW_IF_FRMOBJ(!model.hasComponent<Frame>(expressedInFrame),
+        Exception,
+        format("Frame at path %s not found in the model. "
+               "Please provide a valid frame path.",
+                expressedInFrame));
+    m_frame = &getModel().getComponent<Frame>(expressedInFrame);
+
+    // If user provided no reaction component names, then set all components to
+    // to be minimized. Otherwise, loop through user-provided component names
+    // and check that they are all accepted components.
+    std::vector<std::string> reactionComponents;
+    std::vector<std::string> allowedComponents = {"moment-x", "moment-y",
+        "moment-z", "force-x", "force-y", "force-z"};
+    if (getProperty_reaction_components().empty()) {
+        reactionComponents = allowedComponents;    
+    } else {
+        for (int i = 0; i < getProperty_reaction_components().size(); ++i) {
+            if (std::find(allowedComponents.begin(), allowedComponents.end(),
+                get_reaction_components(i)) == allowedComponents.end()) {
+                OPENSIM_THROW_FRMOBJ(Exception,
+                    format("Reaction component '%s' specified not recognized.",
+                            get_reaction_components(i)));
+            }
+            reactionComponents.push_back(get_reaction_components(i));
+        }
+    }
+
+    // Loop through all reaction components to minimize and get the
+    // corresponding SpatialVec indices and weights.
+    for (const auto& component :  reactionComponents) {
+        if (component == "moment-x") {
+            m_componentIndices.push_back({0, 0});
+        } else if (component == "moment-y") {
+            m_componentIndices.push_back({0, 1});
+        } else if (component == "moment-z") {
+            m_componentIndices.push_back({0, 2});
+        } else if (component == "force-x") {
+            m_componentIndices.push_back({1, 0});
+        } else if (component == "force-y") {
+            m_componentIndices.push_back({1, 1});
+        } else if (component == "force-z") {
+            m_componentIndices.push_back({1, 2});
+        }
+
+        double compWeight = 1.0;
+        if (get_reaction_weights().contains(component)) {
+            compWeight = get_reaction_weights().get(component).getWeight();
+        }
+        m_componentWeights.push_back(compWeight);
+    }
 }
 
 void MocoJointReactionCost::calcIntegralCostImpl(const SimTK::State& state,
         double& integrand) const {
 
     getModel().realizeAcceleration(state);
-    // TODO: Cache the joint.
-    const auto& joint = getModel().getComponent<Joint>(get_joint_path());
-    integrand = joint.calcReactionOnChildExpressedInGround(state).norm();
+    const auto& ground = getModel().getGround();
+
+    // Compute the reaction loads on the parent or child frame.
+    SimTK::SpatialVec reactionInGround;
+    if (get_loads_frame() == "child") {
+        reactionInGround =
+            m_joint->calcReactionOnChildExpressedInGround(state);
+    } else if (get_loads_frame() == "parent") {
+        reactionInGround =
+            m_joint->calcReactionOnParentExpressedInGround(state);
+    }
+        
+    // Re-express the reactions into the proper frame and repackage into a new
+    // SpatialVec.
+    const SimTK::Vec3 moment = ground.expressVectorInAnotherFrame(state,
+        reactionInGround[0], *m_frame);
+    const SimTK::Vec3 force = ground.expressVectorInAnotherFrame(state,
+        reactionInGround[1], *m_frame);
+    SimTK::SpatialVec reaction(moment, force);
+
+    // Compute cost.
+    integrand = 0;
+    for (int i = 0; i < m_componentIndices.size(); ++i) {
+        const auto index = m_componentIndices[i];
+        const double weight = m_componentWeights[i];
+        integrand += weight * pow(reaction[index.first][index.second], 2);
+    }
 }
--- Moco/Moco/MocoCost/MocoJointReactionCost.h
+++ Moco/Moco/MocoCost/MocoJointReactionCost.h
@@ -3,7 +3,7 @@
 /* -------------------------------------------------------------------------- *
  * OpenSim Moco: MocoJointReactionNormCost.h                                  *
  * -------------------------------------------------------------------------- *
- * Copyright (c) 2017 Stanford University and the Authors                     *
+ * Copyright (c) 2019 Stanford University and the Authors                     *
  *                                                                            *
  * Author(s): Nicholas Bianco                                                 *
  *                                                                            *
@@ -19,19 +19,32 @@
  * -------------------------------------------------------------------------- */
 
 #include "MocoCost.h"
+#include "../MocoWeightSet.h"
+
+#include <OpenSim/Simulation/SimbodyEngine/Joint.h>
 
 namespace OpenSim {
 
-/// Minimize the reaction loads on the child body of a specified joint. 
-/// The norm of the reaction forces and moments integrated over the phase is 
-/// the specific quantity minimized.
+/// Minimize the the sum of squares of specified reaction moment and force 
+/// components for a given joint, integrated over the phase.
+///
+/// In addition to specifying the joint and reaction components, the user may
+/// also specify the frame the loads are computed from ("parent" or "child"),
+/// and the frame the loads are expressed in (any valid frame in the model).
+///
+/// Minimizing the y-direction reaction force on the parent frame of the right
+/// knee joint expressed in the right tibia frame:
+/// @code 
+/// auto* cost = problem.addCost<MocoJointReactionCost>();
+/// cost->setName("tibiofemoral_compressive_force");
+/// cost->setJointPath("/jointset/knee_r");
+/// cost->setLoadsFrame("parent");
+/// cost->setExpressedInFramePath("/bodyset/tibia_r");
+/// cost->setReactionComponents({"force-y"});
+/// @endcode
+///
 /// This cost requires realizing to the Acceleration stage.
 /// @ingroup mococost
-// TODO allow a list property of multiple joints?
-// TODO allow specification of the components of the reaction load SpatialVec
-//      to be minimized.
-// TODO allow specification of either child or parent reaction loads to 
-//      to minimize.
 class OSIMMOCO_API MocoJointReactionCost : public MocoCost {
 OpenSim_DECLARE_CONCRETE_OBJECT(MocoJointReactionCost, MocoCost);
 public: 
@@ -43,11 +56,43 @@ public:
             : MocoCost(std::move(name), weight) {
         constructProperties();
     }
-    /// Provide a valid model path for joint whose reaction loads will be
-    /// minimized. 
-    // TODO when using implicit dynamics, we will need to revisit this cost.
-    void setJointPath(const std::string& path) 
-    {   set_joint_path(path); }
+
+    /// Set the model path to the joint whose reaction load(s) will be
+    /// minimized.
+    void setJointPath(const std::string& jointPath)
+    {   set_joint_path(jointPath); }
+    /// Set the frame from which the reaction loads are computed. Options:
+    /// "parent" or "child" (default: "child").
+    void setLoadsFrame(const std::string& frame)
+    {   set_loads_frame(frame); }
+    /// Set the frame in which the minimized reaction load is expressed.
+    void setExpressedInFramePath(const std::string& framePath) 
+    {   set_expressed_in_frame_path(framePath); }
+    /// Set a specific set of reaction components to be minimized. If not set,
+    /// all reaction components are minimized by default. Replaces the reaction 
+    /// component set if it already exists.
+    void setReactionComponents(const std::vector<std::string>& components){
+        updProperty_reaction_components().clear();
+        for (const auto& component : components) {
+            append_reaction_components(component);
+        }
+    }
+    /// Set the weight for an individual reaction component. If a weight is
+    /// already set for the requested component, then the provided weight
+    /// replaces the previous weight. An exception is thrown if a weight
+    /// for an unknown component is provided.
+    void setWeight(const std::string& stateName, const double& weight) {
+        if (get_reaction_weights().contains(stateName)) {
+            upd_reaction_weights().get(stateName).setWeight(weight);
+        } else {
+            upd_reaction_weights().cloneAndAppend({stateName, weight});
+        }
+    }
+    /// Provide a MocoWeightSet to weight the reaction components in the cost.
+    /// Replaces the weight set if it already exists.
+    void setWeightSet(const MocoWeightSet& weightSet) {
+        upd_reaction_weights() = weightSet;
+    }
 
 protected:
     void initializeOnModelImpl(const Model&) const override;
@@ -55,9 +100,27 @@ protected:
             double& integrand) const override;
 
 private:
+    OpenSim_DECLARE_PROPERTY(joint_path, std::string, 
+            "The model path to the joint whose reaction load(s) will be "
+            "minimized.");
+    OpenSim_DECLARE_PROPERTY(loads_frame, std::string, 
+            "The frame from which the reaction loads are computed. Options: "
+            "'child' or 'parent' (default: 'child').");
+    OpenSim_DECLARE_PROPERTY(expressed_in_frame_path, std::string, 
+            "The frame in which the minimized reaction load is expressed.");
+    OpenSim_DECLARE_LIST_PROPERTY(reaction_components, std::string,
+            "A specific set of reaction components to be minimized. If not "
+            "set, all reaction components are minimized by default.");
+    OpenSim_DECLARE_PROPERTY(reaction_weights, MocoWeightSet,
+            "Set of weight objects to weight individual reaction components in "
+            "the cost.");
+    
     void constructProperties();
-    OpenSim_DECLARE_PROPERTY(joint_path, std::string, "The model path for the "
-            "joint with minimized reaction loads.");
+
+    mutable SimTK::ReferencePtr<const Joint> m_joint;
+    mutable SimTK::ReferencePtr<const Frame> m_frame;
+    mutable std::vector<std::pair<int, int>> m_componentIndices;
+    mutable std::vector<double> m_componentWeights;
 };
 
 } // namespace OpenSim
