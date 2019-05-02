a01798efe21bf199ccd9388f822d054d75d9d333
Christopher Dembia <cld72@cornell.edu>
2019-05-02 09:14:29 -0700

    MocoInverse

--- Moco/Article/muscle_sit_to_stand.py
+++ Moco/Article/muscle_sit_to_stand.py
@@ -18,6 +18,7 @@ def professional_spines(axes):
 def muscle_sit_to_stand(run, plot):
     predict_solution_file = 'predict_solution.sto'
     track_solution_file = 'track_solution.sto'
+    inverse_solution_file = 'inverse_solution.sto'
     def create_tool(model):
         moco = osim.MocoTool()
         solver = moco.initCasADiSolver()
@@ -129,19 +130,38 @@ def muscle_sit_to_stand(run, plot):
         solution = moco.solve()
         solution.write(track_solution_file)
 
+    def inverse():
+        inverse = osim.MocoInverse()
+        inverse.setModel(muscle_driven_model())
+        inverse.setKinematicsFile(predict_solution_file)
+        inverse.set_kinematics_allow_extra_columns(True)
+        inverse.set_lowpass_cutoff_frequency_for_kinematics(6)
+        inverse.set_mesh_interval(0.05)
+        inverse.set_create_reserve_actuators(2)
+        inverse.set_minimize_sum_squared_states(True)
+        inverse.set_tolerance(1e-4)
+        # inverse.append_output_paths('.*normalized_fiber_length')
+        # inverse.append_output_paths('.*passive_force_multiplier')
+        inverseSolution = inverse.solve()
+        inverseSolution.getMocoSolution().write(inverse_solution_file)
+        # inverseOutputs = inverseSolution.getOutputs()
+        # osim.STOFileAdapter.write(inverseOutputs, 'muscle_outputs.sto')
+        # print('Cost without device: %f' %
+        #       inverseSolution.getMocoSolution().getObjective())
+
     if run:
         predict_solution = predict()
         track_solution = track()
+        inverse_solution = inverse()
 
     if plot:
-        predict_solution = osim.MocoIterate(predict_solution_file)
         fig = plt.figure(figsize=(6, 7))
         values = [
             '/jointset/hip_r/hip_flexion_r/value',
             '/jointset/knee_r/knee_angle_r/value',
             '/jointset/ankle_r/ankle_angle_r/value',
         ]
-        # TODO negate sign of hip flexion
+        # TODO hip flexion is misnamed.
         coord_names = {
             '/jointset/hip_r/hip_flexion_r/value': 'hip flexion',
             '/jointset/knee_r/knee_angle_r/value': 'knee flexion',
@@ -154,24 +174,6 @@ def muscle_sit_to_stand(run, plot):
 
         }
 
-        grid = plt.GridSpec(9, 2, hspace=0.7,
-                            left=0.1, right=0.98, bottom=0.07, top=0.96,
-                            )
-        time = predict_solution.getTimeMat()
-        for ic, coordvalue in enumerate(values):
-            ax = fig.add_subplot(grid[3 * ic: 3 * (ic + 1), 0])
-            y = (coord_signs[coordvalue] * np.rad2deg(
-                predict_solution.getStateMat(coordvalue)))
-
-            ax.plot(time, y, color='k')
-            ax.set_ylabel('%s (degrees)' % coord_names[coordvalue])
-            ax.set_xlim(time[0], time[-1])
-            if ic == len(values) - 1:
-                ax.set_xlabel('time (s)')
-            else:
-                ax.set_xticklabels([])
-            professional_spines(ax)
-
         muscles = [
             'glut_max2_r',
             'psoas_r',
@@ -183,20 +185,62 @@ def muscle_sit_to_stand(run, plot):
             'soleus_r',
             'tib_ant_r',
         ]
+        grid = plt.GridSpec(9, 2, hspace=0.7,
+                            left=0.1, right=0.98, bottom=0.07, top=0.96,
+                            )
+        coord_axes = []
+        for ic, coordvalue in enumerate(values):
+            ax = fig.add_subplot(grid[3 * ic: 3 * (ic + 1), 0])
+            ax.set_ylabel('%s (degrees)' % coord_names[coordvalue])
+            if ic == len(values) - 1:
+                ax.set_xlabel('time (s)')
+            else:
+                ax.set_xticklabels([])
+            professional_spines(ax)
+            coord_axes += [ax]
+        muscle_axes = []
         for im, muscle in enumerate(muscles):
             ax = fig.add_subplot(grid[im, 1])
-            ax.plot(time,
-                    predict_solution.getStateMat(
-                        '/forceset/%s/activation' % muscle),
-                    color='k')
             ax.set_title('%s activation' % muscle[:-2])
-            ax.set_xlim(time[0], time[-1])
             ax.set_ylim([0, 1])
             if im == len(muscles) - 1:
                 ax.set_xlabel('time (s)')
             else:
                 ax.set_xticklabels([])
             professional_spines(ax)
+            muscle_axes += [ax]
+        def plot_solution(sol, label, linestyle='-'):
+            time = sol.getTimeMat()
+            for ic, coordvalue in enumerate(values):
+                ax = coord_axes[ic]
+                if ic == 0:
+                    use_label = label
+                else:
+                    use_label = None
+                if coordvalue in sol.getStateNames():
+                    y = (coord_signs[coordvalue] * np.rad2deg(
+                        sol.getStateMat(coordvalue)))
+                    ax.plot(time, y, linestyle=linestyle, label=use_label)
+                    ax.set_xlim(time[0], time[-1])
+                else:
+                    if ic == 0:
+                        ax.plot(0, 0, label=use_label)
+            for im, muscle in enumerate(muscles):
+                ax = muscle_axes[im]
+                ax.plot(time,
+                        sol.getStateMat(
+                            '/forceset/%s/activation' % muscle),
+                        linestyle=linestyle)
+                ax.set_xlim(time[0], time[-1])
+
+
+        predict_solution = osim.MocoIterate(predict_solution_file)
+        track_solution = osim.MocoIterate(track_solution_file)
+        inverse_solution = osim.MocoIterate(inverse_solution_file)
+        plot_solution(predict_solution, 'predict')
+        plot_solution(track_solution, 'track', linestyle='--')
+        plot_solution(inverse_solution, 'inverse', linestyle='--')
+        coord_axes[0].legend(frameon=False)
         fig.savefig('muscle_sit_to_stand.png', dpi=600)
 
 
