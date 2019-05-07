6da8064e107ece4a06677c22b6826b9a0ede912f
Nicholas Bianco <nbianco@stanford.edu>
2019-05-07 11:36:56 -0700

    Add Object_GetClassName for Rotation

--- OpenSim/Common/Object.h
+++ OpenSim/Common/Object.h
@@ -1156,6 +1156,8 @@ template <> struct Object_GetClassName<SimTK::SpatialVec>
 {   static const std::string name() {return "SpatialVec";} };
 template <> struct Object_GetClassName<SimTK::Transform>
 {   static const std::string name() {return "Transform";} };
+template <> struct Object_GetClassName<SimTK::Rotation_<SimTK::Real>>
+{   static const std::string name() {return "Rotation";} };
 
 #define OpenSim_OBJECT_ANY_DEFS(ConcreteClass, SuperClass)                     \
 public:                                                                        \
