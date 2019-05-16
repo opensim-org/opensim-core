70061ceebb4673b00cba5a6e746bac2b1377a43a
Ajay Seth <aseth@stanford.edu>
2019-05-16 12:46:51 -0700

    Improve comments for calibrateModelFromOrientations() usage.

--- OpenSim/Simulation/OpenSense/OpenSenseUtilities.h
+++ OpenSim/Simulation/OpenSense/OpenSenseUtilities.h
@@ -53,16 +53,25 @@ namespace OpenSim {
             const SimTK::CoordinateAxis& baseHeadingAxis = SimTK::ZAxis
         );
         /// @}
-        /** Create a calibrated model from a model in the calibration pose.
+        /** Create a calibrated model from a model in the calibration pose and 
+            the sensor (IMU) orientations (as quaternions) during the calibration.
+            modelCalibrationPoseFile: a model file where default pose matches that
+                                      used to collect the calibration data.
+            calibrationOrientationsFile: a storage file with IMU orientations 
+                                         expressed as Quaternions.
+            baseImuName: The label of the base IMU in the orientations_file used
+                         to account for the heading difference between the sensor
+                         data and the forward direction of the model. Leave blank
+                         if no heading correction is to be applied.
+            baseHeadingAxis: The axis of the base IMU that corresponds to its
+                             heading direction. Options are SimTK::X/Y/ZAxis
             Assumptions about the inputs: 
-            1) the model default pose is the same as the pose used to collect calibration data
-            2) that IMUs are labeled <body(or physical frame)_in_model>_imu so the underlying 
-            PhysicalFrame in the model can be identified and a corresponding Offset frame attached.
-            
-            modelCalibrationPoseFile is model file where default pose matches that used to collect 
-                calibrationOrientationsFile.
-            calibrationOrientationsFile is a storage file with IMU orientations expressed
-                as Quaternions.
+             1) the model default pose is the same as the pose used to collect
+                calibration data
+             2) the sensors are labeled <body(or physical frame)_in_model>_imu in
+                the orientations data. The underlying PhysicalFrame in the model
+                is then identified and a corresponding Offset frame is attached, 
+                which represents the sensor affixed to the model.
          */
         static Model calibrateModelFromOrientations(
             const std::string& modelCalibrationPoseFile,
