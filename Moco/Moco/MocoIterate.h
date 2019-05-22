6109f5f51d1867c5226367a4cc2e813f555220c7
Christopher Dembia <cld72@cornell.edu>
2019-05-22 12:21:48 -0700

    Miscellaneous.

--- Moco/Moco/MocoIterate.h
+++ Moco/Moco/MocoIterate.h
@@ -76,6 +76,9 @@ time,<state-0-name>,...,<control-0-name>,...,<multiplier-0-name>,..., \
 @endsamplefile
 (If stored in a STO file, the delimiters are tabs, not commas.)
 
+Column labels starting with "lambda" are Lagrange multiplier, and columns
+starting with "gamma" are slack variables (probably velocity corrections at
+certain collocation points).
 
 @par Matlab and Python
 Many of the functions in this class have variants ending with "Mat" that
