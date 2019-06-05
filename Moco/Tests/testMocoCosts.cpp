2219f91c3521b932ffdef2ab27853c732c2cfe23
Christopher Dembia <cld72@cornell.edu>
2019-06-05 15:46:48 -0700

    Speed up testMocoCosts.

--- Moco/Tests/testMocoCosts.cpp
+++ Moco/Tests/testMocoCosts.cpp
@@ -239,7 +239,6 @@ MocoTool setupMocoToolDoublePendulumMinimizeEffort() {
     auto& solver = moco.initSolver<SolverType>();
     solver.set_num_mesh_points(20);
     solver.set_optim_convergence_tolerance(1e-6);
-    solver.set_optim_hessian_approximation("exact");
 
     return moco;
 }
