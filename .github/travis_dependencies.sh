be954ef0b7a0aaec5e27d8df2612f27a075f7aae
Christopher Dembia <cld72@cornell.edu>
2019-06-12 08:33:07 -0700

    [skip appveyor]

--- .github/travis_dependencies.sh
+++ .github/travis_dependencies.sh
@@ -10,18 +10,20 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then DEP_CMAKE_ARGS+=(-DCMAKE_OSX_DEPLOYMENT_T
 
 printf '%s\n' "${DEP_CMAKE_ARGS[@]}"
 cmake "${DEP_CMAKE_ARGS[@]}"
-make -j$NPROC opensim-core
-make -j$NPROC adolc
+ls ~
+# make -j$NPROC opensim-core
+# make -j$NPROC adolc
 make -j$NPROC ipopt
-make -j$NPROC casadi
-make -j$NPROC
+# make -j$NPROC casadi
+# make -j$NPROC
 
 # Zip up the installation using a file name that identifies where
 # the binaries were built.
 mkdir ~/to_deploy
-ZIPNAME=opensim-moco-latest_${TRAVIS_OS_NAME}_${BTYPE}.zip
+ZIPNAME=opensim-moco-deps.zip
 # Zip up opensim relative to where it's installed.
 cd $TRAVIS_BUILD_DIR/
+ls ~
 # Leave symlinks intact.
 zip --symlinks --recurse-paths --quiet ~/to_deploy/$ZIPNAME ~/moco_dependencies_install
 
