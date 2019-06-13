109c2da41d980a707bea3f14cc258cccb9acb9fa
Christopher Dembia <cld72@cornell.edu>
2019-06-13 11:02:01 -0700

    Remove the ssh lines [skip appveyor]

--- .github/travis_dependencies.sh
+++ .github/travis_dependencies.sh
@@ -30,15 +30,4 @@ cd $TRAVIS_BUILD_DIR/..
 # Leave symlinks intact.
 zip --symlinks --recurse-paths --quiet ~/to_deploy/$ZIPNAME moco_dependencies_install
 
-## Set up ssh for sourceforge.
-cd $TRAVIS_BUILD_DIR
-if [[ "$DEPLOY" = "yes" ]]; then PREP_SOURCEFORGE_SSH=0; else PREP_SOURCEFORGE_SSH=1; fi
-# Decrypt the private key stored in the repository to the tmp dir.
-if [ $PREP_SOURCEFORGE_SSH = "0" ]; then openssl aes-256-cbc -K $encrypted_115b27a55ea5_key -iv $encrypted_115b27a55ea5_iv -in .github/.deploy_myosin_sourceforge_rsa.enc -out /tmp/deploy_myosin_sourceforge_rsa -d; fi
-# Start the ssh agent.
-if [ $PREP_SOURCEFORGE_SSH = "0" ]; then eval "$(ssh-agent -s)"; fi
-# Register this private key with this client (the travis machine).
-if [ $PREP_SOURCEFORGE_SSH = "0" ]; then chmod 600 /tmp/deploy_myosin_sourceforge_rsa; fi
-if [ $PREP_SOURCEFORGE_SSH = "0" ]; then ssh-add /tmp/deploy_myosin_sourceforge_rsa; fi
-
 rsync --archive --compress --verbose ~/to_deploy/ opensim-bot@frs.sourceforge.net:/home/frs/project/myosin/opensim-moco/
