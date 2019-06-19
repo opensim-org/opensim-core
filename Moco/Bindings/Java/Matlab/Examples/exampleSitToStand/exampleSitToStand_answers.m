27f1fddb60bcbab3dad40c37139fd0bcc31d8f09
Christopher Dembia <cld72@cornell.edu>
2019-06-18 20:45:03 -0700

    exampleSitToStand MocoStudy.

--- Moco/Bindings/Java/Matlab/Examples/exampleSitToStand/exampleSitToStand_answers.m
+++ Moco/Bindings/Java/Matlab/Examples/exampleSitToStand/exampleSitToStand_answers.m
@@ -4,9 +4,9 @@ function exampleSitToStand_answers
 import org.opensim.modeling.*;
 
 %% Part 1: Torque-driven Predictive Problem
-% Part 1a: Get a pre-configured MocoTool and set a torque-driven model on
+% Part 1a: Get a pre-configured MocoStudy and set a torque-driven model on
 % the underlying MocoProblem.
-moco = configureMocoTool();
+moco = configureMocoStudy();
 problem = moco.updProblem();
 problem.setModelCopy(getTorqueDrivenModel());
 
@@ -24,9 +24,9 @@ predictSolution.write('predictSolution.sto');
 moco.visualize(predictSolution);
 
 %% Part 2: Torque-driven Tracking Problem
-% Part 2a: Get a fresh MocoTool and set the torque-driven model on the
+% Part 2a: Get a fresh MocoStudy and set the torque-driven model on the
 % problem.
-moco = configureMocoTool();
+moco = configureMocoStudy();
 problem = moco.updProblem();
 problem.setModelCopy(getTorqueDrivenModel());
 
@@ -100,12 +100,12 @@ compareInverseSolutions(inverseSolution, inverseDeviceSolution);
 
 end
 
-function [moco] = configureMocoTool()
+function [moco] = configureMocoStudy()
 
 import org.opensim.modeling.*;
 
-% Create a new MocoTool.
-moco = MocoTool();
+% Create a new MocoStudy.
+moco = MocoStudy();
 
 % Configure the solver.
 solver = moco.initCasADiSolver();
