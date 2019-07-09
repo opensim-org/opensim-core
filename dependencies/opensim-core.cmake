cbe0ba36da3cb99744a2d3abb494c6405c24a874
Christopher Dembia <cld72@cornell.edu>
2019-07-09 10:08:30 -0700

    Update opensim-core commit hash.

--- dependencies/opensim-core.cmake
+++ dependencies/opensim-core.cmake
@@ -4,7 +4,7 @@
 # will invalidate its cached opensim-core installation if we change the commit.
 # This commented commit hash is not actually used in the superbuild.
 # opensim-core commit:
-# ef8a9e4d6f76199e711e75528a7d25b6fcb834e9
+# 9d2c0f5c648d856876152022fbb278cf399bf3d6
 
 AddDependency(NAME       opensim-core
               URL        ${CMAKE_SOURCE_DIR}/../opensim-core
