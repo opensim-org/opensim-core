e5c5d729aa92e23d430d24711683a546425c5d34
Christopher Dembia <cld72@cornell.edu>
2019-07-16 22:43:10 -0700

    Upload results to Google Drive.

--- .github/mocoshlo.py
+++ .github/mocoshlo.py
@@ -17,11 +17,20 @@ parser.add_argument('--duration', type=str, default="00:30:00",
 parser.add_argument('--name', type=str, default="",
         help="A name for the job (default: directory name).")
 
-parser.add_argument('--sshmaster', dest='sshmaster', action='store_true')
-parser.add_argument('--no-sshmaster', dest='sshmaster', action='store_false')
-parser.add_argument('--sshexit', dest='sshexit', action='store_true')
-parser.add_argument('--no-sshexit', dest='sshexit', action='store_false')
+parser.add_argument('--sshmaster', dest='sshmaster', action='store_true',
+        help="Start master SSH session (default).")
+parser.add_argument('--no-sshmaster', dest='sshmaster', action='store_false',
+        help="Do not start master SSH session (if you already started one).")
+parser.add_argument('--sshexit', dest='sshexit', action='store_true',
+        help="Exit the master session after submitting the job (default).")
+parser.add_argument('--no-sshexit', dest='sshexit', action='store_false',
+        help=("Do not exit the master session after submitting (if you plan to "
+            "submit more jobs."))
 parser.set_defaults(sshmaster=True, sshexit=True)
+
+# TODO windows
+# TODO initial configuring gdrive on Sherlock.
+# TODO building a docker container from a branch (as a separate step?).
 
 
 args = parser.parse_args()
@@ -43,16 +52,14 @@ with open('config.yaml') as f:
 sunetid = config['sunetid']
 
 
-# TODO: Must first set up gdrive on Sherlock.
 
 # Check that the directory contains setup.omoco.
 if not os.path.exists(os.path.join(directory, 'setup.omoco')):
     raise Exception(f"setup.omoco is missing from {directory}.")
 
-home = os.getenv('HOME')
+home = str(Path.home()) # Should work on Windows and UNIX.
 if not os.path.exists(f'{home}/.ssh/controlmasters/'):
     os.makedirs(f'{home}/.ssh/controlmasters')
-# TODO fix tilde for Windows.
 # Create master (-M) SSH session in the background (-f), without running
 # a command (-N), with 
 # https://unix.stackexchange.com/questions/83806/how-to-kill-ssh-session-that-was-started-with-the-f-option-run-in-background
@@ -60,7 +67,7 @@ control_path = "~/.ssh/controlmasters/%C"
 server = f"{sunetid}@login.sherlock.stanford.edu"
 now = datetime.datetime.now()
 time = '%s.%i' % (now.strftime('%Y-%m-%dT%H%M%S'), now.microsecond)
-job_directory = '%s-%s' % (name, time)
+job_directory = '%s-%s' % (time, name)
 print(f"Submitting {job_directory}")
 mocojobs_dir = f"~/nmbl/mocojobs/"
 server_job_dir = f"{mocojobs_dir}{job_directory}"
@@ -84,21 +91,16 @@ singularity exec $GROUP_HOME/opensim-moco/opensim-moco_latest.sif /opensim-moco-
 # Upload results to Google Drive.
 module load system gdrive
 
-# Store date in date variable.
-printf -v date '%(%Y-%m-%d)T\n' -1
-
 opensim_moco_folder_id=$(gdrive list | grep 'opensim-moco' | cut -d" " -f1)
-if [[ $opensim_moco_folder_id ]]; then
-    echo "opensim-moco folder exists."
-else
+if [[ -z "$opensim_moco_folder_id" ]]; then
     echo "Creating opensim-moco folder."
     opensim_moco_folder_id=$(gdrive mkdir opensim-moco | cut -d" " -f2)
-
-# Create folder for today.
-# TODO
+else
+    echo "opensim-moco folder exists."
+fi
 
 # Copy results.
-# TODO
+gdrive upload --recursive --parent $opensim_moco_folder_id {server_job_dir}
 
 """
 
@@ -109,8 +111,6 @@ with open(f'{directory}/{name}.batch', 'w') as f:
 # Recursively make the job directory.
 os.system(f'ssh -S {control_path} {server} "mkdir -p {mocojobs_dir}"')
 os.system(f"scp -o ControlPath={control_path} -r '{directory}/' {server}:{server_job_dir}")
-
-# TODO get moco from DockerHub.
 
 os.system(f'ssh -S {control_path} {server} "cd {server_job_dir} && sbatch {name}.batch"')
 
