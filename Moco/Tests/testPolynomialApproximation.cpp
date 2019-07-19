a186284c8d52e92472fcc329723f2592d823d40f
Antoine Falisse <antoinefalisse@gmail.com>
2019-07-19 16:36:56 -0700

    complete full test

--- Moco/Tests/testPolynomialApproximation.cpp
+++ Moco/Tests/testPolynomialApproximation.cpp
@@ -23,7 +23,7 @@
 
 using namespace OpenSim;
 
-Model createGait2D() {
+Model createGait2D_PolynomialPath() {
 
     ///////////////////////////////////////////////////////////////////////////
     // Create model
@@ -93,12 +93,18 @@ Model createGait2D() {
         Vec3(0), Vec3(0), *pelvis, Vec3(0), Vec3(0));
     auto& pelvis_tilt =
         ground_pelvis->updCoordinate(PlanarJoint::Coord::RotationZ);
+    pelvis_tilt.setRangeMin(-1.570796327);
+    pelvis_tilt.setRangeMax(1.570796327);
     pelvis_tilt.setName("pelvis_tilt");
     auto& pelvis_tx =
         ground_pelvis->updCoordinate(PlanarJoint::Coord::TranslationX);
+    pelvis_tx.setRangeMin(-5);
+    pelvis_tx.setRangeMax(5);
     pelvis_tx.setName("pelvis_tx");
     auto& pelvis_ty =
         ground_pelvis->updCoordinate(PlanarJoint::Coord::TranslationY);
+    pelvis_ty.setRangeMin(-1);
+    pelvis_ty.setRangeMax(2);
     pelvis_ty.setName("pelvis_ty");
     model.addJoint(ground_pelvis);
 
@@ -107,6 +113,8 @@ Model createGait2D() {
         -0.0638353973311301,-0.0823306940058688), Vec3(0), *femur_l, Vec3(0),
         Vec3(0));
     auto& hip_flexion_l = hip_l->updCoordinate();
+    hip_flexion_l.setRangeMin(-1.221730476);
+    hip_flexion_l.setRangeMax(1.570796327);
     hip_flexion_l.setName("hip_flexion_l");
     model.addJoint(hip_l);
 
@@ -115,6 +123,8 @@ Model createGait2D() {
         -0.0638353973311301, 0.0823306940058688), Vec3(0), *femur_r, Vec3(0),
         Vec3(0));
     auto& hip_flexion_r = hip_r->updCoordinate();
+    hip_flexion_r.setRangeMin(-1.221730476);
+    hip_flexion_r.setRangeMax(1.570796327);
     hip_flexion_r.setName("hip_flexion_r");
     model.addJoint(hip_r);
 
@@ -122,6 +132,8 @@ Model createGait2D() {
     auto* knee_l = new PinJoint("knee_l", *femur_l, Vec3(-0.00451221232146798,
         -0.396907245921447, 0), Vec3(0), *tibia_l, Vec3(0), Vec3(0));
     auto& knee_angle_l = knee_l->updCoordinate();
+    knee_angle_l.setRangeMin(-2.094395102);
+    knee_angle_l.setRangeMax(0.174532925);
     knee_angle_l.setName("knee_angle_l");
     model.addJoint(knee_l);
 
@@ -129,6 +141,8 @@ Model createGait2D() {
     auto* knee_r = new PinJoint("knee_r", *femur_r, Vec3(-0.00451221232146798,
         -0.396907245921447, 0), Vec3(0), *tibia_r, Vec3(0), Vec3(0));
     auto& knee_angle_r = knee_r->updCoordinate();
+    knee_angle_r.setRangeMin(-2.094395102);
+    knee_angle_r.setRangeMax(0.174532925);
     knee_angle_r.setName("knee_angle_r");
     model.addJoint(knee_r);
 
@@ -136,12 +150,16 @@ Model createGait2D() {
     auto* ankle_l = new PinJoint("ankle_l", *tibia_l,
         Vec3(0, -0.415694825374905, 0), Vec3(0), *talus_l, Vec3(0), Vec3(0));
     auto& ankle_angle_l = ankle_l->updCoordinate();
+    ankle_angle_l.setRangeMin(-0.872664626);
+    ankle_angle_l.setRangeMax(0.698131701);
     ankle_angle_l.setName("ankle_angle_l");
     model.addJoint(ankle_l);
     // Ankle right
     auto* ankle_r = new PinJoint("ankle_r", *tibia_r,
         Vec3(0, -0.415694825374905, 0), Vec3(0), *talus_r, Vec3(0), Vec3(0));
     auto& ankle_angle_r = ankle_r->updCoordinate();
+    ankle_angle_r.setRangeMin(-0.872664626);
+    ankle_angle_r.setRangeMax(0.698131701);
     ankle_angle_r.setName("ankle_angle_r");
     model.addJoint(ankle_r);
     // Subtalar left
@@ -181,9 +199,9 @@ Model createGait2D() {
     int orderHamstrings = 3;
     const int nCoeffHamstrings = 10;
     double coeffHamstrings[nCoeffHamstrings] = {0.420495248805280,
-        0.028755254940480, 0.018300167519638, 0.006995443639179,
+        0.028755254940480, -0.018300167519638, -0.006995443639179,
         0.053690588126276, 0.002092858497112, 0.001164874424056,
-        0.016799961996946, 1.886801073680048e-04, 0.009576337870147};
+        0.016799961996946, 1.886801073680048e-04, -0.009576337870147};
 
     int dimBFSH = 1;
     int orderBFSH = 3;
@@ -201,7 +219,7 @@ Model createGait2D() {
     int orderIlioPsoas = 3;
     const int nCoeffIlioPsoas = 4;
     double coeffIlioPsoas[nCoeffIlioPsoas] = {0.254156701045543,
-        -0.038230025226882, 0.005210532284289, 0.002534168005785};
+        -0.038230025226882, -0.005210532284289, 0.002534168005785};
 
     int dimRF = 2;
     int orderRF = 5;
@@ -340,6 +358,16 @@ Model createGait2D() {
     IlioPsoas_p_r.append_approximation_coordinates("/jointset/hip_r/hip_flexion_r");
     IlioPsoas_r->set_ignore_tendon_compliance(true);
     IlioPsoas_r->addNewPathPoint("IlioPsoas_r-P1", *pelvis, Vec3(-0.0624834, 0.0856611, 0.0284953));
+    IlioPsoas_r->addNewPathPoint("IlioPsoas_r-P2", *pelvis, Vec3(-0.0229846, -0.0550472, 0.0748371));
+    ConditionalPathPoint* IlioPsoas_P3_r = new ConditionalPathPoint();
+    IlioPsoas_P3_r->setName("IlioPsoas_r-P3");
+    IlioPsoas_P3_r->setCoordinate(hip_flexion_r);
+    IlioPsoas_P3_r->setBody(*pelvis);
+    IlioPsoas_P3_r->setLocation(Vec3(-0.0278133, -0.077742, 0.0804573));
+    IlioPsoas_P3_r->setRangeMin(-1.5708);
+    IlioPsoas_P3_r->setRangeMax(0.785398);
+    IlioPsoas_r->updGeometryPath().updPathPointSet().adoptAndAppend(IlioPsoas_P3_r);
+    IlioPsoas_r->addNewPathPoint("IlioPsoas_r-P4", *femur_r, Vec3(0.00160439, -0.0508392, 0.00381043));
     IlioPsoas_r->addNewPathPoint("IlioPsoas_r-P5", *femur_r, Vec3(-0.0188516, -0.0598639, 0.0104285));
     IlioPsoas_r->setName("IlioPsoas_r");
     model.addComponent(IlioPsoas_r);
@@ -352,7 +380,46 @@ Model createGait2D() {
     RF_p_r.append_approximation_coordinates("/jointset/knee_r/knee_angle_r");
     RF_r->set_ignore_tendon_compliance(true);
     RF_r->addNewPathPoint("RF_r-P1", *pelvis, Vec3(-0.0284893, -0.0300345, 0.0954444));
-    RF_r->addNewPathPoint("RF_r-P2", *femur_r, Vec3(0.0334917, -0.404106, 0.00190522)); // cond
+    ConditionalPathPoint* RF_P2_r = new ConditionalPathPoint();
+    RF_P2_r->setName("RF_r-P2");
+    RF_P2_r->setCoordinate(knee_angle_r);
+    RF_P2_r->setBody(*femur_r);
+    RF_P2_r->setLocation(Vec3(0.0334917, -0.404106, 0.00190522));
+    RF_P2_r->setRangeMin(-2.61799);
+    RF_P2_r->setRangeMax(-1.45997);
+    RF_r->updGeometryPath().updPathPointSet().adoptAndAppend(RF_P2_r);
+    OpenSim::MovingPathPoint* RF_P3_r = new MovingPathPoint();
+    RF_P3_r->setName("RF_r-P3");
+    RF_P3_r->setBody(*tibia_r);
+    double scale = 0.966732152034662;
+    int np_rfX_r = 17;
+    double rfX_r_x[] = { -2.0944, -1.99997, -1.5708, -1.45752, -1.39626, -1.0472, -0.698132, -0.526391, -0.349066, -0.174533, 0, 0.00017453, 0.00034907, 0.0279253, 0.0872665, 0.174533, 2.0944 };
+    double rfX_r_y[] = { 0.0155805, 0.0179938, 0.0275081, 0.0296564, 0.0307615, 0.0365695, 0.0422074, 0.0450902, 0.048391, 0.0534299, 0.0617576, 0.0617669, 0.0617762, 0.0633083, 0.066994, 0.0733035, 0.0573481 };
+    SimTK::Vector rfX_r_y_s(np_rfX_r,&rfX_r_y[0]);
+    rfX_r_y_s = scale * rfX_r_y_s;
+    //rfX_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_rf_r_x(np_rfX_r, rfX_r_x, &rfX_r_y_s[0]);
+    RF_P3_r->set_x_location(SS_rf_r_x);
+    RF_P3_r->setXCoordinate(knee_angle_r);
+    int np_rfY_r = 17;
+    double rfY_r_x[] = { -2.0944, -1.99997, -1.5708, -1.45752, -1.39626, -1.0472, -0.698132, -0.526391, -0.349066, -0.174533, 0, 0.00017453, 0.00034907, 0.0279253, 0.0872665, 0.174533, 2.0944 };
+    double rfY_r_y[] = { 0.0234116, 0.0237613, 0.0251141, 0.0252795, 0.0253146, 0.0249184, 0.0242373, 0.0238447, 0.0234197, 0.0227644, 0.020984, 0.0209814, 0.0209788, 0.0205225, 0.0191754, 0.0159554, -0.0673774 };
+    SimTK::Vector rfY_r_y_s(np_rfY_r,&rfY_r_y[0]);
+    rfY_r_y_s = scale * rfY_r_y_s;
+    //rfY_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_rf_r_y(np_rfY_r, rfY_r_x, &rfY_r_y_s[0]);
+    RF_P3_r->set_y_location(SS_rf_r_y);
+    RF_P3_r->setYCoordinate(knee_angle_r);
+    int np_rfZ_r = 2;
+    double rfZ_r_x[] = { -2.0944, 0.1745 };
+    double rfZ_r_y[] = { 0.0014, 0.0014 };
+    SimTK::Vector rfZ_r_y_s(np_rfZ_r,&rfZ_r_y[0]);
+    rfZ_r_y_s = scale * rfZ_r_y_s;
+    //rfZ_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_rf_r_z(np_rfZ_r, rfZ_r_x, &rfZ_r_y_s[0]);
+    RF_P3_r->set_z_location(SS_rf_r_z);
+    RF_P3_r->setZCoordinate(knee_angle_r);
+    RF_r->updGeometryPath().updPathPointSet().adoptAndAppend(RF_P3_r);
     RF_r->setName("RF_r");
     model.addComponent(RF_r);
 
@@ -362,9 +429,47 @@ Model createGait2D() {
     VAS_p_r.append_length_approximation(*VAS_f);
     VAS_p_r.append_approximation_coordinates("/jointset/knee_r/knee_angle_r");
     VAS_r->set_ignore_tendon_compliance(true);
-    VAS_r->addNewPathPoint("VAS_r-P1", *femur_r, Vec3(-0.0190522, -0.393979, -0.0235645));
-    VAS_r->addNewPathPoint("VAS_r-P2", *femur_r, Vec3(-0.0300824, -0.403304, -0.0258708)); // cond
-    VAS_r->addNewPathPoint("VAS_r-P3", *calcn_r, Vec3(0, 0.0283317, -0.0048438)); // cond
+    VAS_r->addNewPathPoint("VAS_r-P1", *femur_r, Vec3(0.0290796, -0.192928, 0.0310851));
+    VAS_r->addNewPathPoint("VAS_r-P2", *femur_r, Vec3(0.033592, -0.208972, 0.0285782));
+    ConditionalPathPoint* VAS_P3_r = new ConditionalPathPoint();
+    VAS_P3_r->setName("VAS_r-P3");
+    VAS_P3_r->setCoordinate(knee_angle_r);
+    VAS_P3_r->setBody(*femur_r);
+    VAS_P3_r->setLocation(Vec3(0.0343942, -0.404106, 0.0055151));
+    VAS_P3_r->setRangeMin(-2.61799);
+    VAS_P3_r->setRangeMax(-1.42);
+    VAS_r->updGeometryPath().updPathPointSet().adoptAndAppend(VAS_P3_r);
+    OpenSim::MovingPathPoint* VAS_P4_r = new MovingPathPoint();
+    VAS_P4_r->setName("VAS_P4_r");
+    VAS_P4_r->setBody(*tibia_r);
+    int np_viX_r = 17;
+    double viX_r_x[] = { -2.0944, -1.99997, -1.5708, -1.45752, -1.39626, -1.0472, -0.698132, -0.526391, -0.349066, -0.174533, 0, 0.00017453, 0.00034907, 0.0279253, 0.0872665, 0.174533, 2.0944 };
+    double viX_r_y[] = { 0.0082733, 0.0106866, 0.0202042, 0.022353, 0.0234583, 0.0292715, 0.0349465, 0.037871, 0.0412569, 0.0465287, 0.0554632, 0.0554735, 0.0554837, 0.0571717, 0.061272, 0.0684368, 0.0648818 };
+    SimTK::Vector viX_r_y_s(np_viX_r,&viX_r_y[0]);
+    viX_r_y_s = scale * viX_r_y_s;
+    //viX_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_vi_r_x(np_viX_r, viX_r_x, &viX_r_y_s[0]);
+    VAS_P4_r->set_x_location(SS_vi_r_x);
+    VAS_P4_r->setXCoordinate(knee_angle_r);
+    int np_viY_r = 17;
+    double viY_r_x[] = { -2.0944, -1.99997, -1.5708, -1.45752, -1.39626, -1.0472, -0.698132, -0.526391, -0.349066, -0.174533, 0, 0.00017453, 0.00034907, 0.0279253, 0.0872665, 0.174533, 2.0944 };
+    double viY_r_y[] = { 0.025599, 0.0259487, 0.0273124, 0.0274796, 0.0275151, 0.0271363, 0.0265737, 0.0263073, 0.0261187, 0.0260129, 0.0252923, 0.0252911, 0.0252898, 0.0250526, 0.0242191, 0.0218288, -0.0685706 };
+    SimTK::Vector viY_r_y_s(np_viY_r,&viY_r_y[0]);
+    viY_r_y_s = scale * viY_r_y_s;
+    //viY_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_vi_r_y(np_viY_r, viY_r_x, &viY_r_y_s[0]);
+    VAS_P4_r->set_y_location(SS_vi_r_y);
+    VAS_P4_r->setYCoordinate(knee_angle_r);
+    int np_viZ_r = 2;
+    double viZ_r_x[] = { -2.0944, 2.0944 };
+    double viZ_r_y[] = { 0.0018, 0.0018 };
+    SimTK::Vector viZ_r_y_s(np_viZ_r,&viZ_r_y[0]);
+    viZ_r_y_s = scale * viZ_r_y_s;
+    //viZ_r_y_s.scalarMultiplyInPlace(scale);
+    SimmSpline SS_vi_r_z(np_viZ_r, viZ_r_x, &viZ_r_y_s[0]);
+    VAS_P4_r->set_z_location(SS_vi_r_z);
+    VAS_P4_r->setZCoordinate(knee_angle_r);
+    VAS_r->updGeometryPath().updPathPointSet().adoptAndAppend(VAS_P4_r);
     VAS_r->setName("VAS_r");
     model.addComponent(VAS_r);
 
@@ -376,7 +481,15 @@ Model createGait2D() {
     GAS_p_r.append_approximation_coordinates("/jointset/ankle_r/ankle_angle_r");
     GAS_r->set_ignore_tendon_compliance(true);
     GAS_r->addNewPathPoint("GAS_r-P1", *femur_r, Vec3(-0.0190522, -0.393979, -0.0235645));
-    GAS_r->addNewPathPoint("GAS_r-P2", *femur_r, Vec3(-0.0300824, -0.403304, -0.0258708)); // cond
+
+    ConditionalPathPoint* GAS_P2_r = new ConditionalPathPoint();
+    GAS_P2_r->setName("GAS_r-P2");
+    GAS_P2_r->setCoordinate(knee_angle_r);
+    GAS_P2_r->setBody(*femur_r);
+    GAS_P2_r->setLocation(Vec3(-0.0300824, -0.403304, -0.0258708));
+    GAS_P2_r->setRangeMin(-0.785398);
+    GAS_P2_r->setRangeMax(0.174533);
+    GAS_r->updGeometryPath().updPathPointSet().adoptAndAppend(GAS_P2_r);
     GAS_r->addNewPathPoint("GAS_r-P3", *calcn_r, Vec3(0, 0.0283317, -0.0048438));
     GAS_r->setName("GAS_r");
     model.addComponent(GAS_r);
@@ -411,9 +524,9 @@ Model createGait2D() {
 
 // Test comparing muscle-tendon lengths and lengthening speeds obtained with
 // original MATLAB code and current implementation.
-void testPolynomialApproximation() {
+void testPolynomialApproximationImpl() {
 
-    Model model(createGait2D());
+    Model model(createGait2D_PolynomialPath());
     SimTK::State& state = model.initSystem();
 
     int nStates = model.getNumStateVariables();
@@ -491,9 +604,150 @@ void testPolynomialApproximation() {
                 "TA_r").getGeometryPath().getLengtheningSpeed(state);
     CHECK(0.290611643802287 == TA_lmt_r);
     CHECK(-0.021960474948139 == TA_vmt_r);
+}
+
+void testPolynomialApproximation() {
+
+    Model model(createGait2D_PolynomialPath());
+    SimTK::State& state = model.initSystem();
+
+    // Lengths
+    auto& hamstringsPath_r = model.getComponent<PathActuator>(
+            "hamstrings_r").getGeometryPath();
+    double hamstrings_lengthError_r =
+            hamstringsPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& BFSHPath_r = model.getComponent<PathActuator>(
+            "BFSH_r").getGeometryPath();
+    double BFSH_lengthError_r =
+            BFSHPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& GLUPath_r = model.getComponent<PathActuator>(
+            "GLU_r").getGeometryPath();
+    double GLU_lengthError_r =
+            GLUPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& IlioPsoasPath_r = model.getComponent<PathActuator>(
+            "IlioPsoas_r").getGeometryPath();
+    double IlioPsoas_lengthError_r =
+            IlioPsoasPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& RFPath_r = model.getComponent<PathActuator>(
+            "RF_r").getGeometryPath();
+    double RF_lengthError_r =
+            RFPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& VASPath_r = model.getComponent<PathActuator>(
+            "VAS_r").getGeometryPath();
+    double VAS_lengthError_r =
+            VASPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& GASPath_r = model.getComponent<PathActuator>(
+            "GAS_r").getGeometryPath();
+    double GAS_lengthError_r =
+            GASPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& SOLPath_r = model.getComponent<PathActuator>(
+            "SOL_r").getGeometryPath();
+    double SOL_lengthError_r =
+            SOLPath_r.computeApproximationErrorOnGrid(40, "length");
+    auto& TAPath_r = model.getComponent<PathActuator>(
+            "TA_r").getGeometryPath();
+    double TA_lengthError_r =
+            TAPath_r.computeApproximationErrorOnGrid(40, "length");
+
+    // Lengthening speeds
+    double hamstrings_lengtheningSpeedError_r =
+            hamstringsPath_r.computeApproximationErrorOnGrid(40,
+                    "lengthening_speed");
+    double BFSH_lengtheningSpeedError_r =
+            BFSHPath_r.computeApproximationErrorOnGrid(40,
+                    "lengthening_speed");
+    double GLU_lengtheningSpeedError_r =
+            GLUPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+    double IlioPsoas_lengtheningSpeedError_r =
+            IlioPsoasPath_r.computeApproximationErrorOnGrid(40,
+                    "lengthening_speed");
+    double RF_lengtheningSpeedError_r =
+            RFPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+    double VAS_lengtheningSpeedError_r =
+            VASPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+    double GAS_lengtheningSpeedError_r =
+            GASPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+    double SOL_lengtheningSpeedError_r =
+            SOLPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+    double TA_lengtheningSpeedError_r =
+            TAPath_r.computeApproximationErrorOnGrid(40, "lengthening_speed");
+
+    // Moment arms
+
+    const auto& hip_flexion_r = model.getCoordinateSet().get("hip_flexion_r");
+    const auto& knee_angle_r = model.getCoordinateSet().get("knee_angle_r");
+    const auto& ankle_angle_r = model.getCoordinateSet().get("ankle_angle_r");
+
+    double hamstrings_momArmErrorHip_r =
+            hamstringsPath_r.computeApproximationErrorOnGrid(40,
+                    "moment_arm", &hip_flexion_r);
+    double hamstrings_momArmErrorKnee_r =
+            hamstringsPath_r.computeApproximationErrorOnGrid(40,
+                    "moment_arm", &knee_angle_r);
+    double BFSH_momArmError_r =
+            BFSHPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &knee_angle_r);
+    double GLU_momArmError_r =
+            GLUPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &hip_flexion_r);
+    double IlioPsoas_momArmError_r =
+            IlioPsoasPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &hip_flexion_r);
+    double RF_momArmErrorHip_r =
+            RFPath_r.computeApproximationErrorOnGrid(40,"moment_arm",
+                    &hip_flexion_r);
+    double RF_momArmErrorKnee_r =
+            RFPath_r.computeApproximationErrorOnGrid(40,"moment_arm",
+                    &knee_angle_r);
+    double VAS_momArmError_r =
+            VASPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &knee_angle_r);
+    double GAS_momArmErrorKnee_r =
+            GASPath_r.computeApproximationErrorOnGrid(40,"moment_arm",
+                    &knee_angle_r);
+    double GAS_momArmErrorAnkle_r =
+            GASPath_r.computeApproximationErrorOnGrid(40,"moment_arm",
+                    &ankle_angle_r);
+    double SOL_momArmError_r =
+            SOLPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &ankle_angle_r);
+    double TA_momArmError_r =
+            TAPath_r.computeApproximationErrorOnGrid(40,"moment_arm", &ankle_angle_r);
+
+     CHECK(hamstrings_lengthError_r < 3e-3);
+     CHECK(BFSH_lengthError_r < 3e-3);
+     CHECK(GLU_lengthError_r < 3e-3);
+     CHECK(IlioPsoas_lengthError_r < 3e-3);
+     CHECK(RF_lengthError_r < 3e-3);
+     CHECK(VAS_lengthError_r < 3e-3);
+     CHECK(GAS_lengthError_r < 3e-3);
+     CHECK(SOL_lengthError_r < 3e-3);
+     CHECK(TA_lengthError_r < 3e-3);
+
+     CHECK(hamstrings_lengtheningSpeedError_r < 3e-3);
+     CHECK(BFSH_lengtheningSpeedError_r < 3e-3);
+     CHECK(GLU_lengtheningSpeedError_r < 3e-3);
+     CHECK(IlioPsoas_lengtheningSpeedError_r < 3e-3);
+     CHECK(RF_lengtheningSpeedError_r < 3e-3);
+     CHECK(VAS_lengtheningSpeedError_r < 3e-3);
+     CHECK(GAS_lengtheningSpeedError_r < 3e-3);
+     CHECK(SOL_lengtheningSpeedError_r < 3e-3);
+     CHECK(TA_lengtheningSpeedError_r < 3e-3);
+
+     CHECK(hamstrings_momArmErrorHip_r < 3e-3);
+     CHECK(hamstrings_momArmErrorKnee_r < 3e-3);
+     CHECK(BFSH_momArmError_r < 3e-3);
+     CHECK(GLU_momArmError_r < 3e-3);
+     CHECK(IlioPsoas_momArmError_r < 3e-3);
+     CHECK(RF_momArmErrorHip_r < 3e-3);
+     CHECK(RF_momArmErrorKnee_r < 3e-3);
+     CHECK(VAS_momArmError_r < 3e-3);
+     CHECK(GAS_momArmErrorKnee_r < 3e-3);
+     CHECK(GAS_momArmErrorAnkle_r < 3e-3);
+     CHECK(SOL_momArmError_r < 3e-3);
+     CHECK(TA_momArmError_r < 3e-3);
+
+
 
 }
 
 TEST_CASE("testPolynomialApproximation") {
+
+    testPolynomialApproximationImpl();
     testPolynomialApproximation();
 }
