cce08722a5de05441c0daee2b6e1842ed1d81d20
psbiomech <psbiomech@gmail.com>
2019-07-21 22:28:52 -0700

    removed sandbox, rolled back changes to cmake files

--- Moco/Moco/Common/TableProcessor.h
+++ Moco/Moco/Common/TableProcessor.h
@@ -5,7 +5,7 @@
  * -------------------------------------------------------------------------- *
  * Copyright (c) 2019 Stanford University and the Authors                     *
  *                                                                            *
- * Author(s): Christopher Dembia, Nicholas Bianco                             *
+ * Author(s): Christopher Dembia, Nicholas Bianco, Prasanna Sritharan         *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
