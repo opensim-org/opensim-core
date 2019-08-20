e16fdc2395bc360753a985d1cc9f642df9d46429
Christopher Dembia <cld72@cornell.edu>
2019-08-20 13:29:12 -0700

    Add stepbystep to doxygen.

--- Moco/Moco/MocoGoal/MocoGoal.h
+++ Moco/Moco/MocoGoal/MocoGoal.h
@@ -318,7 +318,7 @@ protected:
         values[0] = get_desired_average_speed() - (displacement / duration);
         if (getModeIsCost()) { values[0] = SimTK::square(values[0]); }
     }
-    void initializeOnModelImpl(const Model& model) const override {
+    void initializeOnModelImpl(const Model&) const override {
         setNumIntegralsAndOutputs(0, 1);
     }
 
