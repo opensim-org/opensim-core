7c9f84d77fd00666e51b90d92cd92b34d10960a3
Christopher Dembia <cld72@cornell.edu>
2019-09-09 18:29:44 -0700

    Sherlock: support arbitrary commands, fix parallelism.

--- .github/mocoshlo.py
+++ .github/mocoshlo.py
@@ -10,6 +10,10 @@ from pathlib import Path
 parser = argparse.ArgumentParser(
         description="Submit a Moco job to the Stanford Sherlock computing cluster.")
 parser.add_argument('directory', type=str, help="Location of input files.")
+parser.add_argument('--sunetid', type=str, default=None,
+                    help="SUNetID for logging into Sherlock. Overrides the "
+                         "the sunetid field in a config.yaml file in the "
+                         "current directory.")
 parser.add_argument('--duration', type=str, default="00:30:00",
         help="Maximum duration for the job in HH:MM:SS.")
 parser.add_argument('--name', type=str, default="",
@@ -27,11 +31,26 @@ parser.add_argument('--no-sshexit', dest='sshexit', action='store_false',
         help=("Do not exit the master session after submitting (if you plan to "
             "submit more jobs."))
 parser.set_defaults(sshmaster=True, sshexit=True)
+parser.add_argument('--command', type=str, default=None,
+                    help=("The job should run the given command."
+                          "Otherwise, the job is to run an OMOCO file named "
+                          "setup.omoco."))
+parser.add_argument('--mocotag', type=str, default='latest',
+                    help="Use container "
+                         "$GROUP_HOME/{sunetid}/opensim-moco/"
+                         "opensim-moco_{mocotag}. Default: latest.")
+parser.add_argument('--container', type=str, default=None,
+                    help="Path to the singularity container (.sif) "
+                         "on the cluster. Overrides --mocotag.")
+parser.add_argument('--exclude', type=str, default=None,
+                    help="Exclude files from copying to the cluster. This is "
+                         "passed onto rsync --exclude.")
 
 # TODO windows
 # TODO initial configuring gdrive on Sherlock.
 # TODO building a docker container from a branch (as a separate step?).
 # TODO allow customizing where files are saved in Google Drive.
+# TODO: support ignoring directories when copying (venv).
 
 
 args = parser.parse_args()
@@ -39,7 +58,7 @@ directory = args.directory
 if args.name != "":
     name = args.name
 else:
-    name = Path(directory).name
+    name = Path(directory).absolute().name
 duration = args.duration
 note = args.note
 sshmaster = args.sshmaster
@@ -48,16 +67,20 @@ sshexit = args.sshexit
 if ' ' in name:
     raise Exception("Cannot have spaces in name.")
 
-with open('config.yaml') as f:
-    config = yaml.safe_load(f)
-
-sunetid = config['sunetid']
+sunetid = None
+if args.sunetid:
+    sunetid = args.sunetid
+else:
+    with open('config.yaml') as f:
+        config = yaml.safe_load(f)
+    sunetid = config['sunetid']
 
 
 
 # Check that the directory contains setup.omoco.
-if not os.path.exists(os.path.join(directory, 'setup.omoco')):
-    raise Exception(f"setup.omoco is missing from {directory}.")
+if (not args.command and not os.path.exists(
+        os.path.join(directory, 'setup.omoco'))):
+    raise Exception(f'setup.omoco is missing from {directory}.')
 
 # if note:
 #     with open(os.path.join(directory, 'note.txt'), 'w') as f:
@@ -75,6 +98,7 @@ server = f"{sunetid}@login.sherlock.stanford.edu"
 now = datetime.datetime.now()
 date = now.strftime('%Y-%m-%d')
 time = '%s.%i' % (now.strftime('%Y-%m-%dT%H%M%S'), now.microsecond)
+# TODO: remove microseconds and put in SLURM JOBID.
 job_directory = '%s-%s' % (time, name)
 print(f"Submitting {job_directory}")
 mocojobs_dir = f"~/nmbl/mocojobs/"
@@ -82,18 +106,32 @@ server_job_dir = f"{mocojobs_dir}{job_directory}"
 if sshmaster:
     os.system(f'ssh -M -f -N -S {control_path} {server}')
 
+
+container = f'$GROUP_HOME/{sunetid}/opensim-moco/opensim-moco_{args.mocotag}.sif'
+if args.container:
+    container = args.container
+
+command = '/opensim-moco-install/bin/opensim-moco run-tool setup.omoco'
+if args.command:
+    command = args.command
+
 batch = f"""#!/bin/bash
 #SBATCH --job-name={name}
 #SBATCH --output={name}.out
 #SBATCH --error={name}.err
 #SBATCH --time={duration}
-#SBATCH --mail-type=END
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user={sunetid}@stanford.edu
 #SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=owners,normal
 module load gcc/8.1.0
 
-singularity exec $GROUP_HOME/opensim-moco/opensim-moco_latest.sif /opensim-moco-install/bin/opensim-moco run-tool setup.omoco
+echo "nproc: $(nproc)"
+# TODO: set to 2 * nproc?
+export OPENSIM_MOCO_PARALLEL=$(nproc)
+singularity exec {container} {command}
 
 
 # Upload results to Google Drive.
@@ -125,7 +163,11 @@ with open(f'{directory}/{name}.batch', 'w') as f:
 # Re-use existing SSH tunnel.
 # Recursively make the job directory.
 os.system(f'ssh -S {control_path} {server} "mkdir -p {mocojobs_dir}"')
-os.system(f"scp -o ControlPath={control_path} -r '{directory}/' {server}:{server_job_dir}")
+rsync_args = ''
+if args.exclude:
+    rsync_args = f'--exclude={args.exclude}'
+
+os.system(f"rsync --rsh='ssh -o ControlPath={control_path}' --archive --compress --recursive {rsync_args} '{directory}/' {server}:{server_job_dir}")
 
 os.system(f'ssh -S {control_path} {server} "cd {server_job_dir} && echo \"{note}\" > note.txt && sbatch {name}.batch"')
 
