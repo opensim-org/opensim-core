30090c88d0f4d3c4b5d9f4407d6c1b3865832e3a
jimmyDunne <dunne.jimmy@gmail.com>
2019-09-13 12:04:37 -0400

    add APDM example and update licence

--- Bindings/Java/Matlab/OpenSenseExampleFiles/OpenSense_CalibrateModel.m
+++ Bindings/Java/Matlab/OpenSenseExampleFiles/OpenSense_CalibrateModel.m
@@ -1,7 +1,7 @@
 %% CalibrateOpenSenseModel.m
 % Example code to calibrate orienation data with OpenSense. This
 % script uses the OpenSense library functions and is part of the OpenSense
-% Example files. 
+% Example files.
 
 % ----------------------------------------------------------------------- %
 % The OpenSim API is a toolkit for musculoskeletal modeling and           %
@@ -11,7 +11,7 @@
 % R24 HD065690) and by DARPA through the Warrior Web program.             %
 %                                                                         %
 % Copyright (c) 2005-2019 Stanford University and the Authors             %
-% Author(s): James Dunne                                                  %
+% Author(s): James Dunne, Ajay Seth, Ayman Habib, Jen Hicks, Chris Dembia %
 %                                                                         %
 % Licensed under the Apache License, Version 2.0 (the "License");         %
 % you may not use this file except in compliance with the License.        %
@@ -23,22 +23,22 @@
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
 % implied. See the License for the specific language governing            %
 % permissions and limitations under the License.                          %
-% ----------------------------------------------------------------------- %
+% ----------------------------------------------------------------------- % 
 
-%% Clear the Workspace variables. 
+%% Clear the Workspace variables.
 clear all; close all; clc;
 import org.opensim.modeling.*
 
 %% Set variables to use
 modelFileName = 'Rajagopal_2015.osim';        % The path to an input model
-orientationsFileName = 'MT_012005D6_009-001_orientations.sto';   % The path to orientation data for calibration 
+orientationsFileName = 'MT_012005D6_009-001_orientations.sto';   % The path to orientation data for calibration
 baseIMUName = 'pelvis_imu';                     % The base IMU is the IMU on the base body of the model that dictates the heading (forward) direction of the model.
-baseIMUHeading = CoordinateAxis(2);             % The Coordinate Axis of the base IMU that points in the heading direction. 
+baseIMUHeading = CoordinateAxis(2);             % The Coordinate Axis of the base IMU that points in the heading direction.
 visulizeCalibration = true;                     % Boolean to Visualize the Output model
 
 %% Instantiate an OpenSenseUtilities object
 ou = OpenSenseUtilities();
- 
+
 %% Generate a model that calibrates the IMU sensors to a model pose.
 model = ou.calibrateModelFromOrientations(modelFileName,...
                                           orientationsFileName,...
