757eaab18cfebd7dd98ade2dfdb332b26a375f16
Nicholas Bianco <nbianco@stanford.edu>
2019-09-13 17:26:06 -0700

    Add equilibrium goals to bindings // fix MocoControlGoal exponent type

--- Moco/Moco/MocoGoal/MocoControlGoal.h
+++ Moco/Moco/MocoGoal/MocoControlGoal.h
@@ -69,7 +69,7 @@ public:
             const std::string& controlName, const double& weight);
 
     /// Set the exponent on the control signals.
-    void setExponent(double exponent) { set_exponent(exponent); }
+    void setExponent(int exponent) { set_exponent(exponent); }
     double getExponent() const { return get_exponent(); }
 
     /// Set if the goal should be divided by the displacement of the system's
