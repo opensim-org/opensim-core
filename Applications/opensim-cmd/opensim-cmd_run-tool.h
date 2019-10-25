bd83a90427512c55adbc62f20e9f55c0b78802ac
Christopher Dembia <cld72@cornell.edu>
2019-10-25 10:50:06 -0700

    Remove command-line logging option for now.

--- Applications/opensim-cmd/opensim-cmd_run-tool.h
+++ Applications/opensim-cmd/opensim-cmd_run-tool.h
@@ -38,7 +38,6 @@ Usage:
   opensim-cmd run-tool -h | --help
 
 Options:
-  -d <level>, --log-level <level>  Specify the logging level (verbosity).
   -L <path>, --library <path>  Load a plugin.
 
 Description:
--- Applications/opensim-cmd/opensim-cmd_update-file.h
+++ Applications/opensim-cmd/opensim-cmd_update-file.h
@@ -38,7 +38,6 @@ Usage:
   opensim-cmd update-file -h | --help
 
 Options:
-  -d <level>, --log-level <level>  Specify the logging level (verbosity).
   -L <path>, --library <path>  Load a plugin.
 
 Description:
