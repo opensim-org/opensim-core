5caf5d050df6d05120ea1cc777fe7141a635590f
jimmyDunne <dunne.jimmy@gmail.com>
2019-10-31 09:51:37 -0700

    add direction correction;

--- OpenSim/Simulation/OpenSense/OpenSenseUtilities.cpp
+++ OpenSim/Simulation/OpenSense/OpenSenseUtilities.cpp
@@ -94,7 +94,7 @@ TimeSeriesTable_<SimTK::Rotation> OpenSenseUtilities::
             pelvisHeading = pelvisHeading.negate();
         UnitVec3 groundX = UnitVec3(1, 0, 0);
         SimTK::Real angularDifference = acos(~pelvisHeading*groundX);
-        // Compute the angular coorrection
+        // Compute the angular correction. 
         SimTK::Vec3 xproduct = (pelvisHeading % groundX);
         if (xproduct.get(2)>0){
           angularDifference *= -1;
