242eb98a723fbbd458155b592d1642ff46800490
aymanhab <ahabib@stanford.edu>
2019-11-12 15:05:59 -0800

    Add ModelCalibrator class to Bindings for potential use

--- Bindings/OpenSimHeaders_simulation.h
+++ Bindings/OpenSimHeaders_simulation.h
@@ -136,6 +136,7 @@
 #include <OpenSim/Simulation/CoordinateReference.h>
 #include <OpenSim/Simulation/OrientationsReference.h>
 #include <OpenSim/Simulation/InverseKinematicsSolver.h>
+#include <OpenSim/Simulation/OpenSense/ModelCalibrator.h>
 #include <OpenSim/Simulation/OpenSense/OpenSenseUtilities.h>
 #include <OpenSim/Simulation/OpenSense/InverseKinematicsStudy.h>
 
