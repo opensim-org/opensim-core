7619a25b046bfbcd6ce99b29ddf937d3673a7ecd
Christopher Dembia <cld72@cornell.edu>
2019-11-15 17:02:26 -0800

    Use logging in GCVSpline, Object, Signal.

--- OpenSim/Common/Object.cpp
+++ OpenSim/Common/Object.cpp
@@ -239,12 +239,8 @@ bool Object::operator==(const Object& other) const
     auto printDiff = [](const std::string& name,
                             const std::string& thisValue,
                             const std::string& otherValue) {
-        if (Object::getDebugLevel() > 0) {
-            std::cout << "In Object::operator==(), differing " << name << ":\n"
-                << "left: " << thisValue
-                << "\nright: " << otherValue << std::endl;
-        }
-
+        log_debug("In Object::operator==(), differing {}:\nleft: {}\nright: {}",
+                name, thisValue, otherValue);
     };
     if (getConcreteClassName()  != other.getConcreteClassName()) {
         printDiff("ConcreteClassName", getConcreteClassName(),
@@ -512,22 +508,18 @@ registerType(const Object& aObject)
     // GET TYPE
     const string& type = aObject.getConcreteClassName();
     if(type.empty()) {
-        printf("Object.registerType: ERR- no type name has been set.\n");
+        log_error("Object.registerType: no type name has been set.");
         return;
     }
-    if (getDebugLevel() >= 2) {
-        cout << "Object.registerType: " << type << " .\n";
-    }
+    log_trace("Object.registerType: {}.", type);
 
     // REPLACE IF A MATCHING TYPE IS ALREADY REGISTERED
     for(int i=0; i <_registeredTypes.size(); ++i) {
         Object *object = _registeredTypes.get(i);
         if(object->getConcreteClassName() == type) {
-            if (getDebugLevel() >= 2) {
-                cout<<"Object.registerType: replacing registered object of type ";
-                cout<<type;
-                cout<<"\n\twith a new default object of the same type."<<endl;
-            }
+            log_debug("Object.registerType: replacing registered object of "
+                      "type {} with a new default object of the same type.",
+                      type);
             Object* defaultObj = aObject.clone();
             defaultObj->setName(DEFAULT_NAME);
             _registeredTypes.set(i,defaultObj);
@@ -1369,7 +1361,7 @@ print(const string &aFileName) const
 {
     // Default to strict exception to avoid creating bad files
     // but for debugging allow users to be more lenient.
-    if (getDebugLevel() >= 1) {
+    if (Logger::shouldLog(Logger::Level::Debug)) {
         try {
             warnBeforePrint();
         } catch (...) {}
