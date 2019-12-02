66ae110c17dd4176988a187f8dbaecad2f37dee3
Ayman Habib <ahabib@stanford.edu>
2019-12-01 16:36:29 -0800

    Update IMUInverseKinematicsTool.h
    Update property comment for clarity.

--- OpenSim/Simulation/OpenSense/IMUInverseKinematicsTool.h
+++ OpenSim/Simulation/OpenSense/IMUInverseKinematicsTool.h
@@ -75,7 +75,7 @@ public:
 
     OpenSim_DECLARE_PROPERTY(sensor_to_opensim_rotations, SimTK::Vec3,
             "Space fixed Euler angles (XYZ order) from IMU Space to OpenSim."
-            " Default to Identity");
+            " Default to (0, 0, 0).");
 
     OpenSim_DECLARE_LIST_PROPERTY_SIZE(time_range, double, 2,
         "The time range for the study.");
