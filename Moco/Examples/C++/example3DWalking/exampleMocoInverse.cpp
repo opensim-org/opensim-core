aa997bb35f292f3d250e4ab26edceaf9434a8bd2
Christopher Dembia <cld72@cornell.edu>
2019-12-10 11:22:52 -0800

    Add exampleMocoInverse.

--- Moco/Examples/C++/example3DWalking/exampleMocoInverse.cpp
+++ Moco/Examples/C++/example3DWalking/exampleMocoInverse.cpp
@@ -3,7 +3,7 @@
  * -------------------------------------------------------------------------- *
  * Copyright (c) 2019 Stanford University and the Authors                     *
  *                                                                            *
- * Author(s): Nicholas Bianco                                                 *
+ * Author(s): Christopher Dembia                                              *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
@@ -16,31 +16,11 @@
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
 
-/// This example features two different tracking problems solved using the
-/// MocoTrack tool. 
-///  - The first problem demonstrates the basic usage of the tool interface
-///    to solve a torque-driven marker tracking problem. 
-///  - The second problem shows how to customize a muscle-driven state tracking 
-///    problem using more advanced features of the tool interface.
-/// 
-/// Data and model source: https://simtk.org/projects/full_body
+/// This example shows how to use the MocoInverse tool to exactly prescribe a
+/// motion and estimate muscle behavior for walking.
+/// This problem solves in about 5 minutes.
 ///
-/// TODO move to the readme.
-/// Model
-/// -----
-/// The model described in the file 'subject_walk_armless.osim' included in this
-/// file is a modified version of the Rajagopoal et al. 2016 musculoskeletal 
-/// model. The lumbar, subtalar, and mtp coordinates have been replaced with
-/// WeldJoint%s and residual actuators have been added to the pelvis (1 N-m for
-/// rotational coordinates and 10 N for translational coordinates). Finally, the
-/// arms and all associated components have been removed for simplicity.
-/// 
-/// Data
-/// ----
-/// The coordinate and marker data included in the 'coordinates.sto' and 
-/// 'marker_trajectories.trc' files also come from the Rajagopal et al. 2016
-/// model distribution. The coordinates were computed using inverse kinematics
-/// and modified via the Residual Reduction Algorithm (RRA). 
+/// See the README.txt next to this file for more information.
 
 #include <Moco/osimMoco.h>
 
@@ -48,7 +28,43 @@ using namespace OpenSim;
 
 int main() {
 
-    // TODO.
+    // Construct the MocoInverse tool.
+    MocoInverse inverse;
+    inverse.setName("example3DWalking_MocoInverse");
+
+    // Construct a ModelProcessor and set it on the tool. The default
+    // muscles in the model are replaced with optimization-friendly
+    // DeGrooteFregly2016Muscles, and adjustments are made to the default muscle
+    // parameters.
+    ModelProcessor modelProcessor =
+            ModelProcessor("subject_walk_armless.osim") |
+                    ModOpAddExternalLoads("grf_walk.xml") |
+                    ModOpIgnoreTendonCompliance() |
+                    ModOpReplaceMusclesWithDeGrooteFregly2016() |
+                    // Only valid for DeGrooteFregly2016Muscles.
+                    ModOpIgnorePassiveFiberForcesDGF() |
+                    // Only valid for DeGrooteFregly2016Muscles.
+                    ModOpScaleActiveFiberForceCurveWidthDGF(1.5) |
+                    ModOpAddReserves(1.0);
+    inverse.setModel(modelProcessor);
+
+    // Construct a TableProcessor of the coordinate data and pass it to the
+    // inverse tool. TableProcessors can be used in the same way as
+    // ModelProcessors by appending TableOperators to modify the base table.
+    // A TableProcessor with no operators, as we have here, simply returns the
+    // base table.
+    inverse.setKinematics(TableProcessor("coordinates.sto"));
+
+    // Initial time, final time, and mesh interval.
+    inverse.set_initial_time(0.81);
+    inverse.set_final_time(1.79);
+    inverse.set_mesh_interval(0.02);
+
+    inverse.set_kinematics_allow_extra_columns(true);
+
+    MocoInverseSolution solution = inverse.solve();
+    solution.getMocoSolution().write(
+            "example3DWalking_MocoInverse_solution.sto");
 
     return EXIT_SUCCESS;
 }
