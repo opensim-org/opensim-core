ca8ccdc091921002d92a0fd284bd4b5784b082ef
aymanhab <ahabib@stanford.edu>
2020-03-10 11:59:31 -0700

    Add orientation_file property and lock translational coords if only tracking orientations

--- OpenSim/Tools/InverseKinematicsTool.h
+++ OpenSim/Tools/InverseKinematicsTool.h
@@ -87,6 +87,9 @@ public:
     OpenSim_DECLARE_PROPERTY(marker_file, std::string,
             "Name/path to a .trc or .sto file of type Vec3 of marker data.");
 
+    OpenSim_DECLARE_PROPERTY(orientations_file, std::string,
+            "Name/path to a .sto file of sensor frame orientations as quaternions.");
+
 
     OpenSim_DECLARE_PROPERTY(coordinate_file, std::string,
             "The name of the storage (.sto or .mot) file containing the time "
