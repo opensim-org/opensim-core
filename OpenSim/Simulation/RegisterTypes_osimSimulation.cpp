178debd840a2a40a67d7b5f2deaf4d4903579401
aymanhab <ahabib@stanford.edu>
2020-06-01 13:37:29 -0700

    Register OrientationWeight for serialization

--- OpenSim/Simulation/RegisterTypes_osimSimulation.cpp
+++ OpenSim/Simulation/RegisterTypes_osimSimulation.cpp
@@ -123,7 +123,7 @@
 #include "SimbodyEngine/Coordinate.h"
 #include "SimbodyEngine/SpatialTransform.h"
 #include "OpenSense/IMUPlacer.h"
-
+#include "OrientationsReference.h"
 #include "StatesTrajectoryReporter.h"
 
 #include <string>
@@ -262,6 +262,7 @@ OSIMSIMULATION_API void RegisterTypes_osimSimulation()
     Object::registerType( Bhargava2004MuscleMetabolicsProbe() );
     Object::registerType( Bhargava2004MuscleMetabolicsProbe_MetabolicMuscleParameterSet() );
     Object::registerType( Bhargava2004MuscleMetabolicsProbe_MetabolicMuscleParameter() );
+    Object::registerType(OrientationWeight());
 
     Object::registerType( IMUPlacer());
     
