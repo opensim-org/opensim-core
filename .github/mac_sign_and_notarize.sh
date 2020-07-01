5c10b4176527dddbac32570306cf901a088ee976
Christopher Dembia <cld72@cornell.edu>
2020-07-01 16:50:04 -0700

    Update mac_sign_and_notarize.sh to remove mention of Moco.

--- .github/mac_sign_and_notarize.sh
+++ .github/mac_sign_and_notarize.sh
@@ -4,7 +4,7 @@
 # software is signed by a known developer. Starting with macOS Catalina,
 # Gatekeeper also checks that the software is notarized, and also requires that
 # command-line tools are signed (previously, command-line tools were not
-# quaratined like ".app"s were).
+# quarantined like ".app"s were).
 #
 # Signing means that someone with an Apple Developer ID ran 'codesign' or
 # 'productsign' on the software. This tells users that the software they have
@@ -16,10 +16,10 @@
 # signed. However, checking that software is notarized requires an internet
 # connection, UNLESS the notarization has been stapled to the software.
 #
-# This script takes a Moco ZIP, unzips it to a temporary directory, signs all
-# executables and shared libraries, re-zips, then submits the ZIP to Apple to
-# be notarized. You will get an email indicating if notarization succeeded;
-# though this might take a few hours.
+# This script takes an opensim-core binary ZIP, unzips it to a temporary
+# directory, signs all executables and shared libraries, re-zips, then submits
+# the ZIP to Apple to be notarized. You will get an email indicating if
+# notarization succeeded; though this might take a few hours.
 #
 # The resulting signed ZIP is saved to a folder <zip-name>-signed. This ZIP is
 # what should be provided to users.
@@ -29,7 +29,7 @@
 # sign and notarize.
 #
 # Example:
-# ./mac_sign_and_notarize.sh -s ABC0123456 -i <Apple-ID> -f opensim-moco.zip
+# ./mac_sign_and_notarize.sh -s ABC0123456 -i <Apple-ID> -f opensim-core.zip
 #
 # Run `security find-identity -p basic -v` to get the Developer ID identity; 
 # it's in parentheses at the end of the line. You must have installed a
@@ -47,16 +47,22 @@
 #
 # Ideally, we would also staple the notarization to the ZIP, but the stapler
 # command currently only works on ".app"s, ".pkg"s, and ".dmg"s, and not on
-# ".zip"s or plain executables. So, for now, Moco users will need access to
+# ".zip"s or plain executables. So, for now, users will need access to
 # the internet for their Gatekeeper to determine if the software is notarized.
 #
 # While Apple's documentation says that macOS requires software to be signed
-# AND notarized, testing indicates that it is sufficent for software to be only
+# AND notarized, testing indicates that it is sufficient for software to be only
 # signed (not notarized) for Gatekeeper to allow the user to run the software.
 # This may change over time.
 # 
 # https://developer.apple.com/documentation/xcode/notarizing_macos_software_before_distribution
 # https://scriptingosx.com/2019/09/notarize-a-command-line-tool/
+#
+# Workaround: Users can run the following command to remove the "quarantine"
+# file attribute from code, circumventing the need for a code signature or notarization.
+#
+#   xattr -r -d com.apple.quarantine <opensim-core-install-dir>
+#
 
 
 while getopts s:i:f: option
@@ -65,7 +71,7 @@ while getopts s:i:f: option
     in
     s) IDENTITY=${OPTARG};;
     i) APPLE_ID=${OPTARG};;
-    f) MOCO_ZIP=${OPTARG};;
+    f) OSIM_ZIP=${OPTARG};;
     esac
 done
 
@@ -82,24 +88,24 @@ function sign_code() {
 }
 
 function cleanup() {
-    rm -rf "$MOCO_TEMP_DIR"
-    echo "Deleted temporary directory $MOCO_TEMP_DIR."
+    rm -rf "$OSIM_TEMP_DIR"
+    echo "Deleted temporary directory $OSIM_TEMP_DIR."
 }
 
 # https://stackoverflow.com/questions/4632028/how-to-create-a-temporary-directory
-MOCO_TEMP_DIR=`mktemp -d -t 'opensim-moco-unzipping'`
+OSIM_TEMP_DIR=`mktemp -d -t 'opensim-core-unzipping'`
 
-if [[ ! "$MOCO_TEMP_DIR" || ! -d "$MOCO_TEMP_DIR" ]]; then
+if [[ ! "$OSIM_TEMP_DIR" || ! -d "$OSIM_TEMP_DIR" ]]; then
     echo "Could not create temporary directory."
     exit 1
 fi
-echo "Created temporary directory $MOCO_TEMP_DIR."
+echo "Created temporary directory $OSIM_TEMP_DIR."
 
 # Unzip quietly.
-unzip -q -d "$MOCO_TEMP_DIR" "$MOCO_ZIP"
+unzip -q -d "$OSIM_TEMP_DIR" "$OSIM_ZIP"
 
 # Sign executables and shared libraries.
-for dir in `find "$MOCO_TEMP_DIR" -mindepth 1 -maxdepth 1 -type d`; do
+for dir in `find "$OSIM_TEMP_DIR" -mindepth 1 -maxdepth 1 -type d`; do
     for i in `find "$dir" -name "*.dylib" -type f`; do
         sign_code "$i"
     done
@@ -108,26 +114,26 @@ for dir in `find "$MOCO_TEMP_DIR" -mindepth 1 -maxdepth 1 -type d`; do
         sign_code "$i"
     done
     sign_code "$dir/bin/opensim-cmd"
-    sign_code "$dir/bin/opensim-moco"
+    sign_code "$dir/bin/opensense"
     sign_code "$dir/sdk/Simbody/libexec/simbody/simbody-visualizer.app"
 done
 
 # ZIP the signed files.
-mkdir -p ${MOCO_ZIP}-signed
+mkdir -p ${OSIM_ZIP}-signed
 ORIG_DIR=`pwd`
-cd $MOCO_TEMP_DIR
+cd $OSIM_TEMP_DIR
 zip --quiet --symlinks --recurse-paths \
-    "${ORIG_DIR}/${MOCO_ZIP}-signed/${MOCO_ZIP}" .
+    "${ORIG_DIR}/${OSIM_ZIP}-signed/${OSIM_ZIP}" .
 cd - 
 
 
 # Notarizing.
 # ===========
-xcrun altool --notarize-app --primary-bundle-id "org.opensim.moco" \
+xcrun altool --notarize-app --primary-bundle-id "org.opensim" \
     --username "$APPLE_ID" \
     --password "@keychain:Developer-altool" \
     --asc-provider "$IDENTITY" \
-    --file "${ORIG_DIR}/${MOCO_ZIP}-signed/${MOCO_ZIP}"
+    --file "${ORIG_DIR}/${OSIM_ZIP}-signed/${OSIM_ZIP}"
 
 # If notarization fails, you'll get an email from Apple with a RequestUUID.
 # Run the following command:
