233fe71674d6cb12c81a2e8d11aca49afe846578
Nicholas Bianco <nbianco@stanford.edu>
2020-10-08 14:49:38 -0700

    Bump force tolerance in testStaticOptimization

--- Applications/Analyze/test/testStaticOptimization.cpp
+++ Applications/Analyze/test/testStaticOptimization.cpp
@@ -58,7 +58,7 @@ int main()
     // and the 'standard' solution, which was closest to using 
     // Thelen2003Muscle_Deprecated muscle formulation.
     double actTols[4] = {0.005, 0.025, 0.04, 0.04};
-    double forceTols[4] = {0.5, 4, 5, 6};
+    double forceTols[4] = {1, 4, 5, 6};
     
     SimTK::Array_<std::string> failures;
 
