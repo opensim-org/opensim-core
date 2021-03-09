d2cda79d412683e5db31c6dc95a7c4b752a8f236
Colin Smith <colin.cs.smith@gmail.com>
2021-03-09 18:27:41 +0100

    update bindings to wrap osim jam

---
+++ Bindings/OpenSimHeaders_jam.h
@@ -0,0 +1,18 @@
+#ifndef OPENSIM_OPENSIM_HEADERS_JAM_H_
+#define OPENSIM_OPENSIM_HEADERS_JAM_H_
+/* This header is only used with SWIG to create bindings.
+ */
+
+#include <OpenSim/JAM/About.h>
+#include <OpenSim/JAM/base64.h>
+#include <OpenSim/JAM/COMAKInverseKinematicsTool.h>
+#include <OpenSim/JAM/COMAKTarget.h>
+#include <OpenSim/JAM/COMAKTool.h>
+#include <OpenSim/JAM/ForsimTool.h>
+#include <OpenSim/JAM/H5FileAdapter.h>
+#include <OpenSim/JAM/JointMechanicsTool.h>
+#include <OpenSim/JAM/JAMUtilities.h>
+#include <OpenSim/JAM/VTPFileAdapter.h>
+#include <OpenSim/JAM/osimJAMDLL.h>
+
+#endif // OPENSIM_OPENSIM_HEADERS_JAM_H_
---
+++ Bindings/jam.i
@@ -0,0 +1,11 @@
+%include <OpenSim/JAM/osimJAMDLL.h>
+%include <OpenSim/JAM/About.h>
+%include <OpenSim/JAM/COMAKInverseKinematicsTool.h>
+%include <OpenSim/JAM/COMAKTarget.h>
+%include <OpenSim/JAM/COMAKTool.h>
+%include <OpenSim/JAM/ForsimTool.h>
+%include <OpenSim/JAM/H5FileAdapter.h>
+%include <OpenSim/JAM/JointMechanicsTool.h>
+%include <OpenSim/JAM/JAMUtilities.h>
+%include <OpenSim/JAM/VTPFileAdapter.h>
+%include <OpenSim/JAM/MocoJointReactionGoal.h>
