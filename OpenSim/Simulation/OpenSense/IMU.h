5bb642a9aee5595bdfeef9140b2061f64b92d62c
aymanhab <ahabib@stanford.edu>
2021-05-24 17:01:18 -0700

    Change color to Orange to match old example, create IMU objects rather than lowevel geometry and a separate Frame in OpenSenseUtilities and IMUPlacer.

--- OpenSim/Simulation/OpenSense/IMU.h
+++ OpenSim/Simulation/OpenSense/IMU.h
@@ -82,10 +82,12 @@ public:
 
         // @TODO default color, size, shape should be obtained from hints
         const OpenSim::PhysicalFrame& physFrame = this->get_frame();
+        SimTK::Transform relativeXform = physFrame.findTransformInBaseFrame();
         appendToThis.push_back(
                 SimTK::DecorativeBrick(SimTK::Vec3(0.02, 0.01, 0.005))
                         .setBodyId(physFrame.getMobilizedBodyIndex())
-                                        .setColor(SimTK::Purple));
+                                        .setColor(SimTK::Orange)
+                        .setTransform(relativeXform));
     }
 
 private:
--- OpenSim/Simulation/OpenSense/OpenSenseUtilities.cpp
+++ OpenSim/Simulation/OpenSense/OpenSenseUtilities.cpp
@@ -279,7 +279,7 @@ std::vector<std::shared_ptr<OpenSim::IMU> > OpenSenseUtilities::addModelIMUs(
         const Component& comp = model.getComponent(path);
         next_imu->setName(comp.getName() + "_imu");
         next_imu->connectSocket_frame(comp);
-        model.addComponent(next_imu);
+        model.addModelComponent(next_imu);
         // make sure it's a Frame
         selectedIMUs.push_back(std::shared_ptr<IMU>(next_imu)); 
     }
