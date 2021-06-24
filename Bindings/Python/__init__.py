c6ea54571795ce54d55573e9be77487e4bf6eec2
aymanhab <ahabib@stanford.edu>
2021-06-24 10:44:42 -0700

    create a new file dll_location.txt to be used to locate installation folder at run time

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,3 +1,12 @@
+import sys
+import os
+if (sys.version_info.major == 3 and sys.version_info.minor >= 8):
+    cwd = os.getcwd()
+    # print(cwd)
+    print('Writing new dll_location.txt conatining '+cwd)
+    with open('opensim/dll_location.txt', 'r') as reader:
+        os.add_dll_directory(reader.read())
+
 from .simbody import *
 from .common import *
 from .simulation import *
