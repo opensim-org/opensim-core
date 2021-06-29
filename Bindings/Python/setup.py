478edfd70b63cc3d7bd24dfbc0b098fafaf7d432
aymanhab <ahabib@stanford.edu>
2021-06-29 13:54:29 -0700

    Instead of writing to dll_location.txt that is dependent on current directory, write install location directly to __init__.py

--- Bindings/Python/setup.py
+++ Bindings/Python/setup.py
@@ -2,11 +2,14 @@
 
 import os
 import sys
+import fileinput
 from setuptools import setup
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
    install_path=os.path.abspath('../../bin')
-   with open('opensim/dll_location.txt', 'w') as writer:
-      writer.write(install_path)
+   new_path = install_path.replace(os.sep, '/')
+   with fileinput.FileInput('opensim/__init__.py', inplace=True, backup='.bak') as file:
+     for line in file:
+          print(line.replace('DLL_PATH', "'"+new_path+"'"), end='')
 
 # This provides the variable `__version__`.
 if sys.version_info[0] < 3:
