588173bb24ea1d98a61a212aa20f6e463ad0bc4c
aymanhab <ahabib@stanford.edu>
2021-06-29 16:27:48 -0700

    Allow for running tests without install on windows

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,7 +1,12 @@
 import sys
 import os
+from pathlib import Path
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-        os.add_dll_directory(DLL_PATH)
+        if (Path('opensim/dll_location.txt').is_file()):
+            with open('opensim/dll_location.txt', 'r') as reader:
+                os.add_dll_directory(reader.read())
+        else:
+            os.add_dll_directory(DLL_PATH)
 
 from .simbody import *
 from .common import *
