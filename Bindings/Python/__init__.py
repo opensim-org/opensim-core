ad9e41c821b783247ee8f5d196a2e8b117fdef7f
aymanhab <ahabib@stanford.edu>
2021-06-30 16:40:35 -0700

    Remove creation and reference to dll_location.txt

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,11 +1,7 @@
 import sys
 import os
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-        if (os.path.isfile('opensim/dll_location.txt')):
-            with open('opensim/dll_location.txt', 'r') as reader:
-                os.add_dll_directory(reader.read())
-        else:
-            os.add_dll_directory(DLL_PATH)
+   os.add_dll_directory(DLL_PATH)
 
 from .simbody import *
 from .common import *
