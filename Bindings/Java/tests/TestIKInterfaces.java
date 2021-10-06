ffe512eda7de1a42066b9f0515a02613d5f38617
aymanhab <ahabib@stanford.edu>
2021-10-06 09:17:29 -0700

    Beef up test of IKInterfaces by creating SetMarkerWeights and populating it.

--- Bindings/Java/tests/TestIKInterfaces.java
+++ Bindings/Java/tests/TestIKInterfaces.java
@@ -1,4 +1,5 @@
 import org.opensim.modeling.*;
+import java.io.IOException;
 
 class TestIKInterfaces {
     public static void test_SolverConstuctor() {
@@ -10,8 +11,21 @@ class TestIKInterfaces {
         InverseKinematicsSolver solver = new InverseKinematicsSolver(
             model, markersRef, coordinateRef);
     }
-
+    static void test_MarkerWeightSet() {
+        try {
+            Model model= new Model("gait10dof18musc_subject01.osim");
+            model.initSystem();
+            SetMarkerWeights markerWeightSet = new SetMarkerWeights();
+            for (int i=0; i< model.getMarkerSet().getSize(); i++)
+                markerWeightSet.cloneAndAppend(new 
+                    MarkerWeight(model.getMarkerSet().get(i).getName(), 1.0));
+        }
+        catch (IOException ex) {
+            System.exit(1);
+        }
+    }
     public static void main(String[] args) {
         test_SolverConstuctor();
+        test_MarkerWeightSet();
     }
 };
