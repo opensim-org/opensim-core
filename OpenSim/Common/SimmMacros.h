60b4b04f6012ddbfb39fc83fc1394b0ccdfdfceb
Ayman Habib <ahabib@stanford.edu>
2021-12-13 11:19:43 -0800

    Wrapping performance and refactoring (#3114)
    * minor refactor to reduce num calls to get size of Array property
    
    * Avoid duplicate transformation to Ground frame when computing path length
    
    * remove_unused_variable
    
    * Remove macros that are replaceable by Vec3 operators
    
    * Inline usage of Macro in WrapEllipsoid
    
    * More refactoring, use det for determinant, vec operators instead of loops, inline method CalcDistanceSquaredPointToLine
    
    * Update continuous_integration.yml
    
    Do not use cached dependencies built before osx upgrade on github
    
    * Remove rather than comment out function that was moved to header and inlined.
    
    * Update GeometryPath.cpp
    
    Reuse variable for wrapSetSize
    
    * Update continuous_integration.yml
    
    Undo cache change since we verified build succeeds without it.
    
    * Use normSqr for consistency

--- OpenSim/Common/SimmMacros.h
+++ OpenSim/Common/SimmMacros.h
@@ -27,24 +27,14 @@ namespace OpenSim {
 
 #define TINY_NUMBER 0.0000001
 #define ROUNDOFF_ERROR 0.0000000000002
-#define MAX(a,b) ((a)>=(b)?(a):(b))
-#define MIN(a,b) ((a)<=(b)?(a):(b))
-#define DABS(a) ((a)>(double)0.0?(a):(-(a)))
+#define MAX(a,b) (std::max(a, b))
+#define MIN(a,b) (std::min(a, b))
+#define DABS(a) (std::abs(a))
 #define DSIGN(a) ((a)>=0.0?(1):(-1))
 #define SQR(x)  ((x) * (x))
 #define EQUAL_WITHIN_ERROR(a,b) (DABS(((a)-(b))) <= ROUNDOFF_ERROR)
 #define NOT_EQUAL_WITHIN_ERROR(a,b) (DABS(((a)-(b))) > ROUNDOFF_ERROR)
 #define EQUAL_WITHIN_TOLERANCE(a,b,c) (DABS(((a)-(b))) <= (c))
-#define MAKE_3DVECTOR(pt1,pt2,pt3) {pt3[0]=(pt2[0]-pt1[0]);pt3[1]=(pt2[1]-pt1[1]);pt3[2]=(pt2[2]-pt1[2]);}
-#define MAKE_3DVECTOR21(pt1,pt2,pt3) {pt3[0]=(pt1[0]-pt2[0]);pt3[1]=(pt1[1]-pt2[1]);pt3[2]=(pt1[2]-pt2[2]);}
-#define COPY_1X3VECTOR(from,to) {\
-to[0] = from[0];\
-to[1] = from[1];\
-to[2] = from[2];\
-}
-#define CALC_DETERMINANT(m) (((m[0][0]) * ((m[1][1])*(m[2][2]) - (m[1][2])*(m[2][1]))) - \
-((m[0][1]) * ((m[1][0])*(m[2][2]) - (m[1][2])*(m[2][0]))) + \
-((m[0][2]) * ((m[1][0])*(m[2][1]) - (m[1][1])*(m[2][0]))))
 
 } // end of namespace OpenSim
 
