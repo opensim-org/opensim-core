be987cd878b635ba2c9b015eceaed56a63a5f621
Ayman Habib <ahabib@stanford.edu>
2022-02-24 02:03:47 -0800

    Hint where to locate colpack

--- Bindings/Python/setup_win_python38.py
+++ Bindings/Python/setup_win_python38.py
@@ -3,7 +3,10 @@ import sys
 import os
 import fileinput
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-   install_path = os.path.abspath('../../bin')
+   if (len(sys.argv)==2):
+      install_path = os.path.abspath('../../bin')
+   else:
+      install_path = os.path.abspath('opensim') # Conda layout on windows
    new_path = install_path.replace(os.sep, '/')
    print ('install path found as '+ new_path)
    with fileinput.FileInput('opensim/__init__.py', inplace=True, backup='.bak') as file:
