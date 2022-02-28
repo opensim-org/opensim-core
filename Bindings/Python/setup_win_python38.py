5c2598ec47c21ae9da5c96c243227df09cb99ebd
Ayman Habib <ahabib@stanford.edu>
2022-02-28 09:51:37 -0800

    Use call to decide add_dll_directory arguments

--- Bindings/Python/setup_win_python38.py
+++ Bindings/Python/setup_win_python38.py
@@ -5,10 +5,10 @@ import fileinput
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
    if (len(sys.argv)==2):
       install_path = os.path.abspath('../../bin')
+      new_path = install_path.replace(os.sep, '/')
+      print ('install path found as '+ new_path)
    else:
-      install_path = os.path.abspath('opensim') # Conda layout on windows
-   new_path = install_path.replace(os.sep, '/')
-   print ('install path found as '+ new_path)
+      new_path = 'os.path.dirname(os.path.realpath(__file__))' # Conda layout on windows
    with fileinput.FileInput('opensim/__init__.py', inplace=True, backup='.bak') as file:
      for line in file:
           print(line.replace('DLL_PATH', "'" + new_path + "'"), end='')
