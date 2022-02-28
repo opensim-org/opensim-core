640ba19734e904df7243e737592df18ccc311293
Ayman Habib <ahabib@stanford.edu>
2022-02-28 15:26:06 -0800

    tweak setup_win_python38.py to account for conda layout

--- Bindings/Python/setup_win_python38.py
+++ Bindings/Python/setup_win_python38.py
@@ -3,7 +3,7 @@ import sys
 import os
 import fileinput
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-   if (len(sys.argv)==2):
+   if (len(sys.argv)==1):
       install_path = os.path.abspath('../../bin')
       new_path = install_path.replace(os.sep, '/')
       print ('install path found as '+ new_path)
@@ -11,4 +11,4 @@ if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform
       new_path = 'os.path.dirname(os.path.realpath(__file__))' # Conda layout on windows
    with fileinput.FileInput('opensim/__init__.py', inplace=True, backup='.bak') as file:
      for line in file:
-          print(line.replace('DLL_PATH', "'" + new_path + "'"), end='')
+          print(line.replace('DLL_PATH', new_path), end='')
