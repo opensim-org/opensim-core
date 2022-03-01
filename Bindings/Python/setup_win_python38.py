06f5a88003e96b773866ae8e271615190e7c914d
Ayman Habib <ahabib@stanford.edu>
2022-02-28 20:44:44 -0800

    undo changes for conda and apply them as patch

--- Bindings/Python/setup_win_python38.py
+++ Bindings/Python/setup_win_python38.py
@@ -3,12 +3,9 @@ import sys
 import os
 import fileinput
 if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-   if (len(sys.argv)==1):
-      install_path = os.path.abspath('../../bin')
-      new_path = install_path.replace(os.sep, '/')
-      print ('install path found as '+ new_path)
-   else:
-      new_path = os.path.dirname(os.path.realpath(__file__)) # Conda layout on windows
+   install_path = os.path.abspath('../../bin')
+   new_path = install_path.replace(os.sep, '/')
+   print ('install path found as '+ new_path)
    with fileinput.FileInput('opensim/__init__.py', inplace=True, backup='.bak') as file:
      for line in file:
-          print(line.replace('DLL_PATH', new_path), end='')
+          print(line.replace('DLL_PATH', "'" + new_path + "'"), end='')
