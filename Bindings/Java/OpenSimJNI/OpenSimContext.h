a5010ab3035badd23373e3cc0dbd05df1c656fa7
Travis CI <travis@travis-ci.org>
2022-03-19 13:47:44 +0100

    Updated Bindings (previously transitively imported PathPoint)

--- Bindings/Java/OpenSimJNI/OpenSimContext.h
+++ Bindings/Java/OpenSimJNI/OpenSimContext.h
@@ -26,8 +26,9 @@
 #include <OpenSim/Common/Object.h>
 #include <OpenSim/Common/PropertyTransform.h>
 #include <OpenSim/Simulation/osimSimulationDLL.h>
-#include <OpenSim/Simulation/Model/Model.h>
 #include <OpenSim/Simulation/Model/Force.h>
+#include <OpenSim/Simulation/Model/Model.h>
+#include <OpenSim/Simulation/Model/PathPoint.h>
 #include <OpenSim/Common/Array.h>
 #include <OpenSim/Tools/InverseKinematicsTool.h>
 #include <Bindings/PropertyHelper.h>
