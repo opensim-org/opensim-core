1ffdda4a3061c2f16b0d38e6917a0a6d6b3c6f50
aymanhab <ahabib@stanford.edu>
2022-09-08 15:34:29 -0700

    Address feedback on PR, clarify comments and some cleanup

--- OpenSim/Tests/Wrapping/testWrappingAlgorithm.cpp
+++ OpenSim/Tests/Wrapping/testWrappingAlgorithm.cpp
@@ -33,7 +33,7 @@ using namespace SimTK;
 using namespace std;
 
 void testSingleWrapObjectPerpendicular(OpenSim::WrapObject* wObj);
-void testCompareWrapObjects(OpenSim::WrapObject* wObj1, OpenSim::WrapObject* wObj2);
+void testCompareWrapObjects(OpenSim::WrapCylinder* wObj1, OpenSim::WrapObject* wObj2);
 
 int main()
 {
@@ -88,7 +88,8 @@ int main()
         woOne->set_length(2);
         auto* woTwo = new WrapEllipsoid();
         woTwo->setName("pulley2");
-        // -30 - 30 degrees guarantee no edge which is poorly handled and need to be dropped
+        // -30 - 30 degrees guarantees that wrapping doesn't occur at the cap of the cylinder which is poorly 
+        // handled and may need to be dropped as non-physical scenario
         for (int i = -2; i <= 2; i++) {
             double angle = i * SimTK::Pi / 12;
             woOne->set_xyz_body_rotation(Vec3(0,  angle, 0));
@@ -109,7 +110,8 @@ int main()
     cout << "Done" << endl;
     return 0;
 }
-// Test results of wrapping a sigle path perpendicular to a wrapObject 
+// Test results of wrapping a sigle path perpendicular to a wrapObject
+// particularly perpendicular to cylinder axis 
 // and compare results to analytical/expected answers
 void testSingleWrapObjectPerpendicular(WrapObject* wrapObject)
 {
@@ -170,14 +172,14 @@ void testSingleWrapObjectPerpendicular(WrapObject* wrapObject)
 // Test results of wrapping a sigle path around a wrapObject wObj1
 // and compare results to analytically equivalent wrapObject wObj2
 // For example a rotated cylinder against an ellipsoid
-void testCompareWrapObjects(OpenSim::WrapObject* wObj1, OpenSim::WrapObject* wObj2) {
+void testCompareWrapObjects(OpenSim::WrapCylinder* wObj1, OpenSim::WrapObject* wObj2) {
     auto visualize = false;
-    const double r = 0.5;
+    const double r = wObj1->get_radius();
     Model model;
     model.setName("test" + wObj1->getConcreteClassName()+wObj2->getConcreteClassName());
 
     auto& ground = model.updGround();
-    auto body = new OpenSim::Body("body", 1, Vec3(-.5, 0, 0), Inertia(0.1, 0.1, 0.01));
+    auto body = new OpenSim::Body("body", 1, Vec3(-r, 0, 0), Inertia(0.1, 0.1, 0.01));
     model.addComponent(body);
 
     auto joint = new PinJoint("pin", ground, *body);
@@ -225,14 +227,7 @@ void testCompareWrapObjects(OpenSim::WrapObject* wObj1, OpenSim::WrapObject* wOb
     SimTK::State& s = model.initSystem();
     auto& coord = joint->updCoordinate();
     const CoordinateSet& cset = model.getCoordinateSet();
-    // get angle
-    
-    double ang = wObj->get_xyz_body_rotation()[1];
-    double a = 0.5;
-    double b = .5 / cos(ang);
-    // perimeter approx fomrula by Ramanujan
-    //double p = SimTK::Pi * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
-    
+
     int nsteps = 1000;
     double max_diff = 0.;
     for (int i = 0; i <= nsteps; ++i) {
@@ -245,10 +240,7 @@ void testCompareWrapObjects(OpenSim::WrapObject* wObj1, OpenSim::WrapObject* wOb
 
         double len1 = spring1->getLength(s);
         double len2 = spring2->getLength(s);
-        max_diff = std::max(max_diff, std::abs(len1 - len2));
-        //std::cout << "i=" << i << " length=" << len1 << " " << len2 << " diff="  << std::abs(len1-len2) << std::endl;
         ASSERT_EQUAL<double>(len1, len2, .01);
     }
-    //std::cout << "max_diff:" << max_diff << std::endl;
 }
 
