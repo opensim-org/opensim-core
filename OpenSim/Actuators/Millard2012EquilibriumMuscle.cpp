opensim-org/opensim-core

commit 56bacfcafacbfd90ef107258136649de9de58438

removes userDefinedDynamicsExtra heap allocation

OpenSim/Actuators/Millard2012EquilibriumMuscle.cpp
@@ -1037,10 +1037,9 @@ calcMuscleDynamicsInfo(const SimTK::State& s, MuscleDynamicsInfo& mdi) const
         // Store quantities unique to this Muscle: the passive conservative
         // (elastic) fiber force and the passive non-conservative (damping)
         // fiber force.
-        SimTK::Vector dynExtras = SimTK::Vector(2);
-        dynExtras[0] = p1Fm; //elastic
-        dynExtras[1] = p2Fm; //damping
-        mdi.userDefinedDynamicsExtras = dynExtras;
+        mdi.userDefinedDynamicsExtras.resize(2);
+        mdi.userDefinedDynamicsExtras[0] = p1Fm; //elastic
+        mdi.userDefinedDynamicsExtras[0] = p2Fm; //damping
 
     } catch(const std::exception &x) {
         std::string msg = "Exception caught in Millard2012EquilibriumMuscle::"

commit 1ae55b0d936ee63f5c6adefb1e7f39c5631978d9

fixes incorrect indexing in userDefinedDynamicsExtras
Typo introduced in 56bacfcafacbfd90ef107258136649de9de58438

OpenSim/Actuators/Millard2012EquilibriumMuscle.cpp
@@ -1039,7 +1039,7 @@ calcMuscleDynamicsInfo(const SimTK::State& s, MuscleDynamicsInfo& mdi) const
         // fiber force.
         mdi.userDefinedDynamicsExtras.resize(2);
         mdi.userDefinedDynamicsExtras[0] = p1Fm; //elastic
-        mdi.userDefinedDynamicsExtras[0] = p2Fm; //damping
+        mdi.userDefinedDynamicsExtras[1] = p2Fm; //damping
 
     } catch(const std::exception &x) {
         std::string msg = "Exception caught in Millard2012EquilibriumMuscle::"