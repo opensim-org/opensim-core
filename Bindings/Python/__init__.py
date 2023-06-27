eaa9531e57f5705cbc457a498358e74111b92f15
aymanhab <ahabib@stanford.edu>
2023-06-27 15:53:10 -0700

    Remove setup_win_python38.py and use init.py from conda, update CMakeLists.txt accordingly. Could break windows python tests

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,10 +1,7 @@
 import sys
 import os
-if (sys.version_info.major == 3 and sys.version_info.minor >= 8 and sys.platform.startswith('win')):
-   if (os.path.exists(os.path.join(sys.prefix, 'conda-meta'))):
+if (sys.platform.startswith('win')):
       os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
-   else:
-      os.add_dll_directory(DLL_PATH)
 
 from .simbody import *
 from .common import *
