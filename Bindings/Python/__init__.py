7c98198b3ead99cf90951f08fb6b64a385d08b24
aymanhab <ahabib@stanford.edu>
2023-06-28 11:18:04 -0700

    Detect  if in dev environment and if so add path on windows

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,7 +1,13 @@
 import sys
 import os
 if (sys.platform.startswith('win')):
-      os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
+      curFolder = os.path.dirname(os.path.realpath(__file__))
+      # print("adding dll_directory "+os.path.dirname(os.path.realpath(__file__)))
+      os.add_dll_directory(curFolder)
+      # in dev environment, we also add "../../../../Release"
+      dev_path = os.path.join(curFolder, "../../../../Release")
+      if (os.path.exists(dev_path)):
+           os.add_dll_directory(dev_path)
 
 from .simbody import *
 from .common import *
