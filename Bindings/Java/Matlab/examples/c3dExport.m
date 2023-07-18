91c446dc95a4fce0512bb5ad84a6a45eb5a9ecaa
aymanhab <ahabib@stanford.edu>
2023-07-17 17:13:04 -0700

    Incorporate changes suggested by BJFregly to expose variables to choose COP expression and unit conversion

--- Bindings/Java/Matlab/examples/c3dExport.m
+++ Bindings/Java/Matlab/examples/c3dExport.m
@@ -1,3 +1,4 @@
+
 % ----------------------------------------------------------------------- %
 % The OpenSim API is a toolkit for musculoskeletal modeling and           %
 % simulation. See http://opensim.stanford.edu and the NOTICE file         %
@@ -5,7 +6,7 @@
 % and supported by the US National Institutes of Health (U54 GM072970,    %
 % R24 HD065690) and by DARPA through the Warrior Web program.             %
 %                                                                         %
-% Copyright (c) 2005-2018 Stanford University and the Authors             %
+% Copyright (c) 2005-2019 Stanford University and the Authors             %
 % Author(s): James Dunne                                                  %
 %                                                                         %
 % Licensed under the Apache License, Version 2.0 (the "License");         %
@@ -20,6 +21,22 @@
 % permissions and limitations under the License.                          %
 % ----------------------------------------------------------------------- %
 
+function c3dExtract
+
+% This function was originally developed by the Stanford OpenSim team (see
+% below for credits) but was modified by B.J. Fregly of the Rice
+% Computational Neuromechanics Lab at Rice University.
+% The modified version names output .trc and .mot files with the same
+% basename as the selcted input .c3d file.
+
+%% Define necessary inputs (could be passed into this function instead)
+% Specify point used for calculating output joint reaction moments
+% 0 = electrical center, 1 = COP
+outputMomentsPoint = 0;
+% Set flag indicating whether or not to convert length units from mm to m
+% 0 = no, 1 = yes
+convertLengthUnits = 1;
+
 %% Example of using the Matlab-OpenSim class 
 
 %% Load OpenSim libs
@@ -31,8 +48,8 @@ c3dpath = fullfile(path,filename);
 
 %% Construct an opensimC3D object with input c3d path
 % Constructor takes full path to c3d file and an integer for forceplate
-% representation (1 = COP). 
-c3d = osimC3D(c3dpath,1);
+% representation in output forces file (0 = electrical center, 1 = COP). 
+c3d = osimC3D(c3dpath,outputMomentsPoint);
 
 %% Get some stats...
 % Get the number of marker trajectories
@@ -59,23 +76,32 @@ forceTable = c3d.getTable_forces();
 [markerStruct forceStruct] = c3d.getAsStructs();
 
 %% Convert COP (mm to m) and Moments (Nmm to Nm)
-c3d.convertMillimeters2Meters();
+if convertLengthUnits
+    c3d.convertMillimeters2Meters();
+end
 
 %% Write the marker and force data to file
+% Define output file names
+basename = strtok(filename,'.');
+markersFilename = strcat(basename,'_markers.trc');
+
+switch outputMomentsPoint
+    case 0
+        forcesFilename = strcat(basename,'_forces_EC.mot');
+    case 1
+        forcesFilename = strcat(basename,'_forces_COP.mot');
+end
 
 % Write marker data to trc file.
 % c3d.writeTRC()                       Write to dir of input c3d.
 % c3d.writeTRC('Walking.trc')          Write to dir of input c3d with defined file name.
 % c3d.writeTRC('C:/data/Walking.trc')  Write to defined path input path.
-c3d.writeTRC('test_data_markers.trc');
+c3d.writeTRC(markersFilename);
 
 % Write force data to mot file.
 % c3d.writeMOT()                       Write to dir of input c3d.
 % c3d.writeMOT('Walking.mot')          Write to dir of input c3d with defined file name.
 % c3d.writeMOT('C:/data/Walking.mot')  Write to defined path input path.
-% 
-% This function assumes point and torque data are in mm and Nmm and
-% converts them to m and Nm. If your C3D is already in M and Nm,
-% comment out the internal function convertMillimeters2Meters()
-c3d.writeMOT('test_data_forces.mot');
+c3d.writeMOT(forcesFilename);
 
+end
