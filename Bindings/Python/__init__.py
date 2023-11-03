252b45377d9086e9c33a41ffb3662687250d2a5b
Mohammadreza Rezaie <mrrezaie@outlook.com>
2023-11-04 00:41:19 +0330

    `sys.exec_prefix` instead of `os.path.dirname(sys.executable)`

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -3,7 +3,7 @@ import os
 if (sys.platform.startswith('win')):
     curFolder = os.path.dirname(os.path.realpath(__file__))
     os.add_dll_directory(curFolder)
-    opensimCMD = os.path.join(os.path.dirname(sys.executable), 'Scripts', 'opensim-cmd.exe')
+    opensimCMD = os.path.join(sys.exec_prefix, 'Scripts', 'opensim-cmd.exe')
     if os.path.isfile(opensimCMD):
         os.add_dll_directory(os.path.dirname(opensimCMD))
 
