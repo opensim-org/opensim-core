a71bea5bf3c8b31d3a7a6dd5522686e631b9e2fd
Clay Anderson <fcanderson@users.noreply.github.com>
2024-06-24 11:01:00 -0500

    Removed trailing whitespace in PiecewiseLinearFunction.cpp

--- OpenSim/Common/PiecewiseLinearFunction.cpp
+++ OpenSim/Common/PiecewiseLinearFunction.cpp
@@ -313,7 +313,7 @@ void PiecewiseLinearFunction::updateFromXMLNode(SimTK::Xml::Element& aNode, int 
 {
     Function::updateFromXMLNode(aNode, versionNumber);
     calcCoefficients();
-}   
+}
 
 double PiecewiseLinearFunction::getX(int aIndex) const
 {
