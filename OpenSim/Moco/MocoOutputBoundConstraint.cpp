bd10ce4d7ed658f5cf2ee50f640174c028fa25e8
Allison <allisj@stanford.edu>
2024-07-25 10:59:23 -0700

    .h file

--- OpenSim/Moco/MocoOutputBoundConstraint.cpp
+++ OpenSim/Moco/MocoOutputBoundConstraint.cpp
@@ -3,3 +3,135 @@
 //
 
 #include "MocoOutputBoundConstraint.h"
+#include "MocoProblemInfo.h"
+#include <OpenSim/Common/GCVSpline.h>
+#include <OpenSim/Simulation/SimulationUtilities.h>
+
+using namespace OpenSim;
+
+void MocoOutputBoundConstraint::constructProperties() {
+    constructProperty_output_path("");
+    constructProperty_second_output_path("");
+    constructProperty_operation("");
+    constructProperty_exponent(1);
+    constructProperty_output_index(-1);
+    constructProperty_lower_bound();
+    constructProperty_upper_bound();
+    constructProperty_equality_with_lower(false);
+}
+
+void MocoOutputBoundConstraint::initializeOnModelImpl(
+        const Model& model, const MocoProblemInfo& problemInfo) const {
+    OPENSIM_THROW_IF_FRMOBJ(get_output_path().empty(), Exception,
+            "No output_path provided.");
+    std::string componentPath, outputName, channelName, alias;
+    AbstractInput::parseConnecteePath(
+            get_output_path(), componentPath, outputName, channelName, alias);
+    const auto& component = getModel().getComponent(componentPath);
+    const auto* abstractOutput = &component.getOutput(outputName);
+
+    OPENSIM_THROW_IF_FRMOBJ(get_output_index() < -1, Exception,
+            "Invalid Output index provided.");
+    m_boundVectorNorm = (get_output_index() == -1);
+
+    if (dynamic_cast<const Output<double>*>(abstractOutput)) {
+        m_data_type = Type_double;
+        OPENSIM_THROW_IF_FRMOBJ(get_output_index() != -1, Exception,
+                "An Output index was provided, but the Output is of type 'double'.")
+
+    } else if (dynamic_cast<const Output<SimTK::Vec3>*>(abstractOutput)) {
+        m_data_type = Type_Vec3;
+        OPENSIM_THROW_IF_FRMOBJ(get_output_index() > 2, Exception,
+                "The Output is of type 'SimTK::Vec3', but an Output index greater "
+                "than 2 was provided.");
+        m_index1 = get_output_index();
+
+    } else if (dynamic_cast<const Output<SimTK::SpatialVec>*>(abstractOutput)) {
+        m_data_type = Type_SpatialVec;
+        OPENSIM_THROW_IF_FRMOBJ(get_output_index() > 5, Exception,
+                "The Output is of type 'SimTK::SpatialVec', but an Output index "
+                "greater than 5 was provided.");
+        if (get_output_index() < 3) {
+            m_index1 = 0;
+            m_index2 = get_output_index();
+        } else {
+            m_index1 = 1;
+            m_index2 = get_output_index() - 3;
+        }
+
+    } else {
+        OPENSIM_THROW_FRMOBJ(Exception,
+                "Data type of specified model output not supported.");
+    }
+    m_output.reset(abstractOutput);
+
+    OPENSIM_THROW_IF_FRMOBJ(get_exponent() < 1, Exception,
+            "Exponent must be 1 or greater.");
+    int exponent = get_exponent();
+
+    // The pow() function gives slightly different results than x * x. On Mac,
+    // using x * x requires fewer solver iterations.
+    if (exponent == 1) {
+        m_power_function = [](const double& x) { return x; };
+    } else if (exponent == 2) {
+        m_power_function = [](const double& x) { return x * x; };
+    } else {
+        m_power_function = [exponent](const double& x) {
+            return pow(std::abs(x), exponent);
+        };
+    }
+
+    // Set the "depends-on stage", the SimTK::Stage we must realize to
+    // in order to calculate values from this output.
+    m_dependsOnStage = m_output->getDependsOnStage();
+
+    // initialize bounds
+    m_hasLower = !getProperty_lower_bound().empty();
+    m_hasUpper = !getProperty_upper_bound().empty();
+    if (!m_hasLower && !m_hasUpper) {
+        log_warn("In MocoOutputBoundConstraint '{}', output path(s) are "
+                 "specified but no bounds are provided.", getName());
+    }
+    OPENSIM_THROW_IF_FRMOBJ(get_equality_with_lower() && m_hasUpper, Exception,
+        "If equality_with_lower==true, upper bound function must not be "
+        "set.");
+    OPENSIM_THROW_IF_FRMOBJ(get_equality_with_lower() && !m_hasLower, Exception,
+            "If equality_with_lower==true, lower bound function must be set.");
+
+    auto checkTimeRange = [&](const Function& f) {
+        if (auto* spline = dynamic_cast<const GCVSpline*>(&f)) {
+            OPENSIM_THROW_IF_FRMOBJ(
+                    spline->getMinX() > problemInfo.minInitialTime, Exception,
+                    "The function's minimum domain value ({}) must "
+                    "be less than or equal to the minimum possible "
+                    "initial time ({}).",
+                    spline->getMinX(), problemInfo.minInitialTime);
+            OPENSIM_THROW_IF_FRMOBJ(
+                    spline->getMaxX() < problemInfo.maxFinalTime, Exception,
+                    "The function's maximum domain value ({}) must "
+                    "be greater than or equal to the maximum possible "
+                    "final time ({}).",
+                    spline->getMaxX(), problemInfo.maxFinalTime);
+        }
+    };
+    if (m_hasLower) checkTimeRange(get_lower_bound());
+    if (m_hasUpper) checkTimeRange(get_upper_bound());
+
+    if (get_equality_with_lower()) {
+        setNumEquations(1);
+    } else {
+        setNumEquations((int)m_hasLower + (int)m_hasUpper);
+    }
+
+    m_useCompositeOutputValue = false;
+    // if there's a second output, initialize it
+    if (get_second_output_path() != "") {
+        m_useCompositeOutputValue = true;
+        initializeComposite();
+    } else if (get_operation() != "") {
+        OPENSIM_THROW_FRMOBJ(Exception, fmt::format("An operation was provided "
+                "but a second Output path was not provided. Either provide no "
+                "operation with a single Output, or provide a value to both "
+                "setOperation() and setSecondOutputPath()."));
+    }
+}
--- OpenSim/Moco/MocoOutputBoundConstraint.h
+++ OpenSim/Moco/MocoOutputBoundConstraint.h
@@ -1,38 +1,37 @@
+#ifndef OPENSIM_MOCOOUTPUTBOUNDCONSTRAINT_H
+#define OPENSIM_MOCOOUTPUTBOUNDCONSTRAINT_H
 //
 // Created by Allison John on 7/24/24.
-// Make an output bound constraint that can bound one or two outputs between two
-// functions
+// Make an output bound constraint that can bound one or an arithmetic
+// combination of two outputs between two functions
 //
-
-#ifndef MOCOOUTPUTBOUNDCONSTRAINT_H
-#define MOCOOUTPUTBOUNDCONSTRAINT_H
 
 #include "MocoConstraint.h"
 
 namespace OpenSim {
 
 class MocoOutputBoundConstraint : MocoPathConstraint {
-    OpenSim_DECLARE_CONCRETE_OBJECT(MocoOutputBoundConstraint, MocoPathConstraint);
+    OpenSim_DECLARE_CONCRETE_OBJECT(
+            MocoOutputBoundConstraint, MocoPathConstraint);
 
 public:
     MocoOutputBoundConstraint() { constructProperties(); }
 
     /** Set the absolute path to the output in the model to be used in this path
     constraint. The format is "/path/to/component|output_name". */
-    void addOutputPath(std::string path) {
-        append_control_paths(std::move(path));
+    void setOutputPath(std::string path) { set_output_path(std::move(path)); }
+    const std::string& getOutputPath() const { return get_output_path(); }
+
+    /** Set the absolute path to the optional second Output in the model to be
+    used in this path constraint. The format is "/path/to/component|output_name".
+    This Output should have the same  type as the first Output. If providing a
+    second Output, the user must also provide an operation via `setOperation()`.
+    */
+    void setSecondOutputPath(std::string path) {
+        set_second_output_path(std::move(path));
     }
-    void setControlPaths(const std::vector<std::string>& controlPaths) {
-        updProperty_control_paths().clear();
-        for (const auto& path : controlPaths) { append_control_paths(path); }
-    }
-    void clearControlPaths() { updProperty_control_paths().clear(); }
-    std::vector<std::string> getControlPaths() const {
-        std::vector<std::string> paths;
-        for (int i = 0; i < getProperty_control_paths().size(); ++i) {
-            paths.push_back(get_control_paths(i));
-        }
-        return paths;
+    const std::string& getSecondOutputPath() const {
+        return get_second_output_path();
     }
 
     /** Set the exponent applied to the output value in the constraint. This
@@ -40,8 +39,8 @@ public:
     void setExponent(int exponent) { set_exponent(exponent); }
     int getExponent() const { return get_exponent(); }
 
-    /** Set the index to the value to be constrained when a vector type Output is
-    specified. For SpatialVec Outputs, indices 0, 1, and 2 refer to the
+    /** Set the index to the value to be constrained when a vector type Output
+    is specified. For SpatialVec Outputs, indices 0, 1, and 2 refer to the
     rotational components and indices 3, 4, and 5 refer to the translational
     components. A value of -1 indicates to constrain the vector norm (which is
     the default setting). If an index for a type double Output is provided, an
@@ -49,12 +48,167 @@ public:
     void setOutputIndex(int index) { set_output_index(index); }
     int getOutputIndex() const { return get_output_index(); }
 
+
+    void setLowerBound(const Function& f) { set_lower_bound(f); }
+    void clearLowerBound() { updProperty_lower_bound().clear(); }
+    bool hasLowerBound() const { return !getProperty_lower_bound().empty(); }
+    const Function& getLowerBound() const { return get_lower_bound(); }
+
+    void setUpperBound(const Function& f) { set_upper_bound(f); }
+    void clearUpperBound() { updProperty_upper_bound().clear(); }
+    bool hasUpperBound() const { return !getProperty_upper_bound().empty(); }
+    const Function& getUpperBound() const { return get_upper_bound(); }
+
+    /// Should the control be constrained to be equal to the lower bound (rather
+    /// than an inequality constraint)? In this case, the upper bound must be
+    /// unspecified.
+    void setEqualityWithLower(bool v) { set_equality_with_lower(v); }
+    //// @copydoc setEqualityWithLower()
+    bool getEqualityWithLower() const { return get_equality_with_lower(); }
+
+protected:
+    void initializeOnModelImpl(
+            const Model& model, const MocoProblemInfo&) const override;
+
+    void calcPathConstraintErrorsImpl(
+            const SimTK::State& state, SimTK::Vector& errors) const override;
+    // these could be private
+    /** Calculate the Output value for the provided SimTK::State. Do not
+    call this function until 'initializeOnModelBase()' has been called. */
+    double calcOutputValue(const SimTK::State&) const;
+
+    /** Raise a value to the exponent set via 'setExponent()'. Do not call this
+    function until 'initializeOnModelBase()' has been called. */
+    double setValueToExponent(double value) const {
+        return m_power_function(value);
+    }
+
+    /** Get the "depends-on stage", or the SimTK::Stage we need to realize the
+    system to in order to calculate the Output value. */
+    const SimTK::Stage& getDependsOnStage() const {
+        return m_dependsOnStage;
+    }
+
+private:
+    OpenSim_DECLARE_PROPERTY(output_path, std::string,
+            "The absolute path to the Output in the model to be used in this "
+            "path constraint (i.e., '/path/to/component|output_name').");
+    OpenSim_DECLARE_PROPERTY(second_output_path, std::string,
+            "The absolute path to the optional second Output in the model to be used"
+            " in this path constraint (i.e., '/path/to/component|output_name').");
+    OpenSim_DECLARE_PROPERTY(operation, std::string, "The operation to combine "
+            "the two outputs: 'addition', 'subtraction', 'multiplication', or "
+            "'divison'.");
+    OpenSim_DECLARE_PROPERTY(exponent, int,
+            "The exponent applied to the output value in the constraint "
+            "(default: 1).");
+    OpenSim_DECLARE_PROPERTY(output_index, int,
+            "The index to the value to be constrained when a vector type "
+            "Output is specified. For SpatialVec Outputs, indices 0, 1, "
+            "and 2 refer to the rotational components and indices 3, 4, "
+            "and 5 refer to the translational components. A value of -1 "
+            "indicates to constrain the vector norm (default: -1).");
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(
+        lower_bound, Function, "Lower bound as a function of time.");
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(
+            upper_bound, Function, "Upper bound as a function of time.");
+    OpenSim_DECLARE_PROPERTY(equality_with_lower, bool,
+            "The control must be equal to the lower bound; "
+            "upper must be unspecified (default: false).");
+
+    void constructProperties();
+
+    /** Initialize additional information when there are two Outputs:
+    the second Output, the operation, and the dependsOnStage. */
+    void initializeComposite() const;
+
+    /** Calculate the Output value of one Output. */
+    double calcSingleOutputValue(const SimTK::State&) const;
+    /** Calculate the two Output values and apply the operation. */
+    double calcCompositeOutputValue(const SimTK::State&) const;
+
+    /** Get a reference to the Output for this path constraint. */
+    template <typename T>
+    const Output<T>& getOutput() const {
+        return static_cast<const Output<T>&>(m_output.getRef());
+    }
+    /** Get a reference to the second Output for this path constraint. */
+    template <typename T>
+    const Output<T>& getSecondOutput() const {
+        return static_cast<const Output<T>&>(m_second_output.getRef());
+    }
+
+    /** Apply the operation to two double values. */
+    double applyOperation(double value1, double value2) const {
+        switch (m_operation) {
+        case Addition       : return value1 + value2;
+        case Subtraction    : return value1 - value2;
+        case Multiplication : return value1 * value2;
+        case Division       : return value1 / value2;
+        default             : OPENSIM_THROW_FRMOBJ(Exception,
+                                "Internal error: invalid operation type for "
+                                "double type Outputs.");
+        }
+    }
+    /** Apply the elementwise operation to two SimTK::Vec3 values. */
+    double applyOperation(const SimTK::Vec3& value1,
+                          const SimTK::Vec3& value2) const {
+        switch (m_operation) {
+        case Addition       : return (value1 + value2).norm();
+        case Subtraction    : return (value1 - value2).norm();
+        case Multiplication : return value1.elementwiseMultiply(value2).norm();
+        case Division       : return value1.elementwiseDivide(value2).norm();
+        default             : OPENSIM_THROW_FRMOBJ(Exception,
+                                "Internal error: invalid operation type for "
+                                "SimTK::Vec3 type Outputs.");
+        }
+    }
+    /** Apply the elementwise operation to two SimTK::SpatialVec values.
+    Multiplication and divison operations are not supported for SpatialVec
+    Outputs without an index. */
+    double applyOperation(const SimTK::SpatialVec& value1,
+                          const SimTK::SpatialVec& value2) const {
+        switch (m_operation) {
+        case Addition       : return (value1 + value2).norm();
+        case Subtraction    : return (value1 - value2).norm();
+        default             : OPENSIM_THROW_FRMOBJ(Exception,
+                                "Internal error: invalid operation type for "
+                                "SimTK::SpatialVec type Outputs.");
+        }
+    }
+
+    enum DataType {
+        Type_double,
+        Type_Vec3,
+        Type_SpatialVec,
+    };
+    /** Get the string of the data type, for prints and error messages. */
+    std::string getDataTypeString(DataType type) const {
+        switch (type) {
+        case Type_double     : return "double";
+        case Type_Vec3       : return "SimTK::Vec3";
+        case Type_SpatialVec : return "SimTK::SpatialVec";
+        default              : return "empty Datatype";
+        }
+    }
+
+    mutable DataType m_data_type;
+    mutable SimTK::ReferencePtr<const AbstractOutput> m_output;
+    mutable SimTK::ReferencePtr<const AbstractOutput> m_second_output;
+    enum OperationType { Addition, Subtraction, Multiplication, Division };
+    mutable OperationType m_operation;
+    mutable bool m_useCompositeOutputValue;
+    mutable std::function<double(const double&)> m_power_function;
+    mutable int m_index1;
+    mutable int m_index2;
+    mutable bool m_boundVectorNorm;
+    mutable SimTK::Stage m_dependsOnStage = SimTK::Stage::Acceleration;
+    mutable bool m_hasLower;
+    mutable bool m_hasUpper;
+
 };
 
 } // namespace OpenSim
 
-#endif //MOCOOUTPUTBOUNDCONSTRAINT_H
+#endif // OPENSIM_MOCOOUTPUTBOUNDCONSTRAINT_H
 
-
-two outputs?
-    index for each output?
