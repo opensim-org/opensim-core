e9803f9e6eaf2bd5d0d4c74126fc9c64e7f9453a
Allison <allisj@stanford.edu>
2024-08-01 14:47:54 -0700

    made class

---
+++ OpenSim/Moco/MocoGoal/MocoParameterExpressionGoal.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Allison John on 8/1/24.
+//
+
+#include "MocoParameterExpressionGoal.h"
+#include <lepton/Parser.h>
+#include <lepton/ParsedExpression.h>
+
+using namespace OpenSim;
+
+void MocoParameterExpressionGoal::constructProperties() {
+    constructProperty_expression("");
+    constructProperty_parameters();
+}
+
+void MocoParameterExpressionGoal::initializeOnModelImpl(const Model&) const {
+    _parameterProg = Lepton::Parser::parse(get_expression()).optimize().createProgram();
+}
+
+void MocoParameterExpressionGoal::calcIntegrandImpl(
+        const IntegrandInput& input, double& integrand) const {
+
+    std::map<std::string, double> parameterVars;
+    for (int i=0; i<getProperty_variable_names().size(); ++i) {
+        std::string variableName = getProperty_variable_names()[i];
+        double parameterValue = getProperty_parameters()[i].getValue();
+        parameterVars[variableName] = parameterValue;
+    }
+    integrand = _parameterProg.evaluate(parameterVars);
+}
+
+void MocoParameterExpressionGoal::calcGoalImpl(
+        const MocoGoal::GoalInput& input, SimTK::Vector& values) const {
+    values[0] = input.integral;
+}
+
---
+++ OpenSim/Moco/MocoGoal/MocoParameterExpressionGoal.h
@@ -0,0 +1,58 @@
+//
+// Created by Allison John on 8/1/24.
+//
+
+#ifndef OPENSIM_MOCOPARAMETEREXPRESSIONGOAL_H
+#define OPENSIM_MOCOPARAMETEREXPRESSIONGOAL_H
+
+#include "MocoGoal.h"
+#include <lepton/ExpressionProgram.h>
+
+namespace OpenSim {
+
+class OSIMMOCO_API MocoParameterExpressionGoal : public MocoGoal {
+    OpenSim_DECLARE_ABSTRACT_OBJECT(MocoParameterExpressionGoal, MocoGoal);
+
+public:
+    MocoParameterExpressionGoal() { constructProperties(); }
+    MocoParameterExpressionGoal(std::string name) : MocoGoal(std::move(name)) {
+        constructProperties();
+    }
+    MocoParameterExpressionGoal(std::string name, double weight)
+            : MocoGoal(std::move(name), weight) {
+        constructProperties();
+    }
+
+    void setExpression(std::string& expression) {
+        set_expression(expression);
+    }
+
+    void addParameter(MocoParameter& parameter, std::string variableName) {
+        updProperty_parameters().appendValue(parameter);
+        updProperty_variable_names().appendValue(variableName);
+    }
+
+protected:
+    void initializeOnModelImpl(const Model&) const override;
+    void calcIntegrandImpl(
+            const IntegrandInput& input, SimTK::Real& integrand) const override;
+    void calcGoalImpl(
+            const GoalInput& input, SimTK::Vector& cost) const override;
+    //void printDescriptionImpl() const override;
+
+private:
+    void constructProperties();
+    OpenSim_DECLARE_PROPERTY(expression, std::string,
+            "The expression string with variables q0-q9.");
+    // consider a mapping from variable names to parameters instead
+    OpenSim_DECLARE_LIST_PROPERTY(parameters, MocoParameter,
+            "MocoParameters to use in the expression.");
+    OpenSim_DECLARE_LIST_PROPERTY(variable_names, std::string,
+            "Variable names of the MocoParameters to use in the expression.");
+
+    mutable Lepton::ExpressionProgram _parameterProg;
+};
+
+} // namespace OpenSim
+
+#endif // OPENSIM_MOCOPARAMETEREXPRESSIONGOAL_H
