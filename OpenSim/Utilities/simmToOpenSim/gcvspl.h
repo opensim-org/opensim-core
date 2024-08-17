aa1a3abe8f8179338bfedb9896f6cbe453e866f9
Ayman Habib <ahabib@stanford.edu>
2024-08-16 22:07:56 -0700

    Remove stale simmToOpenSim folder and convertFiles folder that has stale dependency on retired xerces (#3879)

--- OpenSim/Utilities/simmToOpenSim/gcvspl.h
+++
@@ -1,7 +0,0 @@
-/*
-    function prototypes
-*/
-int gcvspl(double *, double *, int *, double *, double *, int *, int *,
-              int *, int *, double *, double *, int *, double *, int *);
-
-double splder(int *, int *, int *, double *, double *, double *, int *, double *);
