086736b1d43385f03e7d258347df1def8eafd070
nickbianco <nbianco@stanford.edu>
2025-03-19 10:26:48 -0700

    Rename to ExpressionBasedPathForce and add testing

--- OpenSim/Simulation/Model/ExpressionBasedPathForce.h
+++ OpenSim/Simulation/Model/ExpressionBasedPathForce.h
@@ -1,7 +1,7 @@
-#ifndef OPENSIM_EXPRESSION_BASED_PATH_SPRING_H_
-#define OPENSIM_EXPRESSION_BASED_PATH_SPRING_H_
+#ifndef OPENSIM_EXPRESSION_BASED_PATH_FORCE_H_
+#define OPENSIM_EXPRESSION_BASED_PATH_FORCE_H_
 /* -------------------------------------------------------------------------- *
- *                   OpenSim:  ExpressionBasedPathSpring.h                    *
+ *                   OpenSim:  ExpressionBasedPathForce.h                     *
  * -------------------------------------------------------------------------- *
  * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
  * See http://opensim.stanford.edu and the NOTICE file for more information.  *
@@ -27,40 +27,52 @@
 #include <OpenSim/Simulation/Model/ForceProducer.h>
 #include <OpenSim/Simulation/Model/GeometryPath.h>
 
+#include <lepton/ExpressionProgram.h>
 namespace OpenSim {
 
 class ScaleSet;
 
 /**
- * A massless, spring-based path Force whose force magnitude is determined by a 
+ * A massless, path-based Force whose force magnitude is determined by a 
  * user-defined expression, with the path stretch (`s = l-l0`) and the path 
  * speed (`ldot`) as variables, where `l` is the path length and `l0` is the 
- * rest length. The path of the ExpressionBasedPathSpring is determined by an 
+ * rest length. The path of the ExpressionBasedPathForce is determined by an 
  * object derived from AbstractGeometryPath. 
  *
  * "s" and "ldot" are the variables names expected by the expression parser.
  * Common C math library functions such as: exp(), pow(), sqrt(), sin(), ...
  * are permitted. See Lepton/Operation.h for a complete list.
  *
+ * By default, the ExpressionBasedPathForce will not apply tension when the 
+ * path stretch is negative (i.e., when the path is shorter than its resting 
+ * length). This behavior can be modified by setting the `clamp_stretch`
+ * property via `setClampStretch()`. Clamping the path stretch creates a 
+ * discontinuity in the force profile, which may not be desired during gradient
+ * based optimization. 
+ *
  * For example: string expression = "-1.5*exp(10*(d-0.25)^2)*(1 + 2.0*ddot)"
- *              provides a model of a path spring with non-linear stiffness 
+ *              provides a model of a path force with non-linear stiffness 
  *              and damping.
  */
-class OSIMSIMULATION_API ExpressionBasedPathSpring : public ForceProducer {
-OpenSim_DECLARE_CONCRETE_OBJECT(ExpressionBasedPathSpring, ForceProducer);
+class OSIMSIMULATION_API ExpressionBasedPathForce : public ForceProducer {
+OpenSim_DECLARE_CONCRETE_OBJECT(ExpressionBasedPathForce, ForceProducer);
 public:
 //=============================================================================
 // PROPERTIES
 //=============================================================================
     OpenSim_DECLARE_PROPERTY(resting_length, double,
-        "The resting length (m) of the ExpressionBasedPathSpring. Default: 0.");
+        "The resting length (m) of the ExpressionBasedPathForce. Default: 0.");
     OpenSim_DECLARE_PROPERTY(expression, std::string,
-        "Expression of the path spring magnitude as a function of "
+        "Expression of the path force magnitude as a function of "
         "the path stretch (s) and the path speed (ldot). "
         "Note, expression cannot have any whitespace separating characters.");
     OpenSim_DECLARE_PROPERTY(path, AbstractGeometryPath,
         "The path defines the length and lengthening speed of the "
-        "ExpressionBasedPathSpring");
+        "ExpressionBasedPathForce");
+    OpenSim_DECLARE_PROPERTY(clamp_stretch, bool,
+        "Clamp the stretch to be non-negative. Default: true. "
+        "If true, the force will not apply tension when the path is "
+        "shorter than its resting length.");
 
 //=============================================================================
 // OUTPUTS
@@ -79,18 +91,26 @@ public:
 //==============================================================================
     
     /** Default constructor. */
-    ExpressionBasedPathSpring();
+    ExpressionBasedPathForce();
 
     /** Convenience constructor.
-     * @param name          name of this %ExpressionBasedPathSpring Object
-     * @param restLength    the spring's resting length
-     * @param expression    the expression used to compute spring tension */
-     ExpressionBasedPathSpring(const std::string& name, 
-        double restLength, double stiffness, double dissipation);
+     * @param name          name of this %ExpressionBasedPathForce Object
+     * @param restLength    the force's resting length
+     * @param expression    the expression used to compute path tension */
+    ExpressionBasedPathForce(const std::string& name, 
+            double restLength, const std::string& expression);
 
+    /** Convenience constructor.
+     * @param name          name of this %ExpressionBasedPathForce Object
+     * @param restLength    the force's resting length
+     * @param expression    the expression used to compute force tension 
+     * @param clampStretch  if true, the force will not apply tension when the
+     *                      path is shorter than its resting length */
+    ExpressionBasedPathForce(const std::string& name, 
+            double restLength, const std::string& expression, bool clampStretch);
 
     /** 
-     * The resting length of the path spring. The default value is 0. 
+     * The resting length of the path force. The default value is 0. 
      */
     double getRestingLength() const {   
         return get_resting_length();
@@ -98,6 +118,31 @@ public:
     /// @copydoc getRestingLength()
     void setRestingLength(double restingLength) {
         set_resting_length(restingLength);
+    }
+
+    /** 
+     * The expression that defines the path force tension. 
+     * @note The expression must not contain any whitespace separating 
+     *       characters.
+     */
+    void setExpression(const std::string& expression) {
+        set_expression(expression);
+    }
+    /// @copydoc setExpression()
+    const std::string& getExpression() const {
+        return get_expression();
+    }
+    
+    /** 
+     * If true, the force will not apply tension when the path is shorter than 
+     * its resting length. Default: true.
+     */
+    bool getClampStretch() const {
+        return get_clamp_stretch();
+    }
+    /// @copydoc getClampStretch()
+    void setClampStretch(bool clampStretch) {
+        set_clamp_stretch(clampStretch);
     }
 
     /** 
@@ -134,31 +179,30 @@ public:
     bool hasVisualPath() const override { return getPath().isVisualPath(); };
 
     /** 
-     * Get the length of the ExpressionBasedPathSpring. 
+     * Get the length of the ExpressionBasedPathForce. 
      * 
      * @note Accessible at SimTK::Stage::Position. 
      */
     double getLength(const SimTK::State& s) const;
 
     /** 
-     * Get the stretch in the ExpressionBasedPathSpring. 
+     * Get the stretch in the ExpressionBasedPathForce. 
      * 
      * @note Accessible at SimTK::Stage::Position. 
      */
     double getStretch(const SimTK::State& s) const;
 
     /** 
-     * Get the lengthening speed of the ExpressionBasedPathSpring. 
+     * Get the lengthening speed of the ExpressionBasedPathForce. 
      * 
      * @note Accessible at SimTK::Stage::Velocity. 
      */
     double getLengtheningSpeed(const SimTK::State& s) const;
 
     /** 
-     * Get the tension generated by the ExpressionBasedPathSpring. 
+     * Get the tension generated by the ExpressionBasedPathForce. 
      * 
-     * @note The value of the tension can only be obtained after the system has
-     * been realized to SimTK::Stage::Dynamics. 
+     * @note Accessible at SimTK::Stage::Dynamics. 
      */
     double getTension(const SimTK::State& s) const;
 
@@ -169,8 +213,8 @@ public:
     double computeMomentArm(const SimTK::State& s, 
             const Coordinate& aCoord) const;
 
-=    // SCALE
-    /** Adjust the resting length of the path spring after the model has been
+    // SCALE
+    /** Adjust the resting length of the path force after the model has been
      * scaled. The `resting_length` property is multiplied by the quotient of
      * the current path length and the path length before scaling. 
      */
@@ -180,6 +224,7 @@ public:
 protected:
     // MODEL COMPONENT INTERFACE
     void extendFinalizeFromProperties() override;
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override;
 
     // FORCE INTERFACE
     OpenSim::Array<std::string> getRecordLabels() const override;
@@ -191,8 +236,11 @@ private:
     void implProduceForces(const SimTK::State&, ForceConsumer&) const override;
 
     void constructProperties();
+
+    Lepton::ExpressionProgram _tensionProg;
+    mutable CacheVariable<double> _tensionCV;
 };
 
 } // namespace OpenSim
 
-#endif // OPENSIM_EXPRESSION_BASED_PATH_SPRING_H_
+#endif // OPENSIM_EXPRESSION_BASED_PATH_FORCE_H_
