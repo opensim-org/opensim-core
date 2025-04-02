add3f70cdd13a7f0c387527d3ba5a2719ee7826b
Nicos <nicos.haralabidis@stanford.edu>
2025-04-02 11:25:46 -0700

    updated copyright year

--- OpenSim/Simulation/Model/MocoBushingForce.h
+++ OpenSim/Simulation/Model/MocoBushingForce.h
@@ -9,7 +9,7 @@
  * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
  * through the Warrior Web program.                                           *
  *                                                                            *
- * Copyright (c) 2005-2017 Stanford University and the Authors                *
+ * Copyright (c) 2025 Stanford University and the Authors                *
  * Author(s): Nicos Haralabidis and Jon Stingel adapted from Matt S. DeMers   *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
