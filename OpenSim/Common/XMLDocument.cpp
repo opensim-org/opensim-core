1a48e33893b14709ee8ff1fb76609c24b089f774
AlexBeattie42 <30098201+alexbeattie42@users.noreply.github.com>
2025-06-18 09:27:51 +0300

    Add scoped using namespace declarations for certain SimTK types.

--- OpenSim/Common/XMLDocument.cpp
+++ OpenSim/Common/XMLDocument.cpp
@@ -537,34 +537,35 @@ string XMLDocument::updateConnecteePath30517(
 
 SimTK::Xml::Element XMLDocument::findElementWithName(
         SimTK::Xml::Element& element, const std::string& name) {
+    using namespace SimTK::Xml;
 
-    if (name.empty()) return SimTK::Xml::Element();
+    if (name.empty()) return Element();
 
     // First, get to the root of the XML document.
-    SimTK::Xml::Element current = element;
+    Element current = element;
     while (current.hasParentElement())
         current = current.getParentElement();
-    SimTK::Xml::Element root = current;
+    Element root = current;
 
     // This will be a recursive lambda function.
-    std::function<SimTK::Xml::Element(SimTK::Xml::Element&, const std::string&)>
+    std::function<Element(Element&, const std::string&)>
         searchForElement;
     // For recursion, must capture the function itself.
     // Returns an invalid Element if no element with `name` could be found.
     searchForElement = [&searchForElement](
-            SimTK::Xml::Element& elem, const std::string& name) -> SimTK::Xml::Element {
+            Element& elem, const std::string& name) -> Element {
         // This is a depth-first search.
         for (auto it = elem.element_begin(); it != elem.element_end();
                 ++it) {
             std::string elemName = it->getOptionalAttributeValue("name");
             if (elemName == name)
                 return elem;
-            SimTK::Xml::Element foundElem = searchForElement(*it, name);
+            Element foundElem = searchForElement(*it, name);
             if (foundElem.isValid())
                 return foundElem;
             // Keep searching other branches.
         }
-        return SimTK::Xml::Element(); // Did not find.
+        return Element(); // Did not find.
     };
     return searchForElement(root, name);
 }
