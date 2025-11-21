cb7a29acfd35215ad0ed6da1a94e82609f074963
aymanhab <ahabib@stanford.edu>
2025-11-21 11:28:50 -0800

    copy dlls on windows

--- buildWheel.cmake
+++ buildWheel.cmake
@@ -1,5 +1,11 @@
-if (APPLE)
-    message(STATUS "Copy *.dylib, *.a files from ${CMAKE_INSTALL_PREFIX}/lib to ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim")
+if (WIN32)
+    file(GLOB OPENSIM_ALL_LIBS "${CMAKE_INSTALL_PREFIX}/bin/*.dll")
+    set(DESTINATION_DIR ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim)
+    foreach(FILE ${OPENSIM_ALL_LIBS})
+        file(COPY ${FILE} DESTINATION ${DESTINATION_DIR})
+    endforeach()
+elseif(APPLE)
+    # message(STATUS "Copy *.dylib, *.a files from ${CMAKE_INSTALL_PREFIX}/lib to ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim")
     file(GLOB OPENSIM_ALL_LIBS "${CMAKE_INSTALL_PREFIX}/lib/*.dylib" 
                                 "${CMAKE_INSTALL_PREFIX}/lib/*.a"
                                 "${CMAKE_INSTALL_PREFIX}/sdk/lib/*.dylib" 
@@ -9,7 +15,7 @@ if (APPLE)
         file(COPY ${FILE} DESTINATION ${DESTINATION_DIR})
     endforeach()
 else()
-    message(STATUS "Copy .so* from ${CMAKE_INSTALL_PREFIX}/lib to ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim")
+    # message(STATUS "Copy .so* from ${CMAKE_INSTALL_PREFIX}/lib to ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim")
     file(GLOB OPENSIM_ALL_LIBS "${CMAKE_INSTALL_PREFIX}/sdk/lib/*.so*" "${CMAKE_INSTALL_PREFIX}/lib/*.so*")
     set(DESTINATION_DIR ${CMAKE_INSTALL_PREFIX}/${OPENSIM_INSTALL_PYTHONDIR}/opensim)
     foreach(FILE ${OPENSIM_ALL_LIBS})
