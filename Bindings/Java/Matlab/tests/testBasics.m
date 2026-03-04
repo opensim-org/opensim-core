025a13ee28db9429ddd6e839f050c6ab137812ae
nickbianco <nbianco@stanford.edu>
2026-03-04 13:38:32 -0600

    Override StdVectorDouble get() method in Matlab to return double instead of java.lang.Double

---
+++ Bindings/Java/Matlab/tests/testBasics.m
@@ -0,0 +1,36 @@
+% -------------------------------------------------------------------------- %
+%                          OpenSim:  testBasics.m                            %
+% -------------------------------------------------------------------------- %
+% The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  %
+% See http://opensim.stanford.edu and the NOTICE file for more information.  %
+% OpenSim is developed at Stanford University and supported by the US        %
+% National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    %
+% through the Warrior Web program.                                           %
+%                                                                            %
+% Copyright (c) 2026 Stanford University and the Authors                     %
+% Author(s): Nicholas Bianco                                                 %
+%                                                                            %
+% Licensed under the Apache License, Version 2.0 (the "License"); you may    %
+% not use this file except in compliance with the License. You may obtain a  %
+% copy of the License at http://www.apache.org/licenses/LICENSE-2.0.         %
+%                                                                            %
+% Unless required by applicable law or agreed to in writing, software        %
+% distributed under the License is distributed on an "AS IS" BASIS,          %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   %
+% See the License for the specific language governing permissions and        %
+% limitations under the License.                                             %
+% -------------------------------------------------------------------------- %
+import org.opensim.modeling.*;
+
+% Test STL typemaps.
+% ------------------
+vec = StdVectorDouble();
+for i = 0:4
+    vec.add(i);
+end
+assert(vec.size() == 5);
+for i = 0:vec.size() - 1
+    value = vec.get(i);
+    assert(value == i);
+    assert(string(class(value)) == 'double');
+end
